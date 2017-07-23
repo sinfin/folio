e3732b5058a7dd61da26365dcaf2d928faf91a30
Filip Ornstein <filip@sinfin.cz>
2017-07-23 09:56:16 +0200

    W console nodes CRUD

--- app/controllers/folio/console/nodes_controller.rb
+++ app/controllers/folio/console/nodes_controller.rb
@@ -2,18 +2,40 @@ require_dependency 'folio/application_controller'
 
 module Folio
   class Console::NodesController < Console::BaseController
-    before_action :find_node, except: [:index]
+    before_action :find_node, except: [:index, :create, :new]
 
     def index
       @nodes = Folio::Node.arrange
     end
 
-    def edit
+    def new
+      @node = Folio::Node.new
+    end
+
+    def create
+      @node = Folio::Node.create(node_params)
+      respond_with @node, location: console_nodes_path
+    end
+
+    def update
+      @node.update(node_params)
+      respond_with @node, location: console_nodes_path
+    end
+
+    def destroy
+      @node.destroy
+      respond_with @node, location: console_nodes_path
     end
 
   private
     def find_node
       @node = Folio::Node.friendly.find(params[:id])
+    end
+
+    def node_params
+      p = params.require(:node).permit(:title, :slug, :perex, :content, :meta_title, :meta_description, :code, :ancestry, :type, :featured, :published_at, :locale)
+      p.delete(:password) unless p[:password].present?
+      p
     end
   end
 end
