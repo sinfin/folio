490181737f0723e6c1461a223b4b68e27043bb57
Martin Dedek <martin.dedek@sinfin.cz>
2017-08-10 13:57:18 +0200

    * file type validation

--- app/models/folio/file.rb
+++ app/models/folio/file.rb
@@ -6,7 +6,11 @@ module Folio
     has_many :file_placements, class_name: 'Folio::FilePlacement'
 
     # Validations
-    validates :file, presence: true
+    def self.types
+      %w"Folio::Image Folio::Document"
+    end
+    validates :file, :type, presence: true
+    validates :type, inclusion: { in: types }
 
     def title
       file_name
