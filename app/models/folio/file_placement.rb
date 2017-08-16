ed08088ad6c0f69682fb657df1a4b733a1cce4d0
Martin Dedek <martin.dedek@sinfin.cz>
2017-08-16 13:02:41 +0200

    * file_placement scopes

--- app/models/folio/file_placement.rb
+++ app/models/folio/file_placement.rb
@@ -3,5 +3,8 @@ module Folio
     # Relations
     belongs_to :file, class_name: 'Folio::File'
     belongs_to :node, class_name: 'Folio::FileNode'
+    # Scopes
+    scope :with_image,   -> { joins(:file).where("folio_files.type = 'Folio::Image'") }
+    scope :with_document,   -> { joins(:file).where("folio_files.type = 'Folio::Document'") }
   end
 end
