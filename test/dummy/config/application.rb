e24e99aee8d82106e957fee44d413059aaee60f7
Filip Ornstein <filip@sinfin.cz>
2017-08-24 11:05:56 +0200

    + set locale for site + console language prompt while translating a page

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -12,6 +12,9 @@ module Dummy
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    I18n.available_locales = [:cs, :en, :de, :es]
+    I18n.default_locale = :cs
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
