4feb4d5fd1c7de98b5ddce864e8ddae81c68cc3f
Petr Marek <contact@petrmarek.eu>
2017-09-26 08:06:22 +0200

    namespace lead mailer

---
+++ app/mailers/folio/lead_mailer.rb
@@ -0,0 +1,15 @@
+module Folio
+  class LeadMailer < ApplicationMailer
+    layout false
+
+    def notification_email(lead)
+      @lead = lead
+      site = Folio::Site.last
+      mail(to: site.email,
+           subject: "#{site.title} lead",
+           from: lead.email) do |format|
+        format.text
+      end
+    end
+  end
+end
