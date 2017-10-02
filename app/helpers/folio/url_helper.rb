41b3e04240276c0a44f31420e3695035edc4fa8c
Martin Dedek <martin.dedek@sinfin.cz>
2017-10-02 10:58:48 +0200

    * url helper

--- app/helpers/folio/url_helper.rb
+++ app/helpers/folio/url_helper.rb
@@ -3,8 +3,17 @@
 module Folio
   module UrlHelper
     def active_link_to(title, path, active = nil)
-      klass = ' active' if active || current_page?(path)
+      klass = ' active' if active || is_link_active?(path)
       link_to(title, path, class: klass)
+    end
+
+
+    # https://github.com/comfy/active_link_to/blob/master/lib/active_link_to/active_link_to.rb
+    def is_link_active?(path)
+      path = Addressable::URI::parse(path).path
+      path = Regexp.escape(path).chomp('/')
+      original = request.original_fullpath
+      !original.match(/^#{path}(\/.*|\?.*)?$/).blank?
     end
 
     def nested_page_url(page)
