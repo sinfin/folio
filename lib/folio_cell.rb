4a4039fca81e0f8c2622eb0a2e35474067164c4d
Petr Marek <contact@petrmarek.eu>
2017-10-02 09:59:41 +0200

    hide protect_against_forgery? warning

--- lib/folio_cell.rb
+++ lib/folio_cell.rb
@@ -3,4 +3,9 @@ class FolioCell < Cell::ViewModel
   include ActionView::Helpers::TranslationHelper
 
   self.view_paths << "#{Folio::Engine.root}/app/cells"
+
+  # https://github.com/trailblazer/cells-rails/issues/23#issuecomment-310537752
+  def protect_against_forgery?
+    controller.send(:protect_against_forgery?)
+  end
 end
