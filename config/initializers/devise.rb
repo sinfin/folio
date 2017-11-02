1af78efec0779de722b4005113d8032f28c9ea3b
Filip Ornstein <filip@sinfin.cz>
2017-11-02 23:32:25 +0100

    F devise parent controller

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -277,5 +277,5 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-  config.parent_controller = 'Folio::Devise::BaseController'
+  config.parent_controller = 'Folio::ApplicationController'
 end
