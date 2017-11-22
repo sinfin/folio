f37e54b876e937c4adbd6250f2c522e5c1779ec6
Petr Marek <contact@petrmarek.eu>
2017-11-22 12:50:53 +0100

    render_atoms only & except modifiers

--- app/helpers/folio/atoms_helper.rb
+++ app/helpers/folio/atoms_helper.rb
@@ -2,14 +2,17 @@
 
 module Folio
   module AtomsHelper
-    def render_atoms(atoms)
+    def render_atoms(atoms, only: [], except: [])
       atoms.ordered.map do |atom|
+        next if only.present? && !only.include?(atom.class)
+        next if except.present? && except.include?(atom.class)
+
         if atom.cell_name
           cell(atom.cell_name, atom.data)
         else
           render "folio/atoms/#{atom.partial_name}", data: atom.data
         end
-      end.join('').html_safe
+      end.compact.join('').html_safe
     end
   end
 end
