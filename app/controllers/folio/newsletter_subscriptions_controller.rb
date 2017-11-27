e52044cc0f97699203b312b27b01385df1272bda
Petr Marek <contact@petrmarek.eu>
2017-11-27 15:32:10 +0100

    newsletter_subscription_form memoize cell_options

--- app/controllers/folio/newsletter_subscriptions_controller.rb
+++ app/controllers/folio/newsletter_subscriptions_controller.rb
@@ -10,13 +10,22 @@ module Folio
 
       NewsletterSubscriptionMailer.notification_email(@newsletter_subscription).deliver_later if success
 
-      render html: cell('folio/newsletter_subscription_form', @newsletter_subscription)
+      render html: cell('folio/newsletter_subscription_form', @newsletter_subscription, cell_options_params)
     end
 
     private
 
       def newsletter_subscription_params
         params.require(:newsletter_subscription).permit(:email)
+      end
+
+      def cell_options_params
+        cell_options = params[:cell_options]
+        if cell_options
+          cell_options.permit(:placeholder, :submit_text, :message)
+        else
+          nil
+        end
       end
   end
 end
