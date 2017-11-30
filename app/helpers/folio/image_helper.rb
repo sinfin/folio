bb9bf3dd18fe933a5c89a58c4133f7f6998a46ff
Petr Marek <contact@petrmarek.eu>
2017-11-30 12:43:48 +0100

    add image_helper

---
+++ app/helpers/folio/image_helper.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Folio
+  module ImageHelper
+    def img_tag_retina(normal, retina, html_options = {})
+      escaped_normal = URI.encode(normal)
+      escaped_retina = URI.encode(retina)
+      html_options[:srcset] = "#{escaped_normal} 1x, #{escaped_retina} 2x"
+      image_tag escaped_normal, html_options
+    end
+
+    def img_tag_retina_static(path, html_options = {})
+      split_path = path.split('.')
+      retina_path = split_path.first(split_path.size - 1).join('.') + '@2x.' + split_path.last
+
+      normal = image_path(path)
+      retina = image_path(retina_path)
+
+      img_tag_retina normal, retina, html_options
+    end
+
+    def dummy_image_url(variant)
+      "http://dummyimage.com/#{variant}/FFF/000.png&text=TODO: Vybrat a nahrát v consoli"
+    end
+  end
+end
