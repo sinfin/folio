afdf6a09093cbad231965440ed81654a5054232c
Petr Marek <contact@petrmarek.eu>
2017-12-04 14:26:21 +0100

    uncomment og:image

--- app/helpers/folio/open_graph_helper.rb
+++ app/helpers/folio/open_graph_helper.rb
@@ -10,17 +10,17 @@ module Folio
       @og_description.presence || public_page_description
     end
 
-    # def og_thumbnail
-    #   image_url @og_image ? @og_image.url : 'fb-share/fb-share.jpg'
-    # end
-    #
-    # def og_image_width
-    #   @og_image.width if @og_image
-    # end
-    #
-    # def og_image_height
-    #   @og_image.height if @og_image
-    # end
+    def og_image
+      @og_image || image_url('fb-share.jpg')
+    end
+
+    def og_image_width
+      @og_image_width || 1200
+    end
+
+    def og_image_height
+      @og_image_height || 630
+    end
 
     def og_site_name
       @og_site_name.presence || t('domain.short')
