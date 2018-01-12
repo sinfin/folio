de9cdf604ca3fd5473f38bea5a0190c7b8f18eef
Petr Marek <contact@petrmarek.eu>
2018-01-12 15:48:07 +0100

    lock pg gem version

--- lib/generators/folio/install_generator.rb
+++ lib/generators/folio/install_generator.rb
@@ -16,7 +16,7 @@ module Folio
         gem 'cells-slim'
         gem 'route_translator'
         gem 'breadcrumbs_on_rails'
-        gem 'pg'
+        gem 'pg', version: '~> 0.21.0'
 
         gem_group :development do
           gem 'rbnacl', version: '< 5.0'
