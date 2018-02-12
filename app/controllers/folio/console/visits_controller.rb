6f144e5a9cd60941130678f3ca8a931c6b559457
Petr Marek <contact@petrmarek.eu>
2018-02-12 14:04:07 +0100

    fix visits_controller namespace

--- app/controllers/folio/console/visits_controller.rb
+++ app/controllers/folio/console/visits_controller.rb
@@ -1,21 +1,20 @@
 # frozen_string_literal: true
 
-module Folio
-  class Console::VisitsController < Console::BaseController
-    load_and_authorize_resource
-    add_breadcrumb Visit.model_name.human(count: 2), :console_visits_path
+class Folio::Console::VisitsController < Folio::Console::BaseController
+  load_and_authorize_resource
+  add_breadcrumb Visit.model_name.human(count: 2), :console_visits_path
 
-    def index
-      @visits = @visits.filter(filter_params) if params[:by_query].present?
-      @visits = @visits.page(current_page)
-    end
-
-    def show
-    end
-
-    private
-      def filter_params
-        params.permit(:by_query)
-      end
+  def index
+    @visits = @visits.filter(filter_params) if params[:by_query].present?
+    @visits = @visits.page(current_page)
   end
+
+  def show
+  end
+
+  private
+
+    def filter_params
+      params.permit(:by_query)
+    end
 end
