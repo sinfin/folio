sinfin/folio

commit b81c2e281ca8f803e244c03b83a5ee1798ee0576

* nodes controller redirect to edit after save

app/controllers/folio/console/nodes_controller.rb
@@ -47,12 +47,12 @@ module Folio
       # set type first beacuse of @node.additional_params
       @node = Node.new(type: params[:node][:type])
       @node.update(node_params)
-      respond_with @node, location: console_nodes_path
+      respond_with @node, location: edit_console_node_path
     end
 
     def update
       @node.update(node_params)
-      respond_with @node, location: console_nodes_path
+      respond_with @node, location: edit_console_node_path
     end
 
     def destroy

commit 060ca318a2ddd3efdb7e80654e2a19d9ece05466

fix nodes#create
broken in b81c2e281ca8f803e244c03b83a5ee1798ee0576

app/controllers/folio/console/nodes_controller.rb
@@ -46,8 +46,8 @@ module Folio
     def create
       # set type first beacuse of @node.additional_params
       @node = Node.new(type: params[:node][:type])
-      @node.update(node_params)
-      respond_with @node, location: edit_console_node_path
+      success = @node.update(node_params)
+      respond_with @node, location: success ? edit_console_node_path : console_nodes_path
     end
 
     def update