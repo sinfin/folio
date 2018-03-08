0b393fc1f8836cc71138635923864df136a9b719
Petr Marek <contact@petrmarek.eu>
2018-03-08 17:39:42 +0100

    fix flag_helper namespace

--- app/helpers/folio/console/flag_helper.rb
+++ app/helpers/folio/console/flag_helper.rb
@@ -5,7 +5,7 @@ module Folio
     CDN = 'https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/2.9.0/flags'
 
     def country_flag(locale)
-      code = Folio::LANGUAGES[locale.to_sym]
+      code = ::Folio::LANGUAGES[locale.to_sym]
       return locale unless code
       image_tag("#{CDN}/4x3/#{code.downcase}.svg",
                 alt: code,
