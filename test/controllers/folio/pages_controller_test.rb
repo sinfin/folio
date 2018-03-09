a7f8092a7da035e3c2edd38a7a89562e8ac07456
Martin Dedek <martin.dedek@sinfin.cz>
2018-03-09 15:52:48 +0100

    + pages controller test

--- test/controllers/folio/pages_controller_test.rb
+++ test/controllers/folio/pages_controller_test.rb
@@ -6,8 +6,41 @@ module Folio
   class PagesControllerTest < ActionDispatch::IntegrationTest
     include Engine.routes.url_helpers
 
-    # test "the truth" do
-    #   assert true
-    # end
+    setup do
+      @category = create(:folio_category)
+      @page = create(:folio_page, parent: @category)
+
+      @category_en = @category.translate!(:en)
+      @category_en.update(slug: 'category_en', published: true, published_at: 1.hour.ago)
+      @page_en = @page.translate!(:en)
+      @page_en.update(slug: 'page_en', published: true, published_at: 1.hour.ago)
+    end
+
+    test 'root page should get show' do
+      get page_url(@category, locale: :cs)
+      assert_response :success
+    end
+
+    test 'category page within category should get show' do
+      get page_url([@category, @page], locale: :cs)
+      assert_response :success
+    end
+
+    test 'category page without category should not get show' do
+      assert_raises(ActiveRecord::RecordNotFound) do
+        get page_url(@page, locale: :cs)
+      end
+    end
+
+    test 'translated page should get show' do
+      get page_url(@category_en, locale: :en)
+      assert_response :success
+    end
+
+    test 'translated page should not get show for other locales' do
+      assert_raises(ActiveRecord::RecordNotFound) do
+        get page_url(@category_en, locale: :cs)
+      end
+    end
   end
 end
