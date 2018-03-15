2d5ec8234e9ee68d96733126143e6c3cb97807bd
Petr Marek <contact@petrmarek.eu>
2018-03-15 09:51:41 +0100

    fix filters_cell test

--- test/cells/folio/console/index/filters_cell_test.rb
+++ test/cells/folio/console/index/filters_cell_test.rb
@@ -4,11 +4,18 @@ require 'test_helper'
 
 class Folio::Console::Index::FiltersCellTest < Cell::TestCase
   test 'show' do
-    html = cell('folio/console/index/filters', [:by_type]).(:show)
+    filter = {
+      by_published: [
+        ['All', nil],
+        ['Published', 'published'],
+        ['Unpublished', 'unpublished'],
+      ]
+    }
+    html = cell('folio/console/index/filters', filter).(:show)
 
     assert_equal 1,
                  html.find_css('input[type="hidden"][name="by_query"]').length
     assert_equal 1,
-                 html.find_css('select[name="by_type"]').length
+                 html.find_css('select[name="by_published"]').length
   end
 end
