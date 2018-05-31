8c4695c839d28c43d5c4c8d79d8e4073006a8924
Petr Marek <contact@petrmarek.eu>
2018-05-31 15:23:57 +0200

    add table_exists? check to friendly_id and acts_as_taggable migrations

--- db/migrate/20170710125813_create_friendly_id_slugs.rb
+++ db/migrate/20170710125813_create_friendly_id_slugs.rb
@@ -1,17 +1,23 @@
 # frozen_string_literal: true
 
 class CreateFriendlyIdSlugs < ActiveRecord::Migration[5.1]
-  def change
-    create_table :friendly_id_slugs do |t|
-      t.string   :slug,           null: false
-      t.integer  :sluggable_id,   null: false
-      t.string   :sluggable_type, limit: 50
-      t.string   :scope
-      t.datetime :created_at
+  def up
+    unless table_exists?(:friendly_id_slugs)
+      create_table :friendly_id_slugs do |t|
+        t.string   :slug,           null: false
+        t.integer  :sluggable_id,   null: false
+        t.string   :sluggable_type, limit: 50
+        t.string   :scope
+        t.datetime :created_at
+      end
+      add_index :friendly_id_slugs, :sluggable_id
+      add_index :friendly_id_slugs, %i[slug sluggable_type]
+      add_index :friendly_id_slugs, %i[slug sluggable_type scope], unique: true
+      add_index :friendly_id_slugs, :sluggable_type
     end
-    add_index :friendly_id_slugs, :sluggable_id
-    add_index :friendly_id_slugs, %i[slug sluggable_type]
-    add_index :friendly_id_slugs, %i[slug sluggable_type scope], unique: true
-    add_index :friendly_id_slugs, :sluggable_type
+  end
+
+  def down
+    drop_table :friendly_id_slugs
   end
 end
