0ccaa97f29b3d95a99e742e4888d0d62b711231f
Martin Dedek <martin.dedek@sinfin.cz>
2018-08-09 16:49:46 +0200

    remove factory for non-existent model

--- test/factories.rb
+++ test/factories.rb
@@ -45,10 +45,6 @@ FactoryBot.define do
     association :node, factory: :folio_node
   end
 
-  factory :folio_image_atom, class: Folio::Atom::Image do
-    association :cover, factory: :folio_image
-  end
-
   factory :folio_file_placement, class: Folio::FilePlacement do
     association :file, factory: :folio_document
     association :placement, factory: :folio_page
