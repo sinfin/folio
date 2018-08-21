2f445f7032e10228305b9e443c9019a28f225105
Petr Marek <contact@petrmarek.eu>
2018-08-21 21:21:29 +0200

    add folio:atom generator

---
+++ lib/generators/folio/atom/atom_generator.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Folio::AtomGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('templates', __dir__)
+
+  def atom_model
+    template 'atom_model.rb.tt', "app/models/atom/#{file_name}.rb"
+  end
+
+  def cell
+    template 'cell.rb.tt', "app/cells/atom/#{file_name}_cell.rb"
+    template 'cell_test.rb.tt', "test/cells/atom/#{file_name}_cell_test.rb"
+    template 'cell.slim.tt', "test/cells/atom/#{file_name}_cell/show.slim"
+  end
+
+  private
+
+    def classname_prefix
+      Rails.application.class.name[0].downcase
+    end
+
+    def dashed_resource_name
+      model_resource_name.gsub('_', '-')
+    end
+end
