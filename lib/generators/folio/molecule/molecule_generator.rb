69da025ad29dd8ec9e2509afccb43bc1350de6d6
Petr Marek <contact@petrmarek.eu>
2018-08-27 22:38:54 +0200

    atom-cell-free molecule generator

--- lib/generators/folio/molecule/molecule_generator.rb
+++ lib/generators/folio/molecule/molecule_generator.rb
@@ -3,25 +3,14 @@
 class Folio::MoleculeGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
 
+  def atom_model
+    template 'atom_model.rb.tt', "app/models/atom/#{file_name}.rb"
+  end
+
   def cell
     template 'cell.rb.tt', "app/cells/molecule/#{plural_route_name}_cell.rb"
-    template 'cell_test.rb.tt', "test/cells/molecule/#{plural_route_name}_cell_test.rb"
     template 'cell.slim.tt', "app/cells/molecule/#{plural_route_name}/show.slim"
-  end
-
-  def update_atom
-    atom_path = "app/models/atom/#{model_resource_name}.rb"
-
-    if File.exists? Rails.root.join(atom_path)
-      inject_into_file atom_path, before: %{end
-
-# == Schema Information} do %{
-  def self.molecule_cell_name
-    'molecule/#{plural_table_name}'
-  end
-}
-      end
-    end
+    template 'cell_test.rb.tt', "test/cells/molecule/#{plural_route_name}_cell_test.rb"
   end
 
   private
@@ -30,15 +19,19 @@ class Folio::MoleculeGenerator < Rails::Generators::NamedBase
       Rails.application.class.name[0].downcase
     end
 
-    def dashed_resource_name
+    def plural_dashed_resource_name
       plural_table_name.gsub('_', '-')
     end
 
-    def dashed_model_resource_name
+    def dashed_resource_name
       model_resource_name.gsub('_', '-')
     end
 
     def molecule_name
       plural_name.camelize
+    end
+
+    def molecule_class_name
+      "#{classname_prefix}-molecule-#{plural_dashed_resource_name}"
     end
 end
