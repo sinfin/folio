15a0c22d51e3995c4d681b781719b86b29d3a018
Petr Marek <contact@petrmarek.eu>
2018-09-04 11:41:19 +0200

    Menu use named default scope for menu items

--- app/models/folio/menu.rb
+++ app/models/folio/menu.rb
@@ -3,7 +3,7 @@
 module Folio
   class Menu < ApplicationRecord
     # Relations
-    has_many :menu_items, -> { order(:position) }, dependent: :destroy
+    has_many :menu_items, -> { ordered }, dependent: :destroy
     accepts_nested_attributes_for :menu_items, allow_destroy: true,
                                                reject_if: :all_blank
 
