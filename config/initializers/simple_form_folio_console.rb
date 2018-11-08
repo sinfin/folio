23a4f5ae92dec53bd263ee2ec511f21219b121a6
Petr Marek <contact@petrmarek.eu>
2018-11-08 11:14:52 +0100

    add with_flag simple form wrapper

---
+++ config/initializers/simple_form_folio_console.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+Dir["#{Folio::Engine.root}/lib/folio/console/simple_form_components/*.rb"].each do |file|
+  require_dependency file
+end
+
+SimpleForm.setup do |config|
+  config.wrappers :with_flag, tag: 'div', class: 'form-group form-group--with-flag', error_class: 'text-danger has-danger' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.optional :maxlength
+    b.optional :minlength
+    b.optional :pattern
+    b.optional :min_max
+    b.optional :readonly
+
+    b.use :input, class: 'form-control'
+    b.use :error, wrap_with: { tag: 'small', class: 'form-text' }
+    b.use :hint,  wrap_with: { tag: 'small', class: 'form-text' }
+
+    b.use :flag, wrap_with: { tag: 'div', class: 'form-group__flag' }
+  end
+end
