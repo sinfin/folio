f2eb7330f14b2af8795d57b7bbea6b3358d8c57a
Petr Marek <contact@petrmarek.eu>
2019-02-04 12:20:41 +0100

    better console form header

--- app/cells/folio/console/form/header_cell.rb
+++ app/cells/folio/console/form/header_cell.rb
@@ -4,4 +4,8 @@ class Folio::Console::Form::HeaderCell < Folio::ConsoleCell
   def translations
     cell('folio/console/nodes/translations', model.original, as_pills: true)
   end
+
+  def title
+    model.try(:to_label) || model.try(:title) || model.class.model_name.human
+  end
 end
