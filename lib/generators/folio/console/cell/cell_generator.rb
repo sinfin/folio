f0aafa2165bc10ed6528755e062b86aeafff7be5
Petr Marek <contact@petrmarek.eu>
2019-02-07 09:14:07 +0100

    add console cell generator

---
+++ lib/generators/folio/console/cell/cell_generator.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class Folio::Console::CellGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('templates', __dir__)
+
+  def cell
+    template 'cell.rb.tt', "app/cells/#{global_namespace_path}/#{name}_cell.rb"
+    template 'cell.slim.tt', "app/cells/#{global_namespace_path}/#{name}/show.slim"
+    template 'cell_test.rb.tt', "test/cells/#{global_namespace_path}/#{name}_cell_test.rb"
+  end
+
+  private
+
+    def classname_prefix
+      'f-c'
+    end
+
+    def dashed_resource_name
+      model_resource_name.gsub('_', '-')
+    end
+
+    def cell_name
+      "#{global_namespace_path}/#{name}"
+    end
+
+    def global_namespace_path
+      global_namespace.underscore
+    end
+
+    def global_namespace
+      'Folio::Console'
+    end
+end
---
+++ test/lib/generators/folio/folio/console/cell_generator_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'generators/folio/console/cell/cell_generator'
+
+module Folio
+  class Folio::Console::CellGeneratorTest < Rails::Generators::TestCase
+    tests Folio::Console::CellGenerator
+    destination Rails.root.join('tmp/generators')
+    setup :prepare_destination
+
+    # test "generator runs without errors" do
+    #   assert_nothing_raised do
+    #     run_generator ["arguments"]
+    #   end
+    # end
+  end
+end
