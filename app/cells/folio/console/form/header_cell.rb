8abc066f0a07e98d0cc8cf4c7de49130b02f39d8
Petr Marek <contact@petrmarek.eu>
2019-02-15 18:39:05 +0100

    update form header

--- app/cells/folio/console/form/header_cell.rb
+++ app/cells/folio/console/form/header_cell.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Folio::Console::Form::HeaderCell < Folio::ConsoleCell
+  def record
+    model.try(:object) || model
+  end
+
   def translations
     cell('folio/console/pages/translations', model, as_pills: true)
   end
--- app/helpers/folio/console/cells_helper.rb
+++ app/helpers/folio/console/cells_helper.rb
@@ -35,6 +35,10 @@ module Folio
                                                   as: as).show.html_safe
     end
 
+    def form_header(f, opts = {})
+      cell('folio/console/form/header', f, opts).show.html_safe
+    end
+
     def form_footer(f)
       cell('folio/console/form/footer', f).show.html_safe
     end
