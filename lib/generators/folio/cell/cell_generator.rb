dfa6f4a0cd0383b6971dc6109c23eaf54261e46b
Petr Marek <contact@petrmarek.eu>
2019-03-21 10:22:28 +0100

    modify cell generator to handle non-namespaced cells with leading slash

--- lib/generators/folio/cell/cell_generator.rb
+++ lib/generators/folio/cell/cell_generator.rb
@@ -4,9 +4,9 @@ class Folio::CellGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
 
   def cell
-    template 'cell.rb.tt', "app/cells/#{global_namespace_path}/#{name}_cell.rb"
-    template 'cell.slim.tt', "app/cells/#{global_namespace_path}/#{name}/show.slim"
-    template 'cell_test.rb.tt', "test/cells/#{global_namespace_path}/#{name}_cell_test.rb"
+    template 'cell.rb.tt', "app/cells/#{cell_name}_cell.rb"
+    template 'cell.slim.tt', "app/cells/#{cell_name}/show.slim"
+    template 'cell_test.rb.tt', "test/cells/#{cell_name}_cell_test.rb"
   end
 
   private
@@ -15,12 +15,20 @@ class Folio::CellGenerator < Rails::Generators::NamedBase
       Rails.application.class.name[0].downcase
     end
 
+    def model_resource_name
+      super.gsub(/\A_/, '')
+    end
+
+    def class_name
+      super.gsub(/\A::/, '')
+    end
+
     def dashed_resource_name
       model_resource_name.gsub('_', '-')
     end
 
     def cell_name
-      "#{global_namespace_path}/#{name}"
+      "#{global_namespace_path}#{name.gsub(/\A\//, '')}"
     end
 
     def global_namespace_path
@@ -28,6 +36,10 @@ class Folio::CellGenerator < Rails::Generators::NamedBase
     end
 
     def global_namespace
-      Rails.application.class.name.deconstantize
+      if name.start_with?('/')
+        ''
+      else
+        "#{Rails.application.class.name.deconstantize}::"
+      end
     end
 end
