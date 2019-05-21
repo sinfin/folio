49748dbc92e661485a340767100de882215d0dfd
dedekm <dedekmm@gmail.com>
2019-05-21 18:08:49 +0200

    fix subscribable

--- app/models/concerns/folio/subscribable.rb
+++ app/models/concerns/folio/subscribable.rb
@@ -9,7 +9,7 @@ module Folio::Subscribable
 
   def subscribe
     if !Rails.env.test? && (!Rails.env.development? || ENV['DEV_MAILCHIMP'])
-      Folio::Mailchimp::SubscribeJob.perform_now(self, status: subscription_default_status,
+      Folio::Mailchimp::SubscribeJob.perform_later(self, status: subscription_default_status,
                                                        merge_vars: subscription_merge_vars,
                                                        tags: subscription_tags)
     end
