982fce52515ccafeaf375bc5e050eeecb557c258
dedekm <dedekmm@gmail.com>
2019-05-24 11:36:04 +0200

    update admin for site, add emails validations

--- app/models/folio/site.rb
+++ app/models/folio/site.rb
@@ -6,14 +6,18 @@ class Folio::Site < Folio::ApplicationRecord
   after_commit :clear_fragment_cache!
 
   # Validations
-  validates :title, :locale, :locales,
+  validates :title, :email, :locale, :locales,
             presence: true
 
   validates :domain,
             uniqueness: true
 
   validates_format_of :email,
-                      with: Folio::EMAIL_REGEXP
+                      :email_from,
+                      with: Folio::EMAIL_REGEXP,
+                      allow_nil: true
+
+  validate :system_emails_should_be_valid
 
   def self.additional_params
     []
@@ -32,10 +36,29 @@ class Folio::Site < Folio::ApplicationRecord
        messenger]
   end
 
+  %i[system_email system_email_copy].each do |attr|
+    define_method("#{attr}_array") do
+      return [] if send(attr).nil?
+
+      send(attr).gsub(' ', '').split(',')
+    end
+  end
+
   private
 
     def clear_fragment_cache!
       Rails.cache.clear
+    end
+
+    def system_emails_should_be_valid
+      %i[system_email system_email_copy].each do |attr|
+        send(:"#{attr}_array").each do |email|
+          unless Folio::EMAIL_REGEXP.match?(email)
+            errors.add(attr, :invalid)
+            break
+          end
+        end
+      end
     end
 end
 
--- db/migrate/20190524080820_add_additional_mail_attributes_to_folio_sites.rb
+++ db/migrate/20190524080820_add_additional_mail_attributes_to_folio_sites.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddAdditionalMailAttributesToFolioSites < ActiveRecord::Migration[5.2]
   def change
     add_column :folio_sites, :system_email, :string
