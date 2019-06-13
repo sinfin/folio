6a13a8e4e635a8f7071d4d15ead7e1fede821449
Petr Marek <contact@petrmarek.eu>
2019-06-13 09:10:58 +0200

    fix atom locale migration

--- db/migrate/20190218132920_add_atom_locale.rb
+++ db/migrate/20190218132920_add_atom_locale.rb
@@ -3,6 +3,7 @@
 class AddAtomLocale < ActiveRecord::Migration[5.2]
   def change
     add_column :folio_atoms, :locale, :string, index: true
+    Folio::Atom::Base.reset_column_information
 
     if Rails.application.config.folio_using_traco
       Folio::Atom::Base.find_each do |atom|
@@ -15,27 +16,27 @@ class AddAtomLocale < ActiveRecord::Migration[5.2]
             translation.file_placements << fp.dup
           end
 
-          translation["title_#{I18n.default_locale}"] = translation["title_#{locale}"]
-          translation["perex_#{I18n.default_locale}"] = translation["perex_#{locale}"]
-          translation["content_#{I18n.default_locale}"] = translation["content_#{locale}"]
+          translation["title_#{I18n.default_locale}"] = atom["title_#{locale}"]
+          translation["perex_#{I18n.default_locale}"] = atom["perex_#{locale}"]
+          translation["content_#{I18n.default_locale}"] = atom["content_#{locale}"]
 
           translation.save(validate: false)
         end
+      end
 
-        (I18n.available_locales - [I18n.default_locale]).each do |locale|
-          %i[title perex content].each do |field|
-            column = "#{field}_#{locale}".to_sym
-            if column_exists?(:folio_atoms, column)
-              remove_column :folio_atoms, column
-            end
+      (I18n.available_locales - [I18n.default_locale]).each do |locale|
+        %i[title perex content].each do |field|
+          column = "#{field}_#{locale}".to_sym
+          if column_exists?(:folio_atoms, column)
+            remove_column :folio_atoms, column
           end
         end
+      end
 
-        %i[title perex content].each do |field|
-          column = "#{field}_#{I18n.default_locale}".to_sym
-          if column_exists?(:folio_atoms, column)
-            rename_column :folio_atoms, column, field
-          end
+      %i[title perex content].each do |field|
+        column = "#{field}_#{I18n.default_locale}".to_sym
+        if column_exists?(:folio_atoms, column)
+          rename_column :folio_atoms, column, field
         end
       end
     end
