5083840385173c725625aaf43ef747c92dbb01c2
Petr Marek <contact@petrmarek.eu>
2019-06-13 10:28:03 +0200

    fix show_for cover css

--- app/cells/folio/console/index/images_cell.rb
+++ app/cells/folio/console/index/images_cell.rb
@@ -3,6 +3,8 @@
 class Folio::Console::Index::ImagesCell < Folio::ConsoleCell
   include Folio::CellLightbox
 
+  class_name 'f-c-index-images', :cover, :gallery
+
   def id_class_name
     # needed for separate lightboxes
     "f-c-index-images--id-#{model.class.table_name}-#{model.id}"
@@ -24,5 +26,9 @@ class Folio::Console::Index::ImagesCell < Folio::ConsoleCell
 
   def broken_src
     'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBmaWxsPSJub25lIiBkPSJNMCAwaDI0djI0SDB6bTAgMGgyNHYyNEgwem0yMSAxOWMwIDEuMS0uOSAyLTIgMkg1Yy0xLjEgMC0yLS45LTItMlY1YzAtMS4xLjktMiAyLTJoMTRjMS4xIDAgMiAuOSAyIDIiLz48cGF0aCBmaWxsPSJub25lIiBkPSJNMCAwaDI0djI0SDB6Ii8+PHBhdGggZD0iTTIxIDV2Ni41OWwtMy0zLjAxLTQgNC4wMS00LTQtNCA0LTMtMy4wMVY1YzAtMS4xLjktMiAyLTJoMTRjMS4xIDAgMiAuOSAyIDJ6bS0zIDYuNDJsMyAzLjAxVjE5YzAgMS4xLS45IDItMiAySDVjLTEuMSAwLTItLjktMi0ydi02LjU4bDMgMi45OSA0LTQgNCA0IDQtMy45OXoiLz48L3N2Zz4='
+  end
+
+  def gallery
+    !options[:cover]
   end
 end
