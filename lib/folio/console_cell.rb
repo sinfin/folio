0effdf9c8366f7254fd134cd69381c50e2936aa5
Petr Marek <contact@petrmarek.eu>
2019-06-20 11:52:33 +0200

    update html_safe_fields_for

--- lib/folio/console_cell.rb
+++ lib/folio/console_cell.rb
@@ -3,7 +3,13 @@
 class Folio::ConsoleCell < Folio::ApplicationCell
   include Folio::Console::CellsHelper
 
-  def html_safe_fields_for(f, key, obj = nil, &block)
+  def html_safe_fields_for(f, key, build: false, &block)
+    obj = f.object.send(key)
+
+    if build && obj.blank?
+      obj = f.object.send("build_#{key}")
+    end
+
     f.simple_fields_for key, obj do |subfields|
       (yield subfields).html_safe
     end
