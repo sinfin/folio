7995964b0bef9b31609d36c411e578b544edfba5
Petr Marek <contact@petrmarek.eu>
2019-06-21 14:27:46 +0200

    tweak table_show_for, add date :short format

--- app/helpers/folio/console/show_for_helper.rb
+++ app/helpers/folio/console/show_for_helper.rb
@@ -14,7 +14,13 @@ module Folio::Console::ShowForHelper
     content_tag(:div, all.join('').html_safe, class: 'f-c-show-for-index')
   end
 
-  def table_show_for(model, &block)
-    content_tag(:div, show_for(model, &block), class: 'f-c-show-for-table')
+  def table_show_for(model, wide: false, &block)
+    if wide
+      class_name = 'f-c-show-for-table f-c-show-for-table--wide'
+    else
+      class_name = 'f-c-show-for-table'
+    end
+
+    content_tag(:div, show_for(model, &block), class: class_name)
   end
 end
