af7f454365ff7ccb6045982005f6eeb8c2647399
Petr Marek <contact@petrmarek.eu>
2019-08-01 11:32:49 +0200

    add dummy gallery atom

---
+++ test/dummy/app/models/dummy/atom/gallery.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class Dummy::Atom::Gallery < Folio::Atom::Base
+  ATTACHMENTS = %i[images]
+
+  STRUCTURE = {
+    title: :string,
+  }
+
+  def self.cell_name
+    'dummy/atom/gallery'
+  end
+end
+
+# == Schema Information
+#
+# Table name: folio_atoms
+#
+#  id             :bigint(8)        not null, primary key
+#  type           :string
+#  content        :text
+#  position       :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  placement_type :string
+#  placement_id   :bigint(8)
+#  model_type     :string
+#  model_id       :bigint(8)
+#  title          :string
+#  perex          :text
+#
+# Indexes
+#
+#  index_folio_atoms_on_model_type_and_model_id          (model_type,model_id)
+#  index_folio_atoms_on_placement_type_and_placement_id  (placement_type,placement_id)
+#
