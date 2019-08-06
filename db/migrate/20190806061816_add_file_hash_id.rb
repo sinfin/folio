9362970cf071bdb7746a3918f37124ea55a5301e
Petr Marek <contact@petrmarek.eu>
2019-08-06 11:42:29 +0200

    fix hash_id migrations

--- db/migrate/20190806061816_add_file_hash_id.rb
+++ db/migrate/20190806061816_add_file_hash_id.rb
@@ -3,10 +3,16 @@
 class AddFileHashId < ActiveRecord::Migration[5.2]
   def change
     add_column :folio_files, :hash_id, :string, index: true
+    add_column :folio_private_attachments, :hash_id, :string, index: true
 
-    Folio::File.find_each do |file|
-      file.set_hash_id
-      file.update_column(:hash_id, file.hash_id)
+    [
+      Folio::PrivateAttachment,
+      Folio::File,
+    ].each do |klass|
+      klass.find_each do |file|
+        file.set_hash_id
+        file.update_column(:hash_id, file.hash_id)
+      end
     end
   end
 end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_080110) do
+ActiveRecord::Schema.define(version: 2019_08_06_061816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
