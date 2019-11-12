850321f4a6b0daf61fdaf912236950c39e37d65d
Petr Marek <contact@petrmarek.eu>
2019-11-12 13:24:08 +0100

    tweak site console form

--- app/controllers/folio/console/sites_controller.rb
+++ app/controllers/folio/console/sites_controller.rb
@@ -4,6 +4,7 @@ class Folio::Console::SitesController < Folio::Console::BaseController
   folio_console_controller_for 'Folio::Site'
 
   before_action :find_site
+  before_action :add_site_breadcrumb
 
   def update
     @site.update(site_params)
@@ -13,25 +14,30 @@ class Folio::Console::SitesController < Folio::Console::BaseController
   private
 
     def site_params
-      params.require(:site).permit(:title,
-                                   :description,
-                                   :domain,
-                                   :locale,
-                                   :google_analytics_tracking_code,
-                                   :facebook_pixel_code,
-                                   :phone,
-                                   :email,
-                                   :email_from,
-                                   :system_email,
-                                   :system_email_copy,
-                                   :address,
-                                   *@site.class.additional_params,
-                                   *file_placements_strong_params,
-                                   locales: [],
-                                   social_links: Folio::Site.social_link_sites)
+      params.require(:site)
+            .permit(:title,
+                    :description,
+                    :domain,
+                    :locale,
+                    :google_analytics_tracking_code,
+                    :facebook_pixel_code,
+                    :phone,
+                    :email,
+                    :email_from,
+                    :system_email,
+                    :system_email_copy,
+                    :address,
+                    *@site.class.additional_params,
+                    *file_placements_strong_params,
+                    locales: [],
+                    social_links: Folio::Site.social_link_sites)
     end
 
     def find_site
       @site = Folio::Site.instance
+    end
+
+    def add_site_breadcrumb
+      add_breadcrumb(@klass.model_name.human(count: 2))
     end
 end
