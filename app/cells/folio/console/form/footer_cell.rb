9eede0335d5a43223be310517b3c767f22fbebd5
Petr Marek <contact@petrmarek.eu>
2020-01-28 11:28:55 +0100

    update console form footer

--- app/cells/folio/console/form/footer_cell.rb
+++ app/cells/folio/console/form/footer_cell.rb
@@ -7,4 +7,14 @@ class Folio::Console::Form::FooterCell < Folio::ConsoleCell
     options[:back_path] || controller.url_for([:console, model.object.class])
   rescue NoMethodError
   end
+
+  def submit_label
+    if options[:submit_continue]
+      t('folio.console.actions.continue')
+    elsif options[:submit_label].present?
+      options[:submit_label]
+    else
+      t('folio.console.actions.submit')
+    end
+  end
 end
