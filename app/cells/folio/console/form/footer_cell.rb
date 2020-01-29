dbe4d60af454b5255ca16d7485a94a72bdc3a37a
Petr Marek <contact@petrmarek.eu>
2020-01-29 18:03:15 +0100

    tweak console layout css

--- app/cells/folio/console/form/footer_cell.rb
+++ app/cells/folio/console/form/footer_cell.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Folio::Console::Form::FooterCell < Folio::ConsoleCell
-  class_name 'f-c-form-footer', :static
-
   def back_path
     options[:back_path] || controller.url_for([:console, model.object.class])
   rescue NoMethodError
