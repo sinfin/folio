e6b5d923106689b57beeee87a1a5e19f0b037ef1
Petr Marek <contact@petrmarek.eu>
2020-02-04 14:53:26 +0100

    add before_lambda and after_lambda to index_show_for

--- app/helpers/folio/console/show_for_helper.rb
+++ app/helpers/folio/console/show_for_helper.rb
@@ -1,17 +1,30 @@
 # frozen_string_literal: true
 
 module Folio::Console::ShowForHelper
-  def index_show_for(collection, &block)
+  def index_show_for(collection, before_lambda: nil, after_lambda: nil, &block)
     if collection.blank?
       return cell('folio/console/index/no_records', @klass).show.html_safe
     end
 
     empty = show_for(collection.first.class.new, &block).html_safe
 
-    all = [empty] + collection.map do |item|
-      show_for(item, &block).html_safe
+    rows = [empty]
+
+    collection.each_with_index do |item, i|
+      if before_lambda
+        result = before_lambda.call(item, collection, i)
+        rows << result if result.present?
+      end
+
+      rows << show_for(item, &block).html_safe
+
+      if after_lambda
+        result = after_lambda.call(item, collection, i)
+        rows << result if result.present?
+      end
     end
-    content_tag(:div, all.join('').html_safe, class: 'f-c-show-for-index')
+
+    content_tag(:div, rows.join('').html_safe, class: 'f-c-show-for-index')
   end
 
   def table_show_for(model, wide: false, &block)
