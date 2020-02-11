af520689e09c8abb15620dfd0166d48c50ed8033
Petr Marek <contact@petrmarek.eu>
2020-02-11 15:07:18 +0100

    update footer, fix footer test

--- test/cells/folio/console/merges/index/footer_cell_test.rb
+++ test/cells/folio/console/merges/index/footer_cell_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 class Folio::Console::Merges::Index::FooterCellTest < Folio::Console::CellTest
   test 'show' do
-    html = cell('folio/console/merges/index/footer', nil).(:show)
+    html = cell('folio/console/merges/index/footer', Folio::Page).(:show)
     assert html.has_css?('.f-c-merges-index-footer')
   end
 end
