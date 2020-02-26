5a4d2c751a67962db084a6fc1351d875b89cee5e
Petr Marek <contact@petrmarek.eu>
2020-02-26 13:30:54 +0100

    maintain index filters when switching index tabs

--- app/cells/folio/console/index/tabs_cell.rb
+++ app/cells/folio/console/index/tabs_cell.rb
@@ -6,4 +6,20 @@ class Folio::Console::Index::TabsCell < Folio::ConsoleCell
   def show
     render if model.present?
   end
+
+  def href(tab)
+    if options[:index_filters]
+      query = []
+      (%i[by_query] + options[:index_filters].keys).each do |key|
+        next if options[:params][key].blank?
+        query << "#{key}=#{options[:params][key]}"
+      end
+
+      if query.present?
+        return "#{tab[:href]}?#{query.join('&')}"
+      end
+    end
+
+    tab[:href]
+  end
 end
