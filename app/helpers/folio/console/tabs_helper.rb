3513a166f4d545e6ec93867fe8e04c3c0a43bc3e
Petr Marek <contact@petrmarek.eu>
2020-03-04 13:36:50 +0100

    respect tab param

--- app/helpers/folio/console/tabs_helper.rb
+++ app/helpers/folio/console/tabs_helper.rb
@@ -3,12 +3,22 @@
 module Folio::Console::TabsHelper
   def tabs(keys)
     if keys.present? && keys.size > 1
+      if keys.include?(params[:tab].try(:to_sym))
+        @folio_active_tab = params[:tab].try(:to_sym)
+      end
+
       render 'folio/console/partials/tabs', keys: keys
     end
   end
 
   def tab(key, active: false, &block)
-    content_tag(:div, class: "tab-pane #{active || key == :content ? 'active' : ''}",
+    if active || (key == :content && !@folio_active_tab)
+      @folio_active_tab = key
+    end
+
+    active = @folio_active_tab == key
+
+    content_tag(:div, class: "tab-pane #{active ? 'active' : ''}",
                       id: "tab-#{key}",
                       &block)
   end
