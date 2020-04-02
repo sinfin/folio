2a3ca3d8b5a9d040c7a3ac60f2930ea35d7310a0
Petr Marek <contact@petrmarek.eu>
2020-04-02 18:28:18 +0200

    fix newsletter_subscription test

--- test/controllers/folio/newsletter_subscriptions_controller_test.rb
+++ test/controllers/folio/newsletter_subscriptions_controller_test.rb
@@ -19,7 +19,7 @@ module Folio
       assert_response(:success)
       html = Nokogiri::HTML(response.body)
       assert_equal 0, html.css('.folio-newsletter-subscription-form-message').size
-      assert_equal 1, html.css('.form-group-invalid #newsletter_subscription_email').size
+      assert_equal 1, html.css('.form-group-invalid .form-control.email').size
     end
 
     test 'valid' do
