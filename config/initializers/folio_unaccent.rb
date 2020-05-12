09a06ec693df07fb7e2ecbb4fc768409a32a24f9
dedekm <dedekmm@gmail.com>
2020-05-12 19:23:52 +0200

    add folio_unaccent create & drop methods to active record migration

---
+++ config/initializers/folio_unaccent.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# creates immutable (and indexable) version of unaccent function
+# https://stackoverflow.com/questions/11005036/does-postgresql-support-accent-insensitive-collations/11007216#11007216
+ActiveRecord::Migration.class_eval do
+  def create_folio_unaccent
+    sql = <<~SQL
+      CREATE OR REPLACE FUNCTION public.unaccent_immutable(regdictionary, text)
+        RETURNS text LANGUAGE c IMMUTABLE PARALLEL SAFE STRICT AS
+      '$libdir/unaccent', 'unaccent_dict';
+
+      CREATE OR REPLACE FUNCTION public.folio_unaccent(text)
+        RETURNS text LANGUAGE sql IMMUTABLE PARALLEL SAFE STRICT AS
+      $func$
+      SELECT public.unaccent_immutable(regdictionary 'public.unaccent', $1)
+      $func$;
+    SQL
+
+    version = select_value('SHOW server_version').to_f
+    sql = sql.gsub('PARALLEL SAFE ', '') if version <= 9.5
+
+    execute sql
+  end
+
+  def drop_folio_unaccent
+    execute <<~SQL
+      DROP FUNCTION public.unaccent_immutable;
+      DROP FUNCTION public.folio_unaccent;
+    SQL
+  end
+end
--- db/migrate/20200505100250_create_folio_unaccent_function.rb
+++ db/migrate/20200505100250_create_folio_unaccent_function.rb
@@ -1,26 +1,15 @@
 # frozen_string_literal: true
 
 class CreateFolioUnaccentFunction < ActiveRecord::Migration[5.2]
-  # creates immutable (and indexable) version of unaccent function
-  # https://stackoverflow.com/questions/11005036/does-postgresql-support-accent-insensitive-collations/11007216#11007216
   def up
-    execute <<~SQL
-      CREATE OR REPLACE FUNCTION public.unaccent_immutable(regdictionary, text)
-        RETURNS text LANGUAGE c IMMUTABLE PARALLEL SAFE STRICT AS
-      '$libdir/unaccent', 'unaccent_dict';
-
-      CREATE OR REPLACE FUNCTION public.folio_unaccent(text)
-        RETURNS text LANGUAGE sql IMMUTABLE PARALLEL SAFE STRICT AS
-      $func$
-      SELECT public.unaccent_immutable(regdictionary 'public.unaccent', $1)
-      $func$;
-    SQL
+    say_with_time 'create folio_unaccent function' do
+      create_folio_unaccent
+    end
   end
 
   def down
-    execute <<~SQL
-      DROP FUNCTION public.unaccent_immutable;
-      DROP FUNCTION public.folio_unaccent;
-    SQL
+    say_with_time 'drop folio_unaccent function' do
+      drop_folio_unaccent
+    end
   end
 end
