55555d242538988fd3d13907ac60a31712bbcdaf
dedekm <dedekmm@gmail.com>
2020-06-11 13:16:02 +0200

    guard

--- db/migrate/20200611101625_add_mailchimp_tags_to_folio_newsletter_subscriptions.rb
+++ db/migrate/20200611101625_add_mailchimp_tags_to_folio_newsletter_subscriptions.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddMailchimpTagsToFolioNewsletterSubscriptions < ActiveRecord::Migration[5.2]
   def change
     add_column :folio_newsletter_subscriptions, :mailchimp_tags, :string
