9a476e935904a586e5049ba2d7fc47fbc11a6876
Petr Marek <contact@petrmarek.eu>
2020-08-04 12:04:26 +0200

    cleanup assets

--- app/assets/config/folio_manifest.js
+++ app/assets/config/folio_manifest.js
@@ -1,2 +1,10 @@
-//= link_directory ../javascripts/folio .js
-//= link_directory ../stylesheets/folio .css
+//= link folio/console/react.css
+//= link folio/console/react.js
+
+//= link folio/mailer/base.css
+
+//= link folio/console/atoms/previews/previews.css
+//= link folio/console/atoms/previews/previews.js
+
+//= link folio/console/base.js
+//= link folio/console/base.css
--- test/dummy/app/assets/config/manifest.js
+++ test/dummy/app/assets/config/manifest.js
@@ -1,4 +1,4 @@
 //= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
-//= link folio_manifest.js
+
+//= link application.js
+//= link application.css
