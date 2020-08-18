9865ab69545d924df7dec37cbe500edfbc6afa15
Petr Marek <contact@petrmarek.eu>
2020-08-18 13:36:31 +0200

    fix rubocop autocorrect flag

--- lib/generators/folio/install_generator.rb
+++ lib/generators/folio/install_generator.rb
@@ -3,43 +3,45 @@
 module Folio
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      desc 'Creates folio initializer, routes and copies locale files to your application.'
+      desc "Creates folio initializer, routes and copies locale files to your application."
 
-      source_root Folio::Engine.root.join('lib/templates')
+      source_root Folio::Engine.root.join("lib/templates")
 
       def add_gems
-        gem 'dotenv-rails'
-        gem 'autoprefixer-rails', '9.8.5'
-        gem 'slim-rails'
-        gem 'cells'
-        gem 'cells-slim', '0.0.6'
-        gem 'cells-rails', '0.1.0'
-        gem 'route_translator'
-        gem 'breadcrumbs_on_rails'
-        gem 'sentry-raven'
-        gem 'pg', version: '~> 1.2.3'
-        gem 'devise-i18n'
-        gem 'rails-i18n'
-        gem 'mini_racer'
-        gem 'premailer', github: 'sinfin/premailer'
-        gem 'premailer-rails'
-        gem 'rubyzip'
-        gem 'rack-mini-profiler'
+        gem "turbolinks"
+        gem "dotenv-rails"
+        gem "autoprefixer-rails", "9.8.5"
+        gem "slim-rails"
+        gem "cells"
+        gem "cells-slim", "0.0.6"
+        gem "cells-rails", "0.1.0"
+        gem "route_translator"
+        gem "breadcrumbs_on_rails"
+        gem "sentry-raven"
+        gem "pg", version: "~> 1.2.3"
+        gem "devise-i18n"
+        gem "rails-i18n"
+        gem "mini_racer"
+        gem "premailer", github: "sinfin/premailer"
+        gem "premailer-rails"
+        gem "rubyzip"
+        gem "rack-mini-profiler"
+        gem "uglifier", ">= 1.3.0"
 
         gem_group :test do
-          gem 'factory_bot'
-          gem 'rack_session_access'
+          gem "factory_bot"
+          gem "rack_session_access"
         end
 
         gem_group :development, :test do
-          gem 'faker', require: false
+          gem "faker", require: false
         end
 
         gem_group :development do
-          gem 'rbnacl', version: '< 5.0'
-          gem 'rbnacl-libsodium'
-          gem 'bcrypt_pbkdf', version: '< 2.0'
-          gem 'ed25519'
+          gem "rbnacl", version: "< 5.0"
+          gem "rbnacl-libsodium"
+          gem "bcrypt_pbkdf", version: "< 2.0"
+          gem "ed25519"
 
           gem "rubocop"
           gem "rubocop-minitest"
@@ -47,30 +49,30 @@ module Folio
           gem "rubocop-rails"
           gem "rubocop-rails_config"
           gem "rubocop-rake"
-          gem 'annotate'
-          gem 'guard-rubocop'
-          gem 'guard-slimlint'
-          gem 'letter_opener'
-          gem 'pry-rails'
-          gem 'slack-notifier'
+          gem "annotate"
+          gem "guard-rubocop"
+          gem "guard-slimlint"
+          gem "letter_opener"
+          gem "pry-rails"
+          gem "slack-notifier"
 
-          gem 'capistrano-rails', require: false
-          gem 'capistrano-sinfin', git: 'git@bitbucket.org:Sinfin/capistrano-sinfin.git', branch: 'master'
-          gem 'capistrano-serviceman', github: 'Sinfin/capistrano-serviceman', branch: 'master'
+          gem "capistrano-rails", require: false
+          gem "capistrano-sinfin", git: "git@bitbucket.org:Sinfin/capistrano-sinfin.git", branch: "master"
+          gem "capistrano-serviceman", github: "Sinfin/capistrano-serviceman", branch: "master"
 
-          gem 'better_errors'
-          gem 'binding_of_caller'
-          gem 'rails-flog', require: 'flog'
+          gem "better_errors"
+          gem "binding_of_caller"
+          gem "rails-flog", require: "flog"
         end
       end
 
       def rm_rails_new_stuff
         [
-          'app/assets/stylesheets/application.css',
-          'app/views/layouts/application.html.erb',
-          'public/404.html',
-          'public/422.html',
-          'public/500.html',
+          "app/assets/stylesheets/application.css",
+          "app/views/layouts/application.html.erb",
+          "public/404.html",
+          "public/422.html",
+          "public/500.html",
         ].each do |path|
           full_path = Rails.root.join(path)
           ::File.delete(full_path) if ::File.exist?(full_path)
@@ -79,88 +81,88 @@ module Folio
 
       def copy_templates
         [
-          '.env.sample',
-          'app/views/layouts/folio/application.slim',
-          'bin/sprites',
-          'config/database.yml',
-          'config/locales/activerecord.cs.yml',
-          'config/locales/activerecord.en.yml',
-          'config/schedule.rb',
-          'config/sitemap.rb',
-          'db/seeds.rb',
-          'vendor/assets/bower.json',
+          ".env.sample",
+          "app/views/layouts/folio/application.slim",
+          "bin/sprites",
+          "config/database.yml",
+          "config/locales/activerecord.cs.yml",
+          "config/locales/activerecord.en.yml",
+          "config/schedule.rb",
+          "config/sitemap.rb",
+          "db/seeds.rb",
+          "vendor/assets/bower.json",
         ].each { |f| template "#{f}.erb", f }
 
         [
-          'app/controllers/anti_cache_controller.rb',
-          'app/controllers/application_controller.rb',
-          'app/controllers/errors_controller.rb',
-          'app/controllers/home_controller.rb',
-          'app/controllers/pages_controller.rb',
-          'app/lib/application_cell.rb',
-          'app/models/application_record.rb',
-          'config/initializers/assets.rb',
-          'config/initializers/raven.rb',
-          'config/initializers/smtp.rb',
-          'config/routes.rb',
-          'test/controllers/anti_cache_controller_test.rb',
-          'test/factories.rb',
-          'test/test_helper.rb',
+          "app/controllers/anti_cache_controller.rb",
+          "app/controllers/application_controller.rb",
+          "app/controllers/errors_controller.rb",
+          "app/controllers/home_controller.rb",
+          "app/controllers/pages_controller.rb",
+          "app/lib/application_cell.rb",
+          "app/models/application_record.rb",
+          "config/initializers/assets.rb",
+          "config/initializers/raven.rb",
+          "config/initializers/smtp.rb",
+          "config/routes.rb",
+          "test/controllers/anti_cache_controller_test.rb",
+          "test/factories.rb",
+          "test/test_helper.rb",
         ].each { |f| template "#{f}.tt", f }
 
-        template '.env.sample.erb', '.env'
+        template ".env.sample.erb", ".env"
       end
 
       def copy_files
         [
-          '.gitignore',
-          '.rubocop.yml',
-          '.slim-lint.yml',
-          'app/assets/config/manifest.js',
-          'app/assets/images/sprites@1x/.keep',
-          'app/assets/images/sprites@2x/.keep',
-          'app/assets/javascripts/application.js',
-          'app/assets/javascripts/folio/console/main_app.coffee',
-          'app/assets/stylesheets/_cells.scss.erb',
-          'app/assets/stylesheets/_custom_bootstrap.sass',
-          'app/assets/stylesheets/_fonts.scss',
-          'app/assets/stylesheets/_print.sass',
-          'app/assets/stylesheets/_sprites.scss',
-          'app/assets/stylesheets/_variables.sass',
-          'app/assets/stylesheets/application.sass',
-          'app/assets/stylesheets/folio/console/_main_app.sass',
-          'app/assets/stylesheets/modules/.keep',
-          'app/assets/stylesheets/modules/_bootstrap-overrides.sass',
-          'app/assets/stylesheets/modules/_turbolinks.sass',
-          'app/assets/stylesheets/modules/bootstrap-overrides/_type.sass',
-          'app/assets/stylesheets/modules/bootstrap-overrides/mixins/_type.sass',
-          'app/cells/folio/console/atoms/previews/main_app.coffee',
-          'app/views/devise/mailer/invitation_instructions.html.erb',
-          'app/views/devise/mailer/invitation_instructions.text.erb',
-          'app/views/devise/mailer/reset_password_instructions.html.erb',
-          'app/views/folio/console/partials/_appended_menu_items.slim',
-          'app/views/folio/console/partials/_prepended_menu_items.slim',
-          'app/views/folio/pages/show.slim',
-          'app/views/home/index.slim',
-          'bin/bower',
-          'config/secrets.yml',
-          'Guardfile',
-          'lib/tasks/auto_annotate_models.rake',
-          'vendor/assets/bower_components/.keep',
-          'vendor/assets/redactor/redactor.css',
-          'vendor/assets/redactor/redactor.js',
+          ".gitignore",
+          ".rubocop.yml",
+          ".slim-lint.yml",
+          "app/assets/config/manifest.js",
+          "app/assets/images/sprites@1x/.keep",
+          "app/assets/images/sprites@2x/.keep",
+          "app/assets/javascripts/application.js",
+          "app/assets/javascripts/folio/console/main_app.coffee",
+          "app/assets/stylesheets/_cells.scss.erb",
+          "app/assets/stylesheets/_custom_bootstrap.sass",
+          "app/assets/stylesheets/_fonts.scss",
+          "app/assets/stylesheets/_print.sass",
+          "app/assets/stylesheets/_sprites.scss",
+          "app/assets/stylesheets/_variables.sass",
+          "app/assets/stylesheets/application.sass",
+          "app/assets/stylesheets/folio/console/_main_app.sass",
+          "app/assets/stylesheets/modules/.keep",
+          "app/assets/stylesheets/modules/_bootstrap-overrides.sass",
+          "app/assets/stylesheets/modules/_turbolinks.sass",
+          "app/assets/stylesheets/modules/bootstrap-overrides/_type.sass",
+          "app/assets/stylesheets/modules/bootstrap-overrides/mixins/_type.sass",
+          "app/cells/folio/console/atoms/previews/main_app.coffee",
+          "app/views/devise/mailer/invitation_instructions.html.erb",
+          "app/views/devise/mailer/invitation_instructions.text.erb",
+          "app/views/devise/mailer/reset_password_instructions.html.erb",
+          "app/views/folio/console/partials/_appended_menu_items.slim",
+          "app/views/folio/console/partials/_prepended_menu_items.slim",
+          "app/views/folio/pages/show.slim",
+          "app/views/home/index.slim",
+          "bin/bower",
+          "config/secrets.yml",
+          "Guardfile",
+          "lib/tasks/auto_annotate_models.rake",
+          "vendor/assets/bower_components/.keep",
+          "vendor/assets/redactor/redactor.css",
+          "vendor/assets/redactor/redactor.js",
         ].each { |f| copy_file f, f }
 
-        copy_file Folio::Engine.root.join('.ruby-version'), '.ruby-version'
+        copy_file Folio::Engine.root.join(".ruby-version"), ".ruby-version"
       end
 
       def application_settings
-        return if ::File.readlines(Rails.root.join('config/application.rb')).grep("Rails.root.join('lib')").any?
+        return if ::File.readlines(Rails.root.join("config/application.rb")).grep("Rails.root.join("lib")").any?
 
-        inject_into_file 'config/application.rb', after: /config\.load_defaults.+\n/ do <<-'RUBY'
+        inject_into_file "config/application.rb", after: /config\.load_defaults.+\n/ do <<-"RUBY"
     config.exceptions_app = self.routes
 
-    config.time_zone = 'Prague'
+    config.time_zone = "Prague"
 
     config.generators do |g|
       g.stylesheets false
@@ -173,15 +175,15 @@ module Folio
     I18n.default_locale = :cs
 
     config.folio_console_locale = I18n.default_locale
-    config.autoload_paths << Rails.root.join('app/lib')
-    config.eager_load_paths << Rails.root.join('app/lib')
+    config.autoload_paths << Rails.root.join("app/lib")
+    config.eager_load_paths << Rails.root.join("app/lib")
 
     Rails.autoloaders.main.ignore("#{::Folio::Engine.root}/app/lib/folio/console/simple_form_components")
     Rails.autoloaders.main.ignore("#{::Folio::Engine.root}/app/lib/folio/console/simple_form_inputs")
 
     overrides = [
-      Folio::Engine.root.join('app/overrides').to_s,
-      Rails.root.join('app/overrides').to_s,
+      Folio::Engine.root.join("app/overrides").to_s,
+      Rails.root.join("app/overrides").to_s,
     ]
 
     overrides.each { |override| Rails.autoloaders.main.ignore(override) }
@@ -199,35 +201,35 @@ module Folio
       end
 
       def development_settings
-        gsub_file 'config/environments/development.rb', /# Don't care if the mailer can't send.*\n/, ''
+        gsub_file "config/environments/development.rb", /# Don"t care if the mailer can"t send.*\n/, "
 
-        gsub_file 'config/environments/development.rb', /  config\.action_mailer\.raise_delivery_errors = false/ do
+        gsub_file "config/environments/development.rb", /  config\.action_mailer\.raise_delivery_errors = false/ do
           [
-            'config.action_mailer.raise_delivery_errors = true',
-            'config.action_mailer.delivery_method = :letter_opener',
-            'config.action_mailer.perform_deliveries = true',
+            "config.action_mailer.raise_delivery_errors = true",
+            "config.action_mailer.delivery_method = :letter_opener",
+            "config.action_mailer.perform_deliveries = true",
           ].join("\n  ")
         end
       end
 
       def test_settings
-        inject_into_file 'config/environments/test.rb', before: '# Raises error for missing translations' do
+        inject_into_file "config/environments/test.rb", before: "# Raises error for missing translations" do
           "config.middleware.use RackSessionAccess::Middleware\n\n  "
         end
       end
 
       def production_settings
-        gsub_file 'config/environments/production.rb', 'config.assets.js_compressor = :uglifier', 'config.assets.js_compressor = Folio::SelectiveUglifier.new(harmony: false) # change to true to use es6'
+        gsub_file "config/environments/production.rb", "config.assets.js_compressor = :uglifier", "config.assets.js_compressor = Folio::SelectiveUglifier.new(harmony: false) # change to true to use es6"
       end
 
       def setup_routes
-        route "mount Folio::Engine => '/'"
+        route "mount Folio::Engine => "/""
       end
 
       def chmod_files
         [
-          'bin/bower',
-          'bin/sprites'
+          "bin/bower",
+          "bin/sprites"
         ].each do |file|
           ::File.chmod(0775, Rails.root.join(file))
         end
