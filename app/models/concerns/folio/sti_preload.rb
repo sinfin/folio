b60e6756d1e7cdd9498b962bffe5971cec9076a5
Petr Marek <contact@petrmarek.eu>
2020-10-08 09:55:03 +0200

    extend Folio::StiPreload concern

--- app/models/concerns/folio/sti_preload.rb
+++ app/models/concerns/folio/sti_preload.rb
@@ -13,22 +13,34 @@ module Folio::StiPreload
         []
       end
 
+      def sti_file_paths
+        []
+      end
+
       def descendants
         preload_sti unless sti_preloaded
         super
       end
 
       def preload_sti
+        preload_path = -> (path) do
+          relative_path = path.sub(/.*\/app\/models\//, "").delete_suffix(".rb")
+          classified = "#{relative_path}/X".classify.delete_suffix("::X")
+          if classified.safe_constantize
+            logger.debug("Preloading STI type #{classified}")
+          else
+            logger.error("Failed to preload STI file: #{path}")
+          end
+        end
+
         sti_paths.each do |sti_path|
           Dir["#{sti_path}/**/*.rb"].each do |path|
-            relative_path = path.sub(/.*\/app\/models\//, "").delete_suffix(".rb")
-            classified = "#{relative_path}/X".classify.delete_suffix("::X")
-            if classified.safe_constantize
-              logger.debug("Preloading STI type #{classified}")
-            else
-              logger.error("Failed to preload STI file: #{path}")
-            end
+            preload_path.call(path)
           end
+        end
+
+        sti_file_paths.each do |sti_file_path|
+          preload_path.call(sti_file_path)
         end
 
         self.sti_preloaded = true
