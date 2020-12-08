1687df03e873db494886a84f1113c0a06058dca6
Petr Marek <contact@petrmarek.eu>
2020-12-08 14:14:18 +0100

    add lead mailer test

--- test/mailers/folio/lead_mailer_test.rb
+++ test/mailers/folio/lead_mailer_test.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "generators/folio/email_templates/email_templates_generator"
 
 class Folio::LeadMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    create(:folio_site)
+    Folio::EmailTemplatesGenerator.new.seed_records
+  end
+
+  test "notification_email" do
+    lead = create(:folio_lead, note: "Foo Bar")
+
+    mail = Folio::LeadMailer.notification_email(lead)
+    assert_equal [Folio::Site.instance.email], mail.to
+    assert_match "Foo Bar", mail.body.encoded
+  end
 end
