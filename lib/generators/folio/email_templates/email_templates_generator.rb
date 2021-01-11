deb8097a2a8f3ef06ebed1e1fbfe6b6a28bd6d78
Petr Marek <contact@petrmarek.eu>
2021-01-11 14:36:04 +0100

    allow destroy in email_templates yml

--- lib/generators/folio/email_templates/email_templates_generator.rb
+++ lib/generators/folio/email_templates/email_templates_generator.rb
@@ -5,40 +5,49 @@ class Folio::EmailTemplatesGenerator < Rails::Generators::Base
   source_root File.expand_path("templates", __dir__)
 
   def seed_records
+    records = []
+
     [
-      Rails.root.join("data/email_templates_data.yml"),
       File.expand_path("templates/email_templates_data.yml", __dir__),
+      Rails.root.join("data/email_templates_data.yml"),
     ].each do |path|
       next unless File.exist?(path)
       yaml_data = YAML.load_file(path)
       next unless yaml_data
+      records += yaml_data
+    end
 
-      yaml_data.each do |raw|
-        msg_action = "Adding"
-        if em = Folio::EmailTemplate.find_by(mailer: raw["mailer"], action: raw["action"])
-          if ENV["FORCE"]
-            msg_action = "Overwriting (FORCE=1)"
-            em.destroy!
-          else
-            unless Rails.env.test?
-              puts "Skipping existing email template for #{raw["mailer"]}##{raw["action"]}"
-            end
-            next
+    records.each do |raw|
+      msg_action = "Adding"
+
+      if em = Folio::EmailTemplate.find_by(mailer: raw["mailer"], action: raw["action"])
+        if raw["destroy"]
+          msg_action = "Destroying"
+          em.destroy!
+        elsif ENV["FORCE"]
+          msg_action = "Overwriting (FORCE=1)"
+          em.destroy!
+        else
+          unless Rails.env.test?
+            puts "Skipping existing email template for #{raw["mailer"]}##{raw["action"]}"
           end
+          next
         end
-
-        unless Rails.env.test?
-          puts "#{msg_action} email template for #{raw["mailer"]}##{raw["action"]}"
-        end
-
-        data = raw.slice(*Folio::EmailTemplate.column_names)
-
-        default_locale = Rails.application.config.folio_console_locale
-        data["title"] = raw["title_#{default_locale}"].presence
-        data["title"] ||= data["title_en"]
-
-        Folio::EmailTemplate.create!(data)
       end
+
+      unless Rails.env.test?
+        puts "#{msg_action} email template for #{raw["mailer"]}##{raw["action"]}"
+      end
+
+      next if raw["destroy"]
+
+      data = raw.slice(*Folio::EmailTemplate.column_names)
+
+      default_locale = Rails.application.config.folio_console_locale
+      data["title"] = raw["title_#{default_locale}"].presence
+      data["title"] ||= data["title_en"]
+
+      Folio::EmailTemplate.create!(data)
     end
   end
 end
