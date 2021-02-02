3ae188188e3529c79a01223aeba8a1e6bfc6a8ba
Petr Marek <contact@petrmarek.eu>
2021-02-02 12:39:10 +0100

    update rescue_responses for ParameterMissing

--- config/initializers/rescue_responses.rb
+++ config/initializers/rescue_responses.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
+Rails.configuration.action_dispatch.rescue_responses["ActionController::ParameterMissing"] = :bad_request
 Rails.configuration.action_dispatch.rescue_responses["CanCan::AccessDenied"] = :unauthorized
