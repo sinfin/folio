57328d8a0cc85f80374d116a4f117632ced10790
dedekm <dedekmm@gmail.com>
2021-02-09 14:16:54 +0100

    fix migrations

--- db/migrate/20180910082930_rm_site_relation.rb
+++ db/migrate/20180910082930_rm_site_relation.rb
@@ -4,7 +4,7 @@ class RmSiteRelation < ActiveRecord::Migration[5.2]
   def change
     remove_reference :folio_nodes, :site
 
-    if column_exists?(:visits, :site_id)
+    if table_exists?(:visits) && column_exists?(:visits, :site_id)
       remove_reference :visits, :site
     end
   end
