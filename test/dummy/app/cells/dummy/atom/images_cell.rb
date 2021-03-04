ca142e3150a70382da2053ca6d323365ec14be11
Petr Marek <contact@petrmarek.eu>
2021-03-04 13:03:40 +0100

    update images prepared atom

--- test/dummy/app/cells/dummy/atom/images_cell.rb
+++ test/dummy/app/cells/dummy/atom/images_cell.rb
@@ -1,44 +1,28 @@
 # frozen_string_literal: true
 
 class Dummy::Atom::ImagesCell < ApplicationCell
-  DYNAMIC_TARGET_HEIGHT = 250
-  DYNAMIC_THUMB_HEIGHT = 500
+  DYNAMIC_TARGET_HEIGHT = 260
+  DYNAMIC_THUMB_MAX_WIDTH = 600
+  DYNAMIC_THUMB_MAX_HEIGHT = 400
+  DYNAMIC_MARGIN = 15
+
+  GRID_SIZE = 266
 
   def show
     render if model && model.image_placements.present?
   end
 
-  def col_number
-    @col_number ||= case model.image_placements.size
-                    when 1
-                      6
-                    when 2
-                      6
-                    else
-                      4
-    end
+  def dynamic_geometry
+    @dynamic_geometry ||= "#{DYNAMIC_THUMB_MAX_WIDTH}x#{DYNAMIC_THUMB_MAX_HEIGHT}>"
   end
 
-  def grid_width
-    @grid_width ||= case model.image_placements.size
-                    when 1
-                      540
-                    when 2
-                      540
-                    else
-                      350
-    end
-  end
+  def dynamic_item_data(placement)
+    t = placement.file.thumb(dynamic_geometry)
+    ratio = t.width.to_f / t.height
 
-  def dynamic_style(placement)
-    file = placement.file
-    width = DYNAMIC_TARGET_HEIGHT * file.file_width / file.file_height
-    max_width = DYNAMIC_THUMB_HEIGHT * file.file_width / file.file.height
-
-    "width: #{width}px; flex-grow: #{width}; max-width: #{max_width}px"
-  end
-
-  def dynamic_max_width(placement)
-    nil
+    {
+      ratio: ratio,
+      width: (DYNAMIC_TARGET_HEIGHT.to_f * ratio).round,
+    }
   end
 end
