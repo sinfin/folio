74251fe5d828d27a446258538411b452c1598a35
Petr Marek <contact@petrmarek.eu>
2021-03-24 10:50:25 +0100

    fix ui/icon cell test

--- test/dummy/test/cells/dummy/ui/icon_cell_test.rb
+++ test/dummy/test/cells/dummy/ui/icon_cell_test.rb
@@ -4,7 +4,7 @@ require "test_helper"
 
 class Dummy::Ui::IconCellTest < Cell::TestCase
   test "show" do
-    html = cell("dummy/ui/icon", "fast_forward").(:show)
+    html = cell("dummy/ui/icon", :fast_forward).(:show)
     assert html.has_css?(".d-ui-icon")
     assert html.has_css?(".d-ui-icon--fast_forward")
   end
