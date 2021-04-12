3ad44757c194e77f428cd839b24914468924bc4b
Petr Marek <contact@petrmarek.eu>
2021-04-12 09:30:26 +0200

    add ui/share and ui/clipboard cells, ui/share atom

--- test/dummy/app/assets/javascripts/application.js
+++ test/dummy/app/assets/javascripts/application.js
@@ -3,6 +3,7 @@
 //= require turbolinks
 //= require justified-layout
 //= require slick-carousel/slick/slick.min
+//= require clipboard/dist/clipboard
 
 //= require folio-bootstrap/js/dist/util
 //= require folio-bootstrap/js/dist/modal
@@ -16,8 +17,10 @@
 //= require folio/image/image
 //= require folio/newsletter_subscriptions/form/form
 
+//= require dummy/ui/clipboard/clipboard
 //= require dummy/ui/flash/flash
 //= require dummy/ui/menu/menu
 //= require dummy/ui/navigation/navigation
 
 //= require dummy/molecule/hero/hero
+
---
+++ test/dummy/app/cells/dummy/ui/clipboard_cell.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class Dummy::Ui::ClipboardCell < ApplicationCell
+end
