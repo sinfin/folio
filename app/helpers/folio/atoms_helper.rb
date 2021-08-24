f84c97896cb1ed28e7dd840dbd7c2b35ec46d81c
Petr Marek <contact@petrmarek.eu>
2021-08-24 07:26:11 +0200

    allow to pass cell_options to atoms helpers

--- app/helpers/folio/atoms_helper.rb
+++ app/helpers/folio/atoms_helper.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 
 module Folio::AtomsHelper
-  def render_atoms(atoms, only: [], except: [])
+  def render_atoms(atoms, only: [], except: [], cell_options: {})
     atoms.map do |atom|
       next if only.present? && !only.include?(atom.class)
       next if except.present? && except.include?(atom.class)
 
       if atom.class.cell_name
-        cell(atom.class.cell_name, atom, atom.cell_options.presence || {})
+        cell(atom.class.cell_name,
+             atom,
+             atom.cell_options.present? ? atom.cell_options.merge(cell_options) : cell_options)
       else
         render "folio/atoms/#{atom.partial_name}", data: atom
       end
     end.compact.join("").html_safe
   end
 
-  def render_atoms_in_molecules(atoms_in_molecules, only: [], except: [])
+  def render_atoms_in_molecules(atoms_in_molecules, only: [], except: [], cell_options: {})
     atoms_in_molecules.map do |molecule, atoms|
       if only.present?
         atoms = atoms.select { |a| only.include?(a.class) }
@@ -30,11 +32,12 @@ module Folio::AtomsHelper
         if atoms.present?
           if molecule.is_a?(String)
             cell(molecule,
-                 atoms)
+                 atoms,
+                 cell_options)
           else
             cell(molecule.cell_name,
                  atoms,
-                 molecule.cell_options.presence || {})
+                 molecule.cell_options.present? ? molecule.cell_options.merge(cell_options) : cell_options)
           end
         end
       else
