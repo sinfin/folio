1d9e94a80deaf47153b29ba2b2e9358d069b687e
Petr Marek <contact@petrmarek.eu>
2021-09-07 09:34:21 +0200

    add searches#show form

--- test/dummy/app/cells/dummy/searches/show_cell.rb
+++ test/dummy/app/cells/dummy/searches/show_cell.rb
@@ -2,4 +2,26 @@
 
 class Dummy::Searches::ShowCell < ApplicationCell
   include Pagy::Frontend
+  include SimpleForm::ActionViewExtensions::FormHelper
+
+  def form(&block)
+    opts = {
+      url: controller.main_app.dummy_search_path,
+      method: :get,
+      html: { class: "d-searches-show__form h1" },
+    }
+
+    simple_form_for("", opts, &block)
+  end
+
+  def query_input(f)
+    f.input :q,
+            input_html: {
+              class: "d-searches-show__input",
+              value: params[:q],
+              placeholder: t(".placeholder")
+            },
+            label: false,
+            wrapper: false
+  end
 end
