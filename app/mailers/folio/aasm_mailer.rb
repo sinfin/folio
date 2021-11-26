265a2260e607ddc578430fa702275944def99c16
Petr Marek <contact@petrmarek.eu>
2021-11-26 21:20:48 +0100

    tweak aasm mailer config - add reply_to and from

--- app/mailers/folio/aasm_mailer.rb
+++ app/mailers/folio/aasm_mailer.rb
@@ -8,10 +8,20 @@ class Folio::AasmMailer < Folio::ApplicationMailer
 
     mail to: email,
          subject: subject,
-         bcc: self.class.bcc_email
+         bcc: self.class.bcc_email,
+         reply_to: self.class.reply_to_email,
+         from: self.class.from_email
   end
 
   def self.bcc_email
-    Rails.application.config.folio_aasm_mailer_bcc
+    Rails.application.config.folio_aasm_mailer_config.try(:[], :bcc)
+  end
+
+  def self.reply_to_email
+    Rails.application.config.folio_aasm_mailer_config.try(:[], :reply_to)
+  end
+
+  def self.from_email
+    Rails.application.config.folio_aasm_mailer_config.try(:[], :from) || Folio::Site.instance.email_from.presence || Folio::Site.instance.email
   end
 end
