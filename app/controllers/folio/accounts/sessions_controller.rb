20cb1663e67503ad172f71157ba34aea222097a8
Petr Marek <contact@petrmarek.eu>
2021-12-06 07:24:26 +0100

    use stored_location_for in accounts/sessions#create

--- app/controllers/folio/accounts/sessions_controller.rb
+++ app/controllers/folio/accounts/sessions_controller.rb
@@ -4,7 +4,7 @@ class Folio::Accounts::SessionsController < Devise::SessionsController
   layout "folio/console/devise"
 
   def after_sign_in_path_for(_resource)
-    console_root_path
+    stored_location_for(:account).presence || console_root_path
   end
 
   def after_sign_out_path_for(_resource)
