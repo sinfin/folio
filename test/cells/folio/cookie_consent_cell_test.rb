2ae4cc63a276128a7ac4e04463b284149e3c35ca
Petr Marek <contact@petrmarek.eu>
2021-12-17 07:55:26 +0100

    fix cookie consent cell test

--- test/cells/folio/cookie_consent_cell_test.rb
+++ test/cells/folio/cookie_consent_cell_test.rb
@@ -4,13 +4,7 @@ require "test_helper"
 
 class Folio::CookieConsentCellTest < Cell::TestCase
   test "show" do
-    model = {
-      create_url: "#",
-      destroy_url: "#",
-      index_url: "#",
-      param_name: "#",
-    }
-    html = cell("folio/dropzone", model).(:show)
-    assert html.has_css?(".f-dropzone")
+    html = cell("folio/cookie_consent", "/").(:show)
+    assert html
   end
 end
