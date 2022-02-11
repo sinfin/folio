210000fc349c3838754f030bf84b7a232e2fd669
Petr Marek <contact@petrmarek.eu>
2022-02-11 09:42:54 +0100

    add :domain support, months and NID to cookie consent

--- app/cells/folio/cookie_consent_cell.rb
+++ app/cells/folio/cookie_consent_cell.rb
@@ -90,6 +90,14 @@ class Folio::CookieConsentCell < Folio::ApplicationCell
           en: "Used to distinguish users.",
         }
       },
+      NID: {
+        expiration: [6, :months],
+        domain: ".google.com",
+        description: {
+          cs: "Udržuje uživatelská nastavení.",
+          en: "Stores user preferences.",
+        }
+      },
     }
   end
 
@@ -228,7 +236,7 @@ class Folio::CookieConsentCell < Folio::ApplicationCell
 
     {
       col1: h[:name] || key.to_s,
-      col2: model,
+      col2: h[:domain].presence || model,
       col3: h[:expiration] == :end_of_session ? t(".expiration.end_of_session") : t(".expiration.#{h[:expiration][1]}", count: h[:expiration][0]),
       col4: h[:description][I18n.locale] || h[:description][:en],
       is_regex: h[:is_regex] == true,
@@ -238,7 +246,7 @@ class Folio::CookieConsentCell < Folio::ApplicationCell
   def cookie_setting_for_hash(h)
     {
       col1: h[:name],
-      col2: model,
+      col2: h[:domain].presence || model,
       col3: h[:expiration] == :end_of_session ? t(".expiration.end_of_session") : t(".expiration.#{h[:expiration][1]}", count: h[:expiration][0]),
       col4: h[:description][I18n.locale] || h[:description][:en],
       is_regex: h[:is_regex] == true,
