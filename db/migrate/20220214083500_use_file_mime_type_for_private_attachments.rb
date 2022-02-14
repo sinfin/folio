f1a45b327216aa17b03704b1eb8485f592a3c061
Petr Marek <contact@petrmarek.eu>
2022-02-14 09:53:29 +0100

    run guard

--- db/migrate/20220214083500_use_file_mime_type_for_private_attachments.rb
+++ db/migrate/20220214083500_use_file_mime_type_for_private_attachments.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class UseFileMimeTypeForPrivateAttachments < ActiveRecord::Migration[6.1]
   def change
     add_column :folio_private_attachments, :file_mime_type, :string
--- test/dummy/db/migrate/20220214083648_rm_private_attachments_mime_type_column.rb
+++ test/dummy/db/migrate/20220214083648_rm_private_attachments_mime_type_column.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RmPrivateAttachmentsMimeTypeColumn < ActiveRecord::Migration[6.1]
   def change
     remove_column :folio_private_attachments, :mime_type, :string
