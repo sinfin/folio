be4522d6f85f5566594361c3e653a821f1f5fe82
Petr Marek <contact@petrmarek.eu>
2022-02-24 07:36:08 +0100

    update newsletter_subscriptions/form to use csrf js

---
+++ app/cells/folio/newsletter_subscriptions/form/form.js
@@ -0,0 +1,22 @@
+//= require folio/csrf
+
+$(document).on('submit', '.f-newsletter-subscriptions-form__form', function (e) {
+  e.preventDefault()
+
+  const $form = $(this)
+
+  if ($form.hasClass('f-newsletter-subscriptions-form--submitting')) return
+
+  const $wrap = $form.closest('.f-newsletter-subscriptions-form')
+  $form.addClass('f-newsletter-subscriptions-form-submitting')
+
+  window.Folio.Csrf.withToken(() => {
+    $form
+      .find(`input[name="${window.Folio.Csrf.tokenParam}"]`)
+      .val(window.Folio.Csrf.token)
+
+    $.post($form.attr('action'), $form.serialize()).always((response) => {
+      $wrap.replaceWith(response)
+    })
+  })
+})
