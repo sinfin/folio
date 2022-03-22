5a617d28771d9aa4d9035a5f0b673ca7f06450c8
Petr Marek <contact@petrmarek.eu>
2022-03-22 13:43:38 +0100

    don't hide notes that are being editted

--- react/src/containers/NotesFieldsApp/Table/index.js
+++ react/src/containers/NotesFieldsApp/Table/index.js
@@ -14,36 +14,39 @@ export default function Table ({
   return (
     <div className='f-c-r-notes-fields-app-table'>
       {notesForTable.map((note) => (
-        currentlyEdittingUniqueId && currentlyEdittingUniqueId === note.uniqueId ? null : (
-          <div className='f-c-r-notes-fields-app-table__row' key={note.uniqueId}>
-            <input
-              type='checkbox'
-              className='f-c-r-notes-fields-app-table__checkbox mr-3'
-              checked={note.attributes.closed_at !== null}
-              onChange={() => toggleClosedAt(note)}
-            />
+        <div
+          className={`f-c-r-notes-fields-app-table__row ${currentlyEdittingUniqueId && currentlyEdittingUniqueId === note.uniqueId ? 'f-c-r-notes-fields-app-table__row--being-editted' : ''}`}
+          key={note.uniqueId}
+        >
+          <input
+            type='checkbox'
+            className='f-c-r-notes-fields-app-table__checkbox mr-3'
+            checked={note.attributes.closed_at !== null}
+            onChange={() => toggleClosedAt(note)}
+          />
 
-            <div
-              className='f-c-r-notes-fields-app-table__content mr-3'
-              dangerouslySetInnerHTML={{ __html: note.attributes.content.replace(/\n/g, '<br>') }}
-              onClick={() => editNote(note)}
-            />
+          <div
+            className='f-c-r-notes-fields-app-table__content mr-3'
+            dangerouslySetInnerHTML={{ __html: note.attributes.content.replace(/\n/g, '<br>') }}
+            onClick={() => editNote(note)}
+          />
 
-            {currentlyEditting ? null : (
-              <React.Fragment>
-                <DueDate
-                  className='f-c-r-notes-fields-app-table__action mr-2'
-                  dueAt={note.attributes.due_at}
-                  onChange={(dueAt) => changeDueDate(note, dueAt)}
-                />
+          <DueDate
+            className='f-c-r-notes-fields-app-table__action mr-2'
+            dueAt={note.attributes.due_at}
+            onChange={(dueAt) => changeDueDate(note, dueAt)}
+          />
 
-                <span className='f-c-r-notes-fields-app-table__action fa fa--12 fa-edit mr-2' onClick={() => editNote(note)} />
+          <span
+            className='f-c-r-notes-fields-app-table__action fa fa--12 fa-edit mr-2'
+            onClick={() => editNote(note)}
+          />
 
-                <span className='f-c-r-notes-fields-app-table__action fa fa-times text-danger' onClick={() => removeNote(note)} />
-              </React.Fragment>
-            )}
-          </div>
-        )
+          <span
+            className='f-c-r-notes-fields-app-table__action fa fa-times text-danger'
+            onClick={() => removeNote(note)}
+          />
+        </div>
       ))}
     </div>
   )
