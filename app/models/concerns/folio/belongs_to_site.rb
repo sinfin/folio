16a7e5071dd66827d6e0e429d4619117da8cc9b9
Petr Marek <contact@petrmarek.eu>
2022-03-25 07:00:31 +0100

    fix validate_belongs_to_site

--- app/models/concerns/folio/belongs_to_site.rb
+++ app/models/concerns/folio/belongs_to_site.rb
@@ -7,13 +7,13 @@ module Folio::BelongsToSite
     belongs_to :site, class_name: "Folio::Site",
                       required: false
 
-    if Rails.application.config.folio_site_is_a_singleton
-      validate :validate_belongs_to_site
-    end
+    validate :validate_belongs_to_site
   end
 
   private
     def validate_belongs_to_site
+      return if Rails.application.config.folio_site_is_a_singleton
+
       return errors.add(:site, :blank) if site.nil?
 
       if Rails.application.config.folio_site_validate_belongs_to_namespace
