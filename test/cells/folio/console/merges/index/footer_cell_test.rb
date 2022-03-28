8b51334cd2a48625d1d14ef5b744da6abf78908f
Petr Marek <contact@petrmarek.eu>
2022-03-28 10:04:11 +0200

    rake dummy:seed_generators:all

--- test/cells/folio/console/merges/index/footer_cell_test.rb
+++ test/cells/folio/console/merges/index/footer_cell_test.rb
@@ -4,6 +4,8 @@ require "test_helper"
 
 class Folio::Console::Merges::Index::FooterCellTest < Folio::Console::CellTest
   test "show" do
+    create_and_host_site
+
     html = cell("folio/console/merges/index/footer", Folio::Page).(:show)
     assert html.has_css?(".f-c-merges-index-footer")
   end
--- test/dummy/test/cells/dummy/ui/header_cell_test.rb
+++ test/dummy/test/cells/dummy/ui/header_cell_test.rb
@@ -4,10 +4,12 @@ require "test_helper"
 
 class Dummy::Ui::HeaderCellTest < Cell::TestCase
   test "show" do
+    create_and_host_site
+
     html = cell("dummy/ui/header", nil).(:show)
     assert html.has_css?(".d-ui-header")
 
-    Dummy::Menu::Header.create!(locale: :en)
+    Dummy::Menu::Header.create!(locale: :en, site: @site)
     html = cell("dummy/ui/header", nil).(:show)
     assert html.has_css?(".d-ui-header")
   end
