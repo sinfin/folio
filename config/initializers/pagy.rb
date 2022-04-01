a790d184a928f66a33e1b0fad9e63c83260e4b91
Petr Marek <contact@petrmarek.eu>
2022-04-01 10:57:44 +0200

    add pagy/extras/trim to skip page=1 param

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
+require "pagy/extras/bootstrap"
 require "pagy/extras/i18n"
 require "pagy/extras/overflow"
-require "pagy/extras/bootstrap"
+require "pagy/extras/trim"
 
 Pagy::DEFAULT[:overflow] = :last_page
 Pagy::DEFAULT[:items] = 50
