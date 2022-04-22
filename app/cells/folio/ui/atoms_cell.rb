4888877f322e541b1db1c124ff1793325ac9c7bc
Petr Marek <contact@petrmarek.eu>
2022-04-22 11:36:37 +0200

    add _placement_type support to atoms showcase

--- app/cells/folio/ui/atoms_cell.rb
+++ app/cells/folio/ui/atoms_cell.rb
@@ -41,88 +41,98 @@ class Folio::Ui::AtomsCell < Folio::ApplicationCell
     end
   end
 
-  def page
-    require "faker"
+  def placements
+    @placements ||= begin
+      h = {
+        nil: Folio::Page.new,
+      }
 
-    @page = Folio::Page.new
+      require "faker"
 
-    sorted_yaml = YAML.load_file(self.class.data_path).sort_by do |data|
-      klass = data["type"].constantize
-      I18n.transliterate(klass.model_name.human)
-    end.sort_by do |data|
-      klass = data["type"].constantize
-      klass.console_insert_row
-    end
+      sorted_yaml = YAML.load_file(self.class.data_path).sort_by do |data|
+        klass = data["type"].constantize
+        I18n.transliterate(klass.model_name.human)
+      end.sort_by do |data|
+        klass = data["type"].constantize
+        klass.console_insert_row
+      end
 
-    sorted_yaml.each do |data|
-      attrs = data.dup
+      sorted_yaml.each do |data|
+        attrs = data.dup
 
-      attrs = handle_attributes(attrs)
+        attrs = handle_attributes(attrs)
 
-      attrs.delete("_showcase")
-      molecule = attrs.delete("_molecule").presence || 1
+        attrs.delete("_showcase")
+        attrs.delete("_placement_type")
+        molecule = attrs.delete("_molecule").presence || 1
 
-      if attrs["cover"]
-        if attrs["cover"].is_a?(Numeric)
-          attrs["cover"] = images[attrs["cover"]] || images.sample
-        else
-          attrs["cover"] = images.sample
+        if attrs["cover"]
+          if attrs["cover"].is_a?(Numeric)
+            attrs["cover"] = images[attrs["cover"]] || images.sample
+          else
+            attrs["cover"] = images.sample
+          end
         end
-      end
 
-      attrs["documents"] = documents if attrs["documents"]
+        attrs["documents"] = documents if attrs["documents"]
 
-      if attrs["images"]
-        attrs["images"] = images.shuffle
-      end
-
-      if attrs["article"] == true
-        blog_article_klass = "#{::Rails.application.class.name.deconstantize}::Blog::Article".constantize
-
-        attrs["article"] = blog_article_klass.ordered
-                                             .published
-                                             .first
-      end
-
-      attrs["title"] = Faker::Hipster.sentence if attrs["title"] == true
-
-      %w[description text].each do |key|
-        attrs[key] = Faker::Hipster.paragraph if attrs[key] == true
-      end
-
-      %w[content].each do |key|
-        if attrs[key] == true
-          attrs[key] = "<p>#{Faker::Hipster.paragraph}</p>"
-        elsif attrs[key] == "long"
-          attrs[key] = 3.times.map { "<p>#{Faker::Hipster.paragraph}</p>" }.join("")
+        if attrs["images"]
+          attrs["images"] = images.shuffle
         end
-      end
 
-      attrs.each do |key, value|
-        if value == true
-          if association_klasses = attrs["type"].constantize::ASSOCIATIONS[key.to_sym]
-            if association_klasses.is_a?(Hash)
-              association_klasses = association_klasses[:klasses]
-            end
+        if attrs["article"] == true
+          blog_article_klass = "#{::Rails.application.class.name.deconstantize}::Blog::Article".constantize
 
-            record = association_klasses.first.constantize.last
+          attrs["article"] = blog_article_klass.ordered
+                                               .published
+                                               .first
+        end
 
-            if record
-              attrs[key] = record
-            else
-              attrs.delete(key)
+        attrs["title"] = Faker::Hipster.sentence if attrs["title"] == true
+
+        %w[description text].each do |key|
+          attrs[key] = Faker::Hipster.paragraph if attrs[key] == true
+        end
+
+        %w[content].each do |key|
+          if attrs[key] == true
+            attrs[key] = "<p>#{Faker::Hipster.paragraph}</p>"
+          elsif attrs[key] == "long"
+            attrs[key] = 3.times.map { "<p>#{Faker::Hipster.paragraph}</p>" }.join("")
+          end
+        end
+
+        attrs.each do |key, value|
+          if value == true
+            if association_klasses = attrs["type"].constantize::ASSOCIATIONS[key.to_sym]
+              if association_klasses.is_a?(Hash)
+                association_klasses = association_klasses[:klasses]
+              end
+
+              record = association_klasses.first.constantize.last
+
+              if record
+                attrs[key] = record
+              else
+                attrs.delete(key)
+              end
             end
           end
         end
+
+        if h[data["_placement_type"]].blank?
+          h[data["_placement_type"]] = (data["_placement_type"] || "Folio::Page").constantize.new
+        end
+
+        molecule.times do
+          atom = h[data["_placement_type"]].atoms.build(attrs)
+          atom.data["_placement_type"] = data["_placement_type"] || "Folio::Page"
+          atom.data["_showcase"] = data["_showcase"]
+        end
       end
 
-      molecule.times do
-        atom = @page.atoms.build(attrs)
-        atom.data["_showcase"] = data["_showcase"]
-      end
+      h
     end
-
-    @page
   end
 
   def container_class_name_for_atom(atom)
