3a25791b0fa0e0bed8c6d21b0f8f82f8b13429e2
Petr Marek <contact@petrmarek.eu>
2022-05-02 12:41:32 +0200

    rm obsolete folio/browser_update cell

--- test/cells/folio/browser_update_cell_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-# require 'test_helper'
-
-# class BrowserUpdateCellTest < Cell::TestCase
-#   test 'show' do
-#     html = cell('folio/browser_update').(:show)
-#     assert html
-#   end
-# end
