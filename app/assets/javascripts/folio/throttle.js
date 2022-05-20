ee3a3130c8253dca67d76826b571ab6a32d2a0d5
Petr Marek <contact@petrmarek.eu>
2022-05-20 10:16:00 +0200

    update folio/throttle js

--- app/assets/javascripts/folio/throttle.js
+++ app/assets/javascripts/folio/throttle.js
@@ -1,20 +1,22 @@
 // Extracted from https://stackoverflow.com/a/27078401
 
-window.folioThrottle = function (callback, limit) {
+window.Folio = window.Folio || {}
+
+window.Folio.throttle = function (callback, limit) {
   if (!limit) {
-    limit = 150;
+    limit = 150
   }
 
-  var waiting = false;                  // Initially, we're not waiting
+  let waiting = false // Initially, we're not waiting
 
-  return function () {                  // We return a throttled function
-    if (!waiting) {                     // If we're not waiting
-      callback.apply(this, arguments);  // Execute users function
-      waiting = true;                   // Prevent future invocations
+  return function () { // We return a throttled function
+    if (!waiting) { // If we're not waiting
+      callback.apply(this, arguments) // Execute users function
+      waiting = true // Prevent future invocations
 
-      setTimeout(function () {          // After a period of time
-        waiting = false;                // And allow future invocations
-      }, limit);
+      setTimeout(function () { // After a period of time
+        waiting = false // And allow future invocations
+      }, limit)
     }
   }
 }
