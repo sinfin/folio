de6320692a9a39d929544fa62767ec81591c0e09
Petr Marek <contact@petrmarek.eu>
2022-07-19 09:39:11 +0200

    restore simple_form flag component

---
+++ app/lib/folio/console/simple_form_components/flag_component.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module FlagComponent
+  CDN = "https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/2.9.0/flags"
+
+  def flag(wrapper_options = nil)
+    @flag ||= if options[:flag].present?
+      if code = ::Folio::LANGUAGES[options[:flag].to_sym]
+        src = "#{CDN}/4x3/#{code.downcase}.svg"
+        %{<img src="#{src}" alt="#{code}" class="folio-console-flag">}.html_safe
+      end
+    end
+  end
+end
+
+SimpleForm.include_component(FlagComponent)
