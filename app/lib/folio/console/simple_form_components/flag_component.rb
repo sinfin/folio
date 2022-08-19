779167a34f29b89d57f9aa2edc1b404334f6f188
Petr Marek <contact@petrmarek.eu>
2022-08-19 09:43:54 +0200

    fix flag_component namespace

--- app/lib/folio/console/simple_form_components/flag_component.rb
+++ app/lib/folio/console/simple_form_components/flag_component.rb
@@ -1,16 +1,22 @@
 # frozen_string_literal: true
 
-module FlagComponent
-  CDN = "https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/2.9.0/flags"
+module Folio
+  module Console
+    module SimpleFormComponents
+      module FlagComponent
+        CDN = "https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/2.9.0/flags"
 
-  def flag(wrapper_options = nil)
-    @flag ||= if options[:flag].present?
-      if code = ::Folio::LANGUAGES[options[:flag].to_sym]
-        src = "#{CDN}/4x3/#{code.downcase}.svg"
-        %{<img src="#{src}" alt="#{code}" class="folio-console-flag">}.html_safe
+        def flag(wrapper_options = nil)
+          @flag ||= if options[:flag].present?
+            if code = ::Folio::LANGUAGES[options[:flag].to_sym]
+              src = "#{CDN}/4x3/#{code.downcase}.svg"
+              %{<img src="#{src}" alt="#{code}" class="folio-console-flag">}.html_safe
+            end
+          end
+        end
       end
     end
   end
 end
 
-SimpleForm.include_component(FlagComponent)
+SimpleForm.include_component(Folio::Console::SimpleFormComponents::FlagComponent)
