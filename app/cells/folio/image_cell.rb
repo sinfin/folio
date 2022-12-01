5eb4885f9503678fc6bffcaf5731dc1315470f03
Petr Marek <contact@petrmarek.eu>
2022-12-01 12:46:03 +0100

    use file.description as fallback alt in image cell

--- app/cells/folio/image_cell.rb
+++ app/cells/folio/image_cell.rb
@@ -58,7 +58,7 @@ class Folio::ImageCell < Folio::ApplicationCell
       h = {
         normal:,
         src: normal.url,
-        alt: options[:alt] || "",
+        alt: options[:alt] || file.try(:description) || "",
         title: options[:title],
         width: normal[:width],
         height: normal[:height],
