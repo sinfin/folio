f0044c9a76c25e21edba65c459a35b7ac039d849
Petr Marek <contact@petrmarek.eu>
2022-12-16 11:16:05 +0100

    add user scopes test

--- test/models/folio/user_test.rb
+++ test/models/folio/user_test.rb
@@ -22,7 +22,7 @@ class Folio::UserTest < ActiveSupport::TestCase
     assert 0, Folio::NewsletterSubscription.count
   end
 
-  test "do not stores second address if it is not in use" do
+  test "do not store second address if it is not in use" do
     user = create(:folio_user, primary_address: nil)
 
     assert_nil user.primary_address
@@ -51,5 +51,56 @@ class Folio::UserTest < ActiveSupport::TestCase
 
     assert user.reload.use_secondary_address
     assert_equal "Somewhere", user.secondary_address.city
+  end
+
+  test "scope by_full_name_query" do
+    user = create(:folio_user, first_name: "foo", last_name: "bar")
+
+    assert Folio::User.by_full_name_query("foo bar").exists?(id: user.id)
+    assert Folio::User.by_full_name_query("foo").exists?(id: user.id)
+    assert Folio::User.by_full_name_query("bar").exists?(id: user.id)
+    assert Folio::User.by_full_name_query("fo").exists?(id: user.id)
+    assert Folio::User.by_full_name_query("ba").exists?(id: user.id)
+    assert_not Folio::User.by_full_name_query("xaxa").exists?(id: user.id)
+  end
+
+  test "scope by_address_identification_number_query" do
+    primary_address = create(:folio_address_primary, identification_number: "123456789")
+    user = create(:folio_user, primary_address:)
+
+    assert Folio::User.by_address_identification_number_query("1234").exists?(id: user.id)
+    assert Folio::User.by_address_identification_number_query("5678").exists?(id: user.id)
+    assert_not Folio::User.by_address_identification_number_query("5607").exists?(id: user.id)
+  end
+
+  test "scope by_addresses_query" do
+    primary_address = create(:folio_address_primary,
+                             name: "Lorem Ipsum",
+                             address_line_1: "Downing Street 10",
+                             city: "London",
+                             zip: "12345")
+    user = create(:folio_user, primary_address:)
+
+    assert Folio::User.by_addresses_query("Downing Street 10").exists?(id: user.id)
+    assert Folio::User.by_addresses_query("Downing").exists?(id: user.id)
+    assert Folio::User.by_addresses_query("10").exists?(id: user.id)
+    assert Folio::User.by_addresses_query("London").exists?(id: user.id)
+    assert Folio::User.by_addresses_query("Lorem").exists?(id: user.id)
+    assert Folio::User.by_addresses_query("Ips").exists?(id: user.id)
+    assert Folio::User.by_addresses_query("12345").exists?(id: user.id)
+    assert_not Folio::User.by_addresses_query("xaxa").exists?(id: user.id)
+  end
+
+  test "scope by_email_query" do
+    user = create(:folio_user, email: "foo@bar.baz")
+
+    assert Folio::User.by_email_query("foo").exists?(id: user.id)
+    assert Folio::User.by_email_query("fo").exists?(id: user.id)
+    assert Folio::User.by_email_query("bar").exists?(id: user.id)
+    assert Folio::User.by_email_query("ba").exists?(id: user.id)
+    assert Folio::User.by_email_query(".baz").exists?(id: user.id)
+    assert Folio::User.by_email_query("@bar.baz").exists?(id: user.id)
+    assert Folio::User.by_email_query("@bar").exists?(id: user.id)
+    assert_not Folio::User.by_email_query("xaxa").exists?(id: user.id)
   end
 end
