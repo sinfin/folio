fa14b5afdf7dd4959f284ba3eab3956adf468b6a
dedekm <dedekmm@gmail.com>
2023-03-22 08:10:09 +0100

    add fields_layout option to console/addresses/fields cell

--- app/cells/folio/console/addresses/fields_cell.rb
+++ app/cells/folio/console/addresses/fields_cell.rb
@@ -10,15 +10,28 @@ class Folio::Console::Addresses::FieldsCell < Folio::ConsoleCell
     ]
   end
 
-  def input(g, key, required: false)
+  def fields_layout(g, key)
+    options[:fields_layout] && options[:fields_layout][key] || g.object.class.fields_layout
+  end
+
+  def input(g, key, address_required: true)
+    required = address_required ? nil : false
+
+    # FIXME: address doesn't know it's placement at this moment, can't change validation according to the placement class
+    if options[:required] && options[:required].include?(key)
+      required = true
+    end
+
     if key == :country_code
-      g.input(key, priority: g.object.class.priority_countries, required:)
+      g.input(key, only: g.object.class.countries_whitelist,
+                   priority: g.object.class.priority_countries,
+                   include_blank: false)
     else
       g.input(key, required:)
     end
   end
 
-  def required?(key, attributes)
+  def address_required?(key, attributes)
     return true if model.object.send("should_validate_#{key}?")
     all_blank = attributes.all? { |attr, val| attr == "type" || val.blank? }
     !all_blank
