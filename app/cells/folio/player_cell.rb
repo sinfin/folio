0bd4941974b0e835223737ce32a2ad71160e5dbc
Petr Marek <contact@petrmarek.eu>
2023-04-27 09:32:08 +0200

    use console file serializer for player form controls

--- app/cells/folio/player_cell.rb
+++ app/cells/folio/player_cell.rb
@@ -4,10 +4,18 @@ class Folio::PlayerCell < ApplicationCell
   def data
     {
       "controller" => "f-player",
-      "file" => Folio::FileSerializer.new(model)
-                                     .serializable_hash[:data]
-                                     .to_json,
+      "file" => serializer.new(model)
+                          .serializable_hash[:data]
+                          .to_json,
       "f-player-show-form-controls-value" => options[:show_form_controls] ? "true" : "false",
     }
+  end
+
+  def serializer
+    if options[:show_form_controls]
+      Folio::Console::FileSerializer
+    else
+      Folio::FileSerializer
+    end
   end
 end
