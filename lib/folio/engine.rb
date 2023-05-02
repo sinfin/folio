47a73b252b270d9f99f60aa783ca24573b471cde
Petr Marek <contact@petrmarek.eu>
2023-05-02 12:05:46 +0200

    confirm email change by default

--- lib/folio/engine.rb
+++ lib/folio/engine.rb
@@ -75,7 +75,7 @@ module Folio
     config.folio_users_require_phone = false
     config.folio_users_sign_out_everywhere = true
     config.folio_users_confirmable = false
-    config.folio_users_confirm_email_change = false
+    config.folio_users_confirm_email_change = true
     config.folio_users_publicly_invitable = true
     config.folio_users_use_address = true
     config.folio_users_omniauth_providers = %i[facebook google_oauth2 twitter2]
