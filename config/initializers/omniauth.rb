b795edefc0c4a611c0eac7b8e60377800c7c9327
Petr Marek <contact@petrmarek.eu>
2023-05-19 11:37:07 +0200

    update apple omniauth keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,14 @@ require Folio::Engine.root.join("app/models/folio/omniauth") # to load Folio::Om
 if Rails.application.config.folio_users
   Rails.application.config.middleware.use OmniAuth::Builder do
     Rails.application.config.folio_users_omniauth_providers.each do |provider_key|
-      if ENV["OMNIAUTH_#{provider_key.to_s.upcase}_CLIENT_ID"].present?
+      if provider_key == :apple && ENV["OMNIAUTH_APPLE_SERVICE_BUNDLE_ID"]
+        provider :apple, ENV["OMNIAUTH_APPLE_SERVICE_BUNDLE_ID"], "", {
+          scope: "email name",
+          team_id: ENV["OMNIAUTH_APPLE_APP_ID_PREFIX"],
+          key_id: ENV["OMNIAUTH_APPLE_KEY_ID"],
+          pem: ENV["OMNIAUTH_APPLE_P8_FILE_CONTENT_WITH_EXTRA_NEWLINE"],
+        }
+      elsif ENV["OMNIAUTH_#{provider_key.to_s.upcase}_CLIENT_ID"].present?
         if provider_key == :twitter2
           provider provider_key,
                    ENV["OMNIAUTH_#{provider_key.to_s.upcase}_CLIENT_ID"],
@@ -16,13 +23,6 @@ if Rails.application.config.folio_users
                    ENV["OMNIAUTH_#{provider_key.to_s.upcase}_CLIENT_ID"],
                    ENV["OMNIAUTH_#{provider_key.to_s.upcase}_CLIENT_SECRET"]
         end
-      elsif provider_key == :apple && ENV["APPLE_SERVICE_BUNDLE_ID"]
-        provider :apple, ENV["APPLE_SERVICE_BUNDLE_ID"], "", {
-          scope: "email name",
-          team_id: ENV["APPLE_APP_ID_PREFIX"],
-          key_id: ENV["APPLE_KEY_ID"],
-          pem: ENV["APPLE_P8_FILE_CONTENT_WITH_EXTRA_NEWLINE"],
-        }
       end
     end
   end
