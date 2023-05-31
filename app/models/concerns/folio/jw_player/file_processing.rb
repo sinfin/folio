6b4078b146b25712bec210c8029b21f8535826aa
Petr Mlčoch <foton@centrum.cz>
2023-05-31 09:42:49 +0200

    fixed preview playback for JWT

--- app/models/concerns/folio/jw_player/file_processing.rb
+++ app/models/concerns/folio/jw_player/file_processing.rb
@@ -16,32 +16,44 @@ module Folio::JwPlayer::FileProcessing
     require "jwt"
   end
 
-  def remote_path
-    "/v2/media/#{remote_key}"
+  def remote_path(key)
+    "/v2/media/#{key}"
+  end
+
+  def remote_url_base
+    "https://cdn.jwplayer.com"
   end
 
   def remote_full_url
-    "https://cdn.jwplayer.com#{remote_path}"
+    remote_url_base + remote_path(remote_key)
+  end
+
+  def remote_preview_url
+    remote_url_base + remote_path(remote_preview_key)
+  end
+
+  def remote_signed_full_url(expires_at = 2.hours.from_now)
+    token = token_for_remote_signed_url(remote_path(remote_key), expires_at)
+    "#{remote_full_url}?token=#{token}"
+  end
+
+  def remote_signed_preview_url(expires_at = 2.hours.from_now)
+    token = token_for_remote_signed_url(remote_path(remote_preview_key), expires_at)
+    "#{remote_preview_url}?token=#{token}"
   end
 
   # player needs to send headers `{ "alg": "HS256",  "typ": "JWT"}`
-  def remote_signed_full_url(expires_at = 2.hours.from_now)
+  def token_for_remote_signed_url(remote_path, expires_at)
     params = {
       "resource" => remote_path,
       "exp" => expires_at.to_i
     }
 
     if Rails.env.test?
-      token = "test"
+      "test"
     else
-      token = JWT.encode(params, ENV.fetch("JWPLAYER_API_V1_SECRET"), "HS256", typ: "JWT")
+      JWT.encode(params, ENV.fetch("JWPLAYER_API_V1_SECRET"), "HS256", typ: "JWT")
     end
-
-    "#{remote_full_url}?token=#{token}"
-  end
-
-  def remote_preview_url
-    "https://cdn.jwplayer.com/v2/media/#{remote_preview_key}"
   end
 
   def full_media_job_class
