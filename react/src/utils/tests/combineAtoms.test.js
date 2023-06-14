46e7f20cd5903d70c0ecd1d92e460ac642780799
Petr Marek <contact@petrmarek.eu>
2023-06-14 08:15:35 +0200

    fix combineAtoms for molecule edit

--- react/src/utils/tests/combineAtoms.test.js
+++ react/src/utils/tests/combineAtoms.test.js
@@ -39,6 +39,42 @@ describe('atomsReducer', () => {
     expect(combineAtoms(hash)).toEqual(['a', 'b', 'c'])
   })
 
+  it('works for molecule splice middle', () => {
+    const hash = {
+      oldAtoms: ['a', 'b', 'c', 'c'],
+      newAtoms: ['d', 'd'],
+      formAction: 'splice',
+      indices: [1],
+      edit: false
+    }
+
+    expect(combineAtoms(hash)).toEqual(['a', 'd', 'd', 'b', 'c', 'c'])
+  })
+
+  it('works for molecule splice to almost the end', () => {
+    const hash = {
+      oldAtoms: ['a', 'b', 'c', 'c'],
+      newAtoms: ['d', 'd'],
+      formAction: 'splice',
+      indices: [2, 3],
+      edit: false
+    }
+
+    expect(combineAtoms(hash)).toEqual(['a', 'b', 'd', 'd', 'c', 'c'])
+  })
+
+  it('works for edit molecule splice', () => {
+    const hash = {
+      oldAtoms: ['a', 'b', 'c', 'c'],
+      newAtoms: ['c', 'd'],
+      formAction: 'splice',
+      indices: [2, 3],
+      edit: true
+    }
+
+    expect(combineAtoms(hash)).toEqual(['a', 'b', 'c', 'd'])
+  })
+
   it('works for prepend', () => {
     const hash = {
       oldAtoms: ['a', 'b'],
