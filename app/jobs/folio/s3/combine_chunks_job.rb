4e8dcb8615c2a8ff235a7f96cbb0f3fdc57dbd62
Petr Marek <contact@petrmarek.eu>
2023-06-22 13:37:45 +0200

    Revert "multipart mv in s3_data_store_override for files bigger than 5 gb"
    This reverts commit e69c6835444389f1263dacd348a9575b20e86748.

--- app/jobs/folio/s3/combine_chunks_job.rb
+++ app/jobs/folio/s3/combine_chunks_job.rb
@@ -1,16 +1,37 @@
 # frozen_string_literal: true
 
 class Folio::S3::CombineChunksJob < Folio::S3::BaseJob
-  def perform_for_valid(s3_path:, klass:, existing_id:, web_session_id:, user_id:, attributes:, from_chunks:)
-    s3_multipart_upload(s3_path:, target_path: s3_path)
+  def perform_for_valid(s3_path:, klass:, existing_id:, web_session_id:, user_id:, attributes:)
+    multipart_upload = s3_client.create_multipart_upload(bucket: s3_bucket, key: s3_path)
+
+    s3_structs_for_parts = s3_client.list_objects(bucket: s3_bucket, prefix: "#{s3_path}.").contents
+
+    parts = s3_structs_for_parts.each_with_index
+                                .map do |s3_struct_for_part, i|
+      copy_response = s3_client.upload_part_copy(bucket: s3_bucket,
+                                                 copy_source: "#{s3_bucket}/#{s3_struct_for_part.key}",
+                                                 key: s3_path,
+                                                 part_number: i + 1,
+                                                 upload_id: multipart_upload.upload_id)
+
+      { etag: copy_response.copy_part_result.etag, part_number: i + 1 }
+    end
+
+    s3_client.complete_multipart_upload(bucket: s3_bucket,
+                                        key: s3_path,
+                                        multipart_upload: { parts: },
+                                        upload_id: multipart_upload.upload_id)
 
     Folio::S3::CreateFileJob.perform_later(s3_path:,
                                            type: klass.to_s,
                                            existing_id:,
                                            web_session_id:,
                                            user_id:,
-                                           attributes:,
-                                           from_chunks: true)
+                                           attributes:)
+  ensure
+    s3_client.list_objects(bucket: s3_bucket, prefix: "#{s3_path}.")
+             .contents
+             .each { |s3_struct_for_part| test_aware_s3_delete(s3_struct_for_part.key) }
   end
 
   def self.multipart?
--- app/overrides/lib/dragonfly-s3_data_store/dragonfly/s3_data_store_override.rb
+++ app/overrides/lib/dragonfly-s3_data_store/dragonfly/s3_data_store_override.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Dragonfly::S3DataStore.class_eval do
-  include Folio::S3::Client
-
   alias_method :write_original, :write
 
   def write(content, opts = {})
@@ -19,21 +17,14 @@ Dragonfly::S3DataStore.class_eval do
         # this is different
         target_path = full_path(uid)
 
-        if content.meta["folio_s3_from_chunks"].present?
-          s3_multipart_upload(s3_path: content.meta["folio_s3_path"], target_path:)
-        else
-          storage.copy_object(bucket_name,
-                              content.meta["folio_s3_path"],
-                              bucket_name,
-                              target_path,
-                              "x-amz-metadata-directive" => "REPLACE")
-        end
+        storage.copy_object(bucket_name,
+                            content.meta["folio_s3_path"],
+                            bucket_name,
+                            target_path,
+                            "x-amz-metadata-directive" => "REPLACE")
 
         file = storage.directories.new(key: bucket_name).files.get(target_path)
-
-        cleaned_up_headers = full_storage_headers(headers,
-                                                  content.meta.without("folio_s3_path",
-                                                                       "folio_s3_from_chunks"))
+        cleaned_up_headers = full_storage_headers(headers, content.meta.without("folio_s3_path"))
 
         file.acl = cleaned_up_headers["x-amz-acl"] || "public-read"
         file.metadata = cleaned_up_headers
