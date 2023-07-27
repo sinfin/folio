837bf3157c294a8618c60031e4cc7c053c364ef1
Petr Marek <contact@petrmarek.eu>
2023-07-27 06:45:11 +0200

    fix sti_badge for new records

--- app/cells/folio/console/ui/sti_badge_cell.rb
+++ app/cells/folio/console/ui/sti_badge_cell.rb
@@ -2,8 +2,16 @@
 
 class Folio::Console::Ui::StiBadgeCell < Folio::ConsoleCell
   def icon
-    if model.class.respond_to?(:klasses_for_console)
-      model.class.klasses_for_console[model.class]
+    if model_class.respond_to?(:klasses_for_console)
+      model_class.klasses_for_console[model_class]
+    end
+  end
+
+  def model_class
+    @model_class ||= if model.new_record? && model.type
+      model.type.constantize
+    else
+      model.class
     end
   end
 end
