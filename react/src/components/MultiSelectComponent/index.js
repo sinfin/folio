05ece00bb3b6eaadb9ee10bc03d5fc5e5a5f3e3b
Petr Marek <contact@petrmarek.eu>
2023-08-02 13:53:36 +0200

    update react multi select

--- react/src/components/MultiSelectComponent/index.js
+++ react/src/components/MultiSelectComponent/index.js
@@ -6,9 +6,31 @@ import FileFilter from 'containers/FileFilter'
 import Uploader from 'containers/Uploader'
 import UploadTagger from 'containers/UploadTagger'
 import Loader from 'components/Loader'
-import Card from 'components/Card'
 import FileList from 'components/FileList'
 import FilePlacementList from 'components/FilePlacementList'
+
+const I18N = {
+  cs: {
+    available_document: 'Dostupné dokumenty',
+    available_image: 'Dostupné obrázky',
+    available_video: 'Dostupná videa',
+    available_audio: 'Dostupná audia',
+    selected_document: 'Vybrané dokumenty',
+    selected_image: 'Vybrané obrázky',
+    selected_video: 'Vybraná videa',
+    selected_audio: 'Vybraná audia'
+  },
+  en: {
+    available_document: 'Available documents',
+    available_image: 'Available images',
+    available_video: 'Available videos',
+    available_audio: 'Available audios',
+    selected_document: 'Selected documents',
+    selected_image: 'Selected images',
+    selected_video: 'Selected videos',
+    selected_audio: 'Selected audios'
+  }
+}
 
 class MultiSelectComponent extends LazyLoadCheckingComponent {
   componentWillMount () {
@@ -25,48 +47,41 @@ class MultiSelectComponent extends LazyLoadCheckingComponent {
 
     return (
       <Uploader fileType={this.props.fileType} filesUrl={this.props.filesUrl} reactType={this.props.reactType}>
-        <Card
-          highlighted
-          header={window.FolioConsole.translations[`selected${this.props.fileType}`]}
-          className='f-c-multi-select-component__card f-c-multi-select-component__card--placements'
-        >
-          <FilePlacementList
-            filePlacements={this.props.filePlacements}
-            onSortEnd={this.props.onSortEnd}
-            onAltChange={this.props.onAltChange}
-            onTitleChange={this.props.onTitleChange}
-            unselectFilePlacement={this.props.unselectFilePlacement}
+        <h2>{window.Folio.i18n(I18N, `selected_${this.props.reactType}`)}</h2>
+
+        <FilePlacementList
+          filePlacements={this.props.filePlacements}
+          onSortEnd={this.props.onSortEnd}
+          onAltChange={this.props.onAltChange}
+          onTitleChange={this.props.onTitleChange}
+          unselectFilePlacement={this.props.unselectFilePlacement}
+          fileTypeIsImage={this.props.fileTypeIsImage}
+          fileType={this.props.fileType}
+          nested={this.props.nested}
+          openFileModal={this.props.openFileModal}
+        />
+
+        <h2>{window.Folio.i18n(I18N, `available_${this.props.reactType}`)}</h2>
+
+        <FileFilter fileType={this.props.fileType} filesUrl={this.props.filesUrl} fileTypeIsImage={this.props.fileTypeIsImage} className='f-c-anti-container-fluid f-c-anti-container-fluid--padded' />
+
+        <UploadTagger fileType={this.props.fileType} nested={this.props.nested} taggable={this.props.taggable} />
+
+        {this.props.filesStatus.loading ? <Loader standalone /> : (
+          <FileList
+            files={this.props.unselectedFilesForList}
             fileTypeIsImage={this.props.fileTypeIsImage}
+            displayAsThumbs={this.props.displayAsThumbs}
+            onClick={this.props.selectFile}
+            pagination={this.props.filesPagination}
+            changeFilesPage={this.props.changeFilesPage}
             fileType={this.props.fileType}
-            nested={this.props.nested}
+            filesUrl={this.props.filesUrl}
+            selecting='multiple'
             openFileModal={this.props.openFileModal}
+            dropzoneTrigger
           />
-        </Card>
-
-        <Card
-          header={window.FolioConsole.translations[`available${this.props.fileType}`]}
-          headerUpload
-          filters={<FileFilter fileType={this.props.fileType} filesUrl={this.props.filesUrl} fileTypeIsImage={this.props.fileTypeIsImage} />}
-          className='f-c-multi-select-component__card f-c-multi-select-component__card--files'
-        >
-          <UploadTagger fileType={this.props.fileType} nested={this.props.nested} taggable={this.props.taggable} />
-
-          {this.props.filesStatus.loading ? <Loader standalone /> : (
-            <FileList
-              files={this.props.unselectedFilesForList}
-              fileTypeIsImage={this.props.fileTypeIsImage}
-              displayAsThumbs={this.props.displayAsThumbs}
-              onClick={this.props.selectFile}
-              pagination={this.props.filesPagination}
-              changeFilesPage={this.props.changeFilesPage}
-              fileType={this.props.fileType}
-              filesUrl={this.props.filesUrl}
-              selecting='multiple'
-              openFileModal={this.props.openFileModal}
-              dropzoneTrigger
-            />
-          )}
-        </Card>
+        )}
       </Uploader>
     )
   }
--- react/src/components/NestedModelControls/index.js
+++ react/src/components/NestedModelControls/index.js
@@ -1,15 +1,10 @@
 import React from 'react'
 
 import { makeConfirmed } from 'utils/confirmed'
+import FolioConsoleUiButtons from 'components/FolioConsoleUiButtons'
 import FolioConsoleUiButton from 'components/FolioConsoleUiButton'
 
 const NestedModelControls = ({ moveUp, moveDown, remove, edit, vertical }) => {
-  let btnGroupClassName = 'btn-group mr-1'
-
-  if (vertical) {
-    btnGroupClassName = 'btn-group btn-group-vertical align-items-center'
-  }
-
   const destroyButton = remove && (
     <FolioConsoleUiButton
       variant='danger'
@@ -28,31 +23,26 @@ const NestedModelControls = ({ moveUp, moveDown, remove, edit, vertical }) => {
   )
 
   return (
-    <div className='f-c-nested-model-controls'>
-      <div className={btnGroupClassName}>
-        {moveUp && (
-          <FolioConsoleUiButton
-            variant='secondary'
-            icon='arrow_up'
-            onClick={moveUp}
-          />
-        )}
+    <FolioConsoleUiButtons className='f-c-nested-model-controls'>
+      {moveUp && (
+        <FolioConsoleUiButton
+          variant='secondary'
+          icon='arrow_up'
+          onClick={moveUp}
+        />
+      )}
 
-        {moveDown && (
-          <FolioConsoleUiButton
-            variant='secondary'
-            icon='arrow_down'
-            onClick={moveDown}
-          />
-        )}
+      {moveDown && (
+        <FolioConsoleUiButton
+          variant='secondary'
+          icon='arrow_down'
+          onClick={moveDown}
+        />
+      )}
 
-        {vertical && editButton}
-        {vertical && destroyButton}
-      </div>
-
-      {!vertical && editButton}
-      {!vertical && destroyButton}
-    </div>
+      {editButton}
+      {destroyButton}
+    </FolioConsoleUiButtons>
   )
 }
 
