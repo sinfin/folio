70d12278b3f99e61a3341b9cfe5e4e218934ebab
Petr Marek <contact@petrmarek.eu>
2023-08-16 08:48:23 +0200

    only cache current_site based on config.action_controller.perform_caching

--- app/controllers/concerns/folio/has_current_site.rb
+++ app/controllers/concerns/folio/has_current_site.rb
@@ -8,7 +8,9 @@ module Folio::HasCurrentSite
   end
 
   def current_site
-    @current_site ||= if ::Rails.application.config.folio_site_cache_current_site && respond_to?(:cache_key_base)
+    @current_site ||= if ::Rails.application.config.action_controller.perform_caching &&
+                         ::Rails.application.config.folio_site_cache_current_site &&
+                         respond_to?(:cache_key_base)
       Rails.cache.fetch(["current_site", request.host] + cache_key_base) do
         Folio.current_site(request:, controller: self)
       end
