cffa2a250658afe27b0467e8b2b7b042e8f41eb1
Petr Marek <contact@petrmarek.eu>
2023-08-28 12:46:48 +0200

    fix bootstrap-overrides in assets generator

--- lib/generators/folio/assets/assets_generator.rb
+++ lib/generators/folio/assets/assets_generator.rb
@@ -24,7 +24,6 @@ class Folio::AssetsGenerator < Rails::Generators::Base
     app/assets/stylesheets/modules/_rich-text.sass
     app/assets/stylesheets/modules/_turbolinks.sass
     app/assets/stylesheets/modules/_with-icon.sass
-    app/assets/stylesheets/modules/bootstrap-overrides/**/*.sass
     bin/icons
     package.json
   ]
@@ -51,6 +50,13 @@ class Folio::AssetsGenerator < Rails::Generators::Base
     TEMPLATES.each { |f| template "#{f}.tt", f }
   end
 
+  def copy_bootstrap_overrides
+    Dir.glob(Folio::Engine.root.join("lib/generators/folio/assets/templates/app/assets/stylesheets/modules/bootstrap-overrides/**/*.sass.tt")).each do |path|
+      clear_path = path.split('lib/generators/folio/assets/templates/', 2).last
+      template clear_path, clear_path.delete_suffix('.tt')
+    end
+  end
+
   def copy_files
     base = ::Folio::Engine.root.join("lib/generators/folio/assets/templates/").to_s
 
@@ -66,7 +72,14 @@ class Folio::AssetsGenerator < Rails::Generators::Base
     [
       "bin/icons",
     ].each do |file|
-      ::File.chmod(0775, Rails.root.join(file))
+      [
+        Rails.root.join(file),
+        file,
+      ].each do |path|
+        if File.exist?(path)
+          ::File.chmod(0775, path)
+        end
+      end
     end
   end
 end
