2fc75c6e89327efe3beb1ce92877ddb85a374425
dedekm <dedekmm@gmail.com>
2023-08-30 15:50:04 +0200

    update console/addresses/show_for_model cell options

--- app/cells/folio/console/addresses/show_for_model_cell.rb
+++ app/cells/folio/console/addresses/show_for_model_cell.rb
@@ -4,7 +4,7 @@ class Folio::Console::Addresses::ShowForModelCell < Folio::ConsoleCell
   include ShowFor::Helper
 
   def cols
-    [
+    options[:cols] || [
       [Folio::Address::Primary, model.primary_address],
       [Folio::Address::Secondary, model.secondary_address],
     ]
