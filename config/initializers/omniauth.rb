c1011538a318c171957aeb4d4d98ce80cc8d93c5
dedekm <dedekmm@gmail.com>
2023-09-11 20:17:23 +0200

    fix omniauth initialization for apps without omniauth providers

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@
 
 require Folio::Engine.root.join("app/models/folio/omniauth") # to load Folio::Omniauth namespace
 
-if Rails.application.config.folio_users
+if Rails.application.config.folio_users && Rails.application.config.folio_users_omniauth_providers.present?
   Rails.application.config.middleware.use OmniAuth::Builder do
     Rails.application.config.folio_users_omniauth_providers.each do |provider_key|
       if ENV["OMNIAUTH_#{provider_key.to_s.upcase}_CLIENT_ID"].present?
