80255435a72bf8c0c8821a6950a6a965af291474
Petr Marek <contact@petrmarek.eu>
2023-09-27 10:51:35 +0200

    add small_label option to ui/boolean_toggle_component

--- app/components/folio/console/ui/boolean_toggle_component.rb
+++ app/components/folio/console/ui/boolean_toggle_component.rb
@@ -3,16 +3,17 @@
 class Folio::Console::Ui::BooleanToggleComponent < Folio::Console::ApplicationComponent
   bem_class_name :label, :verbose
 
-  def initialize(record:, attribute:, url: nil, disabled: false, label: nil, verbose: false, as: nil, confirm: false, class_name: nil)
+  def initialize(record:, attribute:, url: nil, disabled: false, label: nil, verbose: false, as: nil, confirm: false, class_name: nil, small_label: false)
     @record = record
     @attribute = attribute
     @disabled = disabled
-    @label = label || verbose
+    @label = label || small_label || verbose
     @verbose = verbose
     @as = as
     @confirm = confirm
     @url = url
     @class_name = class_name
+    @small_label = small_label
   end
 
   def checked
