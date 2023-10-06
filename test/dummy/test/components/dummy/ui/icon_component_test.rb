970a385733373217b3b4fdd83242b193f519bd4c
Petr Marek <contact@petrmarek.eu>
2023-10-06 08:51:02 +0200

    fix icon component test

--- test/dummy/test/components/dummy/ui/icon_component_test.rb
+++ test/dummy/test/components/dummy/ui/icon_component_test.rb
@@ -4,7 +4,7 @@ require "test_helper"
 
 class Dummy::Ui::IconComponentTest < Folio::ComponentTest
   def test_render
-    render_inline(Dummy::Ui::IconComponent.new(name: :x))
+    render_inline(Dummy::Ui::IconComponent.new(name: :close))
 
     assert_selector(".d-ui-icon")
   end
