65a52fd5e1265282850034ab6e15cd1238c536c7
Vlada <vladislav.trefil@protonmail.com>
2023-10-23 17:49:35 +0200

    change from complete to chat and swtich model

--- app/lib/folio/chat_gpt_client.rb
+++ app/lib/folio/chat_gpt_client.rb
@@ -12,24 +12,29 @@ class Folio::ChatGptClient
       fail "Missing OPENAI_API_KEY"
     end
 
-    @client ||= OpenAI::Client.new(api_key: ENV["OPENAI_API_KEY"])
+    @client ||= OpenAI::Client.new(access_token: ENV["OPENAI_API_KEY"])
   end
 
-  def generate_completion(prompt, length)
-    response = @client.completions(
-      engine: "gpt-3.5-turbo-instruct",
-      prompt:,
-      n: 1,
-      max_tokens: length
+  def generate_response(prompt, length)
+    start_time = Time.now
+    response = @client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo-16k",
+        messages: [{ role: "user", content: prompt }],
+        n: 1,
+        max_tokens: length,
+        temperature: 0.7,
+      }
     )
+    end_time = Time.now
 
-    log_response(prompt, response)
+    log_response(prompt, response, end_time - start_time)
 
     response
   end
 
-  def log_response(prompt, response)
-    json_obj = { prompt:, response: }.to_json
+  def log_response(prompt, response, duration)
+    json_obj = { prompt:, response:, duration: }.to_json
     finish_reason = response.try(:choices).try(:first).try(:finish_reason)
 
     if finish_reason == "stop"
