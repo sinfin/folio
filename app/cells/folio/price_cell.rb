d09f90c6bed3b06e7f3222c8efdf0abb1820c978
Petr Marek <contact@petrmarek.eu>
2023-11-21 11:54:39 +0100

    add format option support to folio/price

--- app/cells/folio/price_cell.rb
+++ app/cells/folio/price_cell.rb
@@ -9,10 +9,15 @@ class Folio::PriceCell < Folio::ApplicationCell
     elsif options[:zero_as_text] && model.zero?
       t(".free")
     else
-      number_to_currency(model,
-                         unit: options[:currency] || t(".default_currency"),
-                         precision: options[:precision] || 0,
-                         delimiter: " ")
+      kwargs = {
+        unit: options[:currency] || t(".default_currency"),
+        precision: options[:precision] || 0,
+        delimiter: " ",
+      }
+
+      kwargs[:format] = options[:format] if options[:format]
+
+      number_to_currency(model, **kwargs)
     end
   end
 end
