dbb63b3b371eea2b4eb91b8c0d2e24471d93f778
Petr Mlčoch <foton@centrum.cz>
2023-12-04 14:04:47 +0100

    improved migration (running RakeTask) to work in Folio and in other Apps

--- db/migrate/20231106080839_fill_up_site_to_folio_records.rb
+++ db/migrate/20231106080839_fill_up_site_to_folio_records.rb
@@ -3,7 +3,8 @@
 class FillUpSiteToFolioRecords < ActiveRecord::Migration[7.0]
   def up
     # it is in migration, because it must done right after deploy
-    Rake::Task["folio:developer_tools:idp_fill_up_site_to_folio_records"].invoke()
+    correct_name = Rake::Task.tasks.collect(&:name).detect { |tn| tn.include?("idp_fill_up_site_to_folio_records") }
+    Rake::Task[correct_name].invoke()
   end
 
   def down
