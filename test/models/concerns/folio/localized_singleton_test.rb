2c298f98483434b43283e8c55eeee2d5ba68fccd
Petr Marek <contact@petrmarek.eu>
2023-12-13 09:24:45 +0100

    fix localized_singleton_test locale

--- test/models/concerns/folio/localized_singleton_test.rb
+++ test/models/concerns/folio/localized_singleton_test.rb
@@ -8,16 +8,18 @@ class Folio::LocalizedSingletonTest < ActiveSupport::TestCase
   end
 
   test "allows only one record" do
-    singletoned_class = TestLocalizedSingleton
-    site = get_any_site
+    I18n.with_locale(:cs) do
+      singletoned_class = TestLocalizedSingleton
+      site = get_any_site
 
-    _allowed_cz = singletoned_class.create!(locale: :cz, title: "Povolený", site:)
-    _allowed_en = singletoned_class.create!(locale: :en, title: "Allowed", site:)
-    not_allowed = singletoned_class.new(locale: :en, title: "Allowed2", site:)
+      _allowed_cz = singletoned_class.create!(locale: :cz, title: "Povolený", site:)
+      _allowed_en = singletoned_class.create!(locale: :en, title: "Allowed", site:)
+      not_allowed = singletoned_class.new(locale: :en, title: "Allowed2", site:)
 
-    assert_not not_allowed.valid?
-    assert_includes(not_allowed.errors[:type],
-                    "Je povolen jen jeden záznam '#{singletoned_class}' s 'locale = \"en\"'.",
-                    not_allowed.errors.to_json)
+      assert_not not_allowed.valid?
+      assert_includes(not_allowed.errors[:type],
+                      "Je povolen jen jeden záznam '#{singletoned_class}' s 'locale = \"en\"'.",
+                      not_allowed.errors.to_json)
+    end
   end
 end
