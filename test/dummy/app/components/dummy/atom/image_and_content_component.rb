26a69d6b8478a840a221584fc117b98fe7d1a71a
Vlada <vladislav.trefil@protonmail.com>
2023-12-13 22:33:50 +0100

    feat(atoms): add image_and_content style and layout

--- test/dummy/app/components/dummy/atom/image_and_content_component.rb
+++ test/dummy/app/components/dummy/atom/image_and_content_component.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 class Dummy::Atom::ImageAndContentComponent < ApplicationComponent
+  THUMB_SIZE = "648x450#"
+
+  bem_class_name :image_right
+
   def initialize(atom:, atom_options: {})
     @atom = atom
     @atom_options = atom_options
+    @image_right = @atom.image_side_with_fallback == "right"
   end
 end
