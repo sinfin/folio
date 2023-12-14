37930650d26c66951d911f8ef0901596e8fb21a2
Vlada <vladislav.trefil@protonmail.com>
2023-12-15 00:02:34 +0100

    feat(atoms): card L style and layout

--- test/dummy/app/components/dummy/molecule/cards/large_component.rb
+++ test/dummy/app/components/dummy/molecule/cards/large_component.rb
@@ -3,6 +3,8 @@
 class Dummy::Molecule::Cards::LargeComponent < ApplicationComponent
   include Folio::Molecule::CoverPlacements
 
+  THUMB_SIZE = "424x420#"
+
   def initialize(atoms:, atom_options: {})
     @atoms = atoms
     @atom_options = atom_options
