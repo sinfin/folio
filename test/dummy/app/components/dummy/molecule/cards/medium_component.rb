a527dfab6701cc7f0652b15a63f126003b3bdba6
Vlada <vladislav.trefil@protonmail.com>
2023-12-14 23:12:34 +0100

    feat(atoms): add cards M style and layout

--- test/dummy/app/components/dummy/molecule/cards/medium_component.rb
+++ test/dummy/app/components/dummy/molecule/cards/medium_component.rb
@@ -3,6 +3,9 @@
 class Dummy::Molecule::Cards::MediumComponent < ApplicationComponent
   include Folio::Molecule::CoverPlacements
 
+  THUMB_SIZE = "240x320#"
+  MOBILE_THUMB_SIZE = "480x396#"
+
   def initialize(atoms:, atom_options: {})
     @atoms = atoms
     @atom_options = atom_options
