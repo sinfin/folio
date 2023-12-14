598990b9d354bf0bcc2cc1ae244b643257bab610
Vlada <vladislav.trefil@protonmail.com>
2023-12-14 11:20:39 +0100

    feat(atoms): add cards XS style and layout

--- test/dummy/app/components/dummy/molecule/cards/extra_small_component.rb
+++ test/dummy/app/components/dummy/molecule/cards/extra_small_component.rb
@@ -3,6 +3,8 @@
 class Dummy::Molecule::Cards::ExtraSmallComponent < ApplicationComponent
   include Folio::Molecule::CoverPlacements
 
+  THUMB_SIZE = "80x80#"
+
   def initialize(atoms:, atom_options: {})
     @atoms = atoms
     @atom_options = atom_options
