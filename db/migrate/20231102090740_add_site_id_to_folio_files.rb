51dcb7d90ffdb1ff2f75204454bec764303478d4
Petr Mlčoch <foton@centrum.cz>
2023-12-18 14:17:28 +0100

    improved  migration to add ite to folio_files

--- db/migrate/20231102090740_add_site_id_to_folio_files.rb
+++ db/migrate/20231102090740_add_site_id_to_folio_files.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class AddSiteIdToFolioFiles < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :folio_files, :site, null: true, foreign_key: { to_table: :folio_sites }
-    change_column_null :folio_files, :site_id, false, (Folio::Site.first&.id || 0)
+  def up
+    unless column_exists?(:folio_files, :site_id)
+      add_reference :folio_files, :site, null: true, foreign_key: { to_table: :folio_sites }
+      change_column_null :folio_files, :site_id, false, (Folio::Site.first&.id || 0)
+    end
+  end
+
+  def down
+    remove_reference :folio_files, :site, if_exists: true
   end
 end
