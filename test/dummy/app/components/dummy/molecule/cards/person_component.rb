ed4f06fe63534c6f811bf790bde2dc55428ee453
Vlada <vladislav.trefil@protonmail.com>
2023-12-18 18:14:18 +0100

    feat(atoms): cards/person layout and style

--- test/dummy/app/components/dummy/molecule/cards/person_component.rb
+++ test/dummy/app/components/dummy/molecule/cards/person_component.rb
@@ -3,8 +3,15 @@
 class Dummy::Molecule::Cards::PersonComponent < ApplicationComponent
   include Folio::Molecule::CoverPlacements
 
+  THUMB_SIZE = "120x120#"
+  LARGE_THUMB_SIZE = "200x264#"
+
   def initialize(atoms:, atom_options: {})
     @atoms = atoms
     @atom_options = atom_options
+  end
+
+  def image_thumb_size(atom)
+    atom.large ? LARGE_THUMB_SIZE : THUMB_SIZE
   end
 end
