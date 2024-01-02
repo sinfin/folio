f5378d075e041b703b8fe91392dcab337a0e5813
David Duben <david.duben1@gmail.com>
2024-01-02 08:58:45 +0100

    header component tweak

--- test/dummy/app/components/dummy/ui/header_component.js
+++ test/dummy/app/components/dummy/ui/header_component.js
@@ -1,11 +1,13 @@
 window.Folio.Stimulus.register('d-ui-header', class extends window.Stimulus.Controller {
-  static targets = ['menu', 'toolbar']
-
-  toggleClass ({ detail: { value } }) {
+  toggleDropdownExpandedClass ({ detail: { value } }) {
     if (value) {
       this.element.classList.add("d-ui-header--toolbar-expanded")
     } else {
       this.element.classList.remove("d-ui-header--toolbar-expanded")
     }
+  }
+
+  toggleMenuOpenedClass () {
+    this.element.classList.toggle("d-ui-header--menu-opened")
   }
 })
