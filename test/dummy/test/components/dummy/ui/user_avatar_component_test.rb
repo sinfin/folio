373eadae109acaf72b0a6504cb15c59369ced0fe
David Duben <david.duben1@gmail.com>
2024-01-02 09:01:21 +0100

    user avatar component tweak

--- test/dummy/test/components/dummy/ui/user_avatar_component_test.rb
+++ test/dummy/test/components/dummy/ui/user_avatar_component_test.rb
@@ -4,9 +4,7 @@ require "test_helper"
 
 class Dummy::Ui::UserAvatarComponentTest < Folio::ComponentTest
   def test_render
-    model = "hello"
-
-    render_inline(Dummy::Ui::UserAvatarComponent.new(model:))
+    render_inline(Dummy::Ui::UserAvatarComponent.new)
 
     assert_selector(".d-ui-user-avatar")
   end
