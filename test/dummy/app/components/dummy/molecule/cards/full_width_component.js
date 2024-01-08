a8bbe08efbd85f8e2d49ee7440e20fbdee2adfff
Vlada <vladislav.trefil@protonmail.com>
2024-01-08 16:08:52 +0100

    style(lint)

--- test/dummy/app/components/dummy/molecule/cards/full_width_component.js
+++ test/dummy/app/components/dummy/molecule/cards/full_width_component.js
@@ -4,12 +4,12 @@ const DOT_ACTIVE_CLASS = 'd-molecule-cards-full-width__controls-dot--active'
 window.Folio.Stimulus.register('d-molecule-cards-full-width', class extends window.Stimulus.Controller {
   static targets = ['slide', 'controlDot']
 
-  connect() {
+  connect () {
     this.currentSlideIndex = 0
     this.updateSlider()
   }
 
-  updateSlider() {
+  updateSlider () {
     this.slideTargets.forEach((slide, index) => {
       if (index === this.currentSlideIndex) {
         slide.classList.add(SLIDE_ACTIVE_CLASS)
