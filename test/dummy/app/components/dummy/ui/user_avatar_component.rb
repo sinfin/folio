dd8af387c10e1edb73b5f357167bc4a2e012c9a1
Petr Marek <contact@petrmarek.eu>
2024-01-08 08:09:26 +0100

    update ui/user_avatar_component syntax

--- test/dummy/app/components/dummy/ui/user_avatar_component.rb
+++ test/dummy/app/components/dummy/ui/user_avatar_component.rb
@@ -1,16 +1,31 @@
 # frozen_string_literal: true
 
 class Dummy::Ui::UserAvatarComponent < ApplicationComponent
-  def initialize(not_link: false)
-    @not_link = not_link
+  def initialize(link: true, current_user_for_test: nil)
+    @link = link
+    @current_user_for_test = current_user_for_test
   end
 
   def data
-    stimulus_controller("d-ui-user-avatar", action: {
-      click: :clicked,
-      "keydown.enter": :clicked,
-      "keydown.esc": :clicked,
-      "d-ui-menu-toolbar-dropdown:closed@window": "dropdownClosed",
-    })
+    if @link
+      stimulus_controller("d-ui-user-avatar", action: {
+        click: :clicked,
+        "keydown.enter": "clicked",
+        "keydown.esc": "clicked",
+        "d-ui-menu-toolbar-dropdown:closed@window": "dropdownClosed",
+      })
+    end
+  end
+
+  def user_class_name
+    unless current_user_with_test_fallback
+      "d-ui-user-avatar--signed-out"
+    end
+  end
+
+  def link_class_name
+    unless @link
+      "d-ui-user-avatar--not-link"
+    end
   end
 end
