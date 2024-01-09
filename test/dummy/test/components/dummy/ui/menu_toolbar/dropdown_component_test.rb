e35ea19f99a8e0ea166d45b09d393476fea95eab
Petr Marek <contact@petrmarek.eu>
2024-01-09 14:33:36 +0100

    fix ui components tests

--- test/dummy/test/components/dummy/ui/menu_toolbar/dropdown_component_test.rb
+++ test/dummy/test/components/dummy/ui/menu_toolbar/dropdown_component_test.rb
@@ -4,7 +4,7 @@ require "test_helper"
 
 class Dummy::Ui::MenuToolbar::DropdownComponentTest < Folio::ComponentTest
   def test_render
-    render_inline(Dummy::Ui::MenuToolbar::DropdownComponent.new(type: :user_menu, signed_in: user_signed_in?))
+    render_inline(Dummy::Ui::MenuToolbar::DropdownComponent.new(type: :user_menu))
 
     assert_selector(".d-ui-menu-toolbar-dropdown")
   end
