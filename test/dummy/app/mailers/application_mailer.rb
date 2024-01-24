6c1babd0db1123535ce8bf108fb0b2d169e36cc5
Petr Marek <contact@petrmarek.eu>
2024-01-24 12:33:37 +0100

    add dummy/mailer#debug for styling

--- test/dummy/app/mailers/application_mailer.rb
+++ test/dummy/app/mailers/application_mailer.rb
@@ -1,8 +1,4 @@
 # frozen_string_literal: true
 
-class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com",
-          bcc: Rails.application.config.folio_mailer_global_bcc
-
-  layout "mailer"
+class ApplicationMailer < Folio::ApplicationMailer
 end
--- test/dummy/test/mailers/previews/folio/devise_mailer_preview.rb
+++ test/dummy/test/mailers/previews/folio/devise_mailer_preview.rb
@@ -2,14 +2,14 @@
 
 class Folio::DeviseMailerPreview < ActionMailer::Preview
   def reset_password_instructions
-    Folio::DeviseMailer.reset_password_instructions(Folio::Account.last,
+    Folio::DeviseMailer.reset_password_instructions(Folio::User.last,
                                                     "RESET_PASSWORD_TOKEN").tap do |email|
       Premailer::Rails::Hook.perform(email)
     end
   end
 
   def invitation_instructions
-    Folio::DeviseMailer.invitation_instructions(Folio::Account.last,
+    Folio::DeviseMailer.invitation_instructions(Folio::User.last,
                                                 "INVITATION_TOKEN").tap do |email|
       Premailer::Rails::Hook.perform(email)
     end
