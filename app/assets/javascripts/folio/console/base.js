7d4a85611dd6d957fb99f93d6e8cacc1f957cbf9
Petr Marek <contact@petrmarek.eu>
2024-01-25 14:50:28 +0100

    rm legacy private_attachments partials

--- app/assets/javascripts/folio/console/base.js
+++ app/assets/javascripts/folio/console/base.js
@@ -98,7 +98,6 @@
 //= require folio/console/modules/simple-form-with-atoms
 //= require folio/console/nested_model_controls/nested_model_controls
 //= require folio/console/new_record_modal/new_record_modal
-//= require folio/console/private_attachments/fields/fields
 //= require folio/console/private_attachments/single_dropzone/single_dropzone
 //= require folio/console/private_attachments_fields_component
 //= require folio/console/publishable_inputs/item/item
--- app/cells/folio/console/private_attachments/fields_cell.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class Folio::Console::PrivateAttachments::FieldsCell < Folio::ConsoleCell
-  DROPZONE_CLASS_NAME = "f-c-private-attachments-fields-dropzone"
-end
