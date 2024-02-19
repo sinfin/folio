132e90611efbfb8b60680fcf445ba801f7003d7f
dedekm <dedekmm@gmail.com>
2024-02-19 13:35:40 +0100

    add option to disable back button for console form footer

--- app/cells/folio/console/form/footer_cell.rb
+++ app/cells/folio/console/form/footer_cell.rb
@@ -2,15 +2,17 @@
 
 class Folio::Console::Form::FooterCell < Folio::ConsoleCell
   def back_path
+    return if options[:back_button] == false
+
     options[:back_path] || through_aware_console_url_for(model.object.class, safe: true)
   end
 
   def preview_path
-    return nil if options[:preview_button] == false
+    return if options[:preview_button] == false
     return options[:preview_path] if options[:preview_path]
 
-    return nil unless model
-    return nil unless model.object.persisted?
+    return unless model
+    return unless model.object.persisted?
 
     options[:preview_path] || preview_url_for(model.object)
   rescue NoMethodError
