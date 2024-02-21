082d0763a7e24d1377054f9ce8709492bf760546
David Duben <david.duben1@gmail.com>
2024-02-21 14:58:18 +0100

    lot card done

--- test/dummy/app/components/dummy/mailer/cards/lot_component.rb
+++ test/dummy/app/components/dummy/mailer/cards/lot_component.rb
@@ -1,4 +1,60 @@
 # frozen_string_literal: true
 
 class Dummy::Mailer::Cards::LotComponent < ApplicationComponent
+  THUMB_SIZE = "232x322#"
+
+  EVENT_TYPES = {
+    auction: "Aukce",
+    internet_auction: "Internetová aukce",
+    gallery: "Galerijní prodej"
+  }
+
+  AUCTION_STATUS = {
+    not: "Nevydraženo",
+    active: "Draží se",
+    finished: "Vydraženo",
+    upcoming: "Bude se dražit"
+  }
+
+  def initialize(folio_image: nil,
+                 author:,
+                 name:,
+                 event: nil,
+                 description: nil,
+                 id: nil,
+                 auction_status: nil,
+                 price: nil,
+                 auction_date: nil,
+                 limiting_end: nil,
+                 button_label: nil,
+                 button_href: nil)
+    @folio_image = folio_image
+    @author = author
+    @name = name
+    @event = event
+    @description = description
+    @id = id
+    @auction_status = auction_status
+    @price = price
+    @auction_date = auction_date
+    @limiting_end = limiting_end
+    @button_label = button_label
+    @button_href = button_href
+  end
+
+  def event_type
+    if @event[:type].present?
+      EVENT_TYPES[@event[:type]] || @event[:type].to_s
+    end
+  end
+
+  def description
+    @description.present? ? @description.values : nil
+  end
+
+  def auction_status
+    if @auction_status.present?
+      AUCTION_STATUS[@auction_status] || @auction_status.to_s
+    end
+  end
 end
