3c56b43917099dcfa6d51f4b3c1ea7f688799b05
jirkamotejl <motejl.jirka@gmail.com>
2024-03-01 14:01:08 +0100

    fix private_attachment_serializer expiring_url

--- app/serializers/folio/console/private_attachment_serializer.rb
+++ app/serializers/folio/console/private_attachment_serializer.rb
@@ -10,6 +10,6 @@ class Folio::Console::PrivateAttachmentSerializer
              :title
 
   attribute :expiring_url do |object|
-    object.file.remote_url(expires_in: 1.hour.from_now)
+    object.file.remote_url(expires: 1.hour.from_now)
   end
 end
