44f99d064eaca146ead3b7d27421d04448b924bb
Lipan Martin <martin.lipan@gmail.com>
2024-03-04 16:58:09 +0100

    Search results

--- test/dummy/app/components/dummy/searches/results_list_component.rb
+++ test/dummy/app/components/dummy/searches/results_list_component.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class Dummy::Searches::ResultsListComponent < ApplicationComponent
-  def initialize(search:)
+  def initialize(search)
     @search = search
   end
 
-  def show
+  def shows
     render if @search.present?
   end
 end
