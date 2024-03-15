03fc17698302e2d852001e1f095ff3da105b7985
Lipan Martin <martin.lipan@gmail.com>
2024-03-15 14:46:09 +0100

    Blog components fe - initial

--- test/dummy/app/cells/dummy/blog/articles/show_header_cell.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class Dummy::Blog::Articles::ShowHeaderCell < ApplicationCell
-  THUMB_SIZE = "700x460"
-end
--- test/dummy/app/components/dummy/blog/articles/show_header_component.rb
+++ test/dummy/app/components/dummy/blog/articles/show_header_component.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class Dummy::Blog::Articles::ShowHeaderComponent < ApplicationComponent
+  COVER_SIZE = "1184x565#"
+  AUTHOR_AVATAR_SIZE = "24x24#"
+
   def initialize(article:)
     @article = article
   end
+
 end
