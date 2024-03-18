3fb141c8326068cb35c86bd53bd77cb62dc9e3ac
David Duben <david.duben1@gmail.com>
2024-03-18 12:26:00 +0100

    refactor code for buttons

--- test/dummy/app/components/dummy/mailer/button_component.rb
+++ test/dummy/app/components/dummy/mailer/button_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Dummy::Mailer::ButtonComponent < ApplicationComponent
-  def initialize(label:, href:, type: nil, size: nil)
+  def initialize(label:, href: nil, type: nil, size: nil)
     @label = label
     @href = href
     @type = type
@@ -16,26 +16,19 @@ class Dummy::Mailer::ButtonComponent < ApplicationComponent
     @size || :md
   end
 
-  # [width, height, font-size]
-  def dimensions
-    case size
-    when :sm
-      %w[135 34 12]
-    when :md
-      %w[150 40 14]
-    end
+  def label
+    @label.present? ? @label : "Button label"
   end
 
-  def vml_start_code
-    code = '<v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="' + @href + '" style="height:' + dimensions[1] + "px;v-text-anchor:middle;width:" + dimensions[0] + 'px;" arcsize="60% "'
-    code += type == "primary" ? 'stroke="f" fillcolor="#000000">' : 'strokecolor="#000000" fillcolor="#f5f5f5">'
-    code += "<w:anchorlock/>"
-    code += type == "primary" ? "<center>" : '<center style="color:#000000;font-family:sans-serif;font-size:' + dimensions[2] + 'px;font-weight:bold;">' + @label + "</center></v:roundrect>"
+  def mso_padding_start
+    code = '<i style="mso-font-width:100%;mso-text-raise:100%" hidden>&emsp;</i><span style="mso-text-raise:50%;">'
 
     code.html_safe
   end
 
-  def vml_end_code
-    "</center></v:roundrect>".html_safe
+  def mso_padding_end
+    code = '</span><i style="mso-font-width:100%;" hidden>&emsp;&#8203;</i>'
+
+    code.html_safe
   end
 end
