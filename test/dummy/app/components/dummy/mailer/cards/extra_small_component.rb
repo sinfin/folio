a508f32a14100d5ea5ce61c2b4af477984398c74
David Duben <david.duben1@gmail.com>
2024-03-18 12:32:35 +0100

    fix components conditions, add links to cards

--- test/dummy/app/components/dummy/mailer/cards/extra_small_component.rb
+++ test/dummy/app/components/dummy/mailer/cards/extra_small_component.rb
@@ -3,8 +3,22 @@
 class Dummy::Mailer::Cards::ExtraSmallComponent < ApplicationComponent
   THUMB_SIZE = "80x80#"
 
-  def initialize(card: nil, even: false)
+  def initialize(card:, even: false)
     @card = card
     @even = even
+  end
+
+  def render?
+    @card.present?
+  end
+
+  def link_with_fallback_tag
+    base_class = "d-mailer-cards-extra-small__link"
+
+    if @card[:href].present?
+      { tag: :a, href: @href, class: base_class, rel: "noopener", target: "_blank" }
+    else
+      {}
+    end
   end
 end
--- test/dummy/app/components/dummy/mailer/cards/extra_small_container_component.rb
+++ test/dummy/app/components/dummy/mailer/cards/extra_small_container_component.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class Dummy::Mailer::Cards::ExtraSmallContainerComponent < ApplicationComponent
-  def initialize(cards: nil)
+  def initialize(cards:)
     @cards = cards
+  end
+
+  def render?
+    @cards.present?
   end
 end
