13861ab98421b6497613657b7d9ea3e5ebec0341
David Duben <david.duben1@gmail.com>
2024-03-18 19:32:36 +0100

    fix extra small container test

--- test/dummy/test/components/dummy/mailer/cards/extra_small_container_component_test.rb
+++ test/dummy/test/components/dummy/mailer/cards/extra_small_container_component_test.rb
@@ -4,9 +4,20 @@ require "test_helper"
 
 class Dummy::Mailer::Cards::ExtraSmallContainerComponentTest < Folio::ComponentTest
   def test_render
-    model = "hello"
+    cards = [
+              {
+                title: "Card headline",
+                text: "Subtitle",
+                href: "#",
+                image: Folio::File::Image.first,
+              },
+              {
+                title: "Card headline",
+                text: "Subtitle",
+              },
+            ]
 
-    render_inline(Dummy::Mailer::Cards::ExtraSmallContainerComponent.new(model:))
+    render_inline(Dummy::Mailer::Cards::ExtraSmallContainerComponent.new(cards:))
 
     assert_selector(".d-mailer-cards-extra-small-container")
   end
