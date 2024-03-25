34f90632dd0ee6840ad5c1cbde704a7dec6543df
dedekm <dedekmm@gmail.com>
2024-03-25 10:59:32 +0100

    set false as default value for folio_users_publicly_invitable

--- lib/folio/engine.rb
+++ lib/folio/engine.rb
@@ -91,7 +91,7 @@ module Folio
     config.folio_users_sign_out_everywhere = true
     config.folio_users_confirmable = false
     config.folio_users_confirm_email_change = true
-    config.folio_users_publicly_invitable = true
+    config.folio_users_publicly_invitable = false
     config.folio_users_use_address = true
     config.folio_users_omniauth_providers = %i[facebook google_oauth2 twitter2 apple]
     config.folio_users_after_ajax_sign_up_redirect = false
