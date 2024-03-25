e524e152bdc37df421b3afa75b5234c9296c7d32
Lipan Martin <martin.lipan@gmail.com>
2024-03-25 09:42:23 +0100

    Topics collapser mobile/desktop limits

--- test/dummy/app/components/dummy/blog/topics/list_component.rb
+++ test/dummy/app/components/dummy/blog/topics/list_component.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Dummy::Blog::Topics::ListComponent < ApplicationComponent
+  MAX_ITEMS_UNCOLLAPSED_MOBILE = 6
+  MAX_ITEMS_UNCOLLAPSED_DESKTOP = 20
+
   def initialize(topics:)
     @topics = topics
   end
