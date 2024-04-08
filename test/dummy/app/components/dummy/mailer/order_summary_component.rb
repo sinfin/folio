d091ee7de68ffe82c6b9bb1e5914672b50d34718
David Duben <david.duben1@gmail.com>
2024-04-08 16:41:31 +0200

    order summary component - fix version for outlook and unsupport media queries

--- test/dummy/app/components/dummy/mailer/order_summary_component.rb
+++ test/dummy/app/components/dummy/mailer/order_summary_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Dummy::Mailer::OrderSummaryComponent < ApplicationComponent
-  THUMB_SIZE = "70x70#"
+  THUMB_SIZE = "70x70"
 
   def initialize(title: nil, items:, total: nil, status: nil)
     @title = title
