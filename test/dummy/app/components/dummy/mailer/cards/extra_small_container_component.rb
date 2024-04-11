4c17174a5e91d289396e96590559cde35359c2b1
David Duben <david.duben1@gmail.com>
2024-04-11 15:02:03 +0200

    responsivity for xs cards, fix mq for cards

--- test/dummy/app/components/dummy/mailer/cards/extra_small_container_component.rb
+++ test/dummy/app/components/dummy/mailer/cards/extra_small_container_component.rb
@@ -11,9 +11,9 @@ class Dummy::Mailer::Cards::ExtraSmallContainerComponent < ApplicationComponent
 
   def card_wrapper_class(index)
     if index.even?
-      "d-mailer-cards-extra-small-container__card-wrapper--even"
+      "d-mailer-cards-extra-small-container__card-wrapper--even xs-card-even-col"
     else
-      "d-mailer-cards-extra-small-container__card-wrapper--odd"
+      "d-mailer-cards-extra-small-container__card-wrapper--odd xs-card-odd-col"
     end
   end
 end
