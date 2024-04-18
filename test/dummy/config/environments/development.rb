fd269a1e11697f52a64c601634db1e1d72ccea7e
David Duben <david.duben1@gmail.com>
2024-04-18 22:28:57 +0200

    fix secret leak (password is already changed)

--- test/dummy/config/environments/development.rb
+++ test/dummy/config/environments/development.rb
@@ -36,12 +36,12 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:         "smtp.gmail.com",
-    port:            587,
-    domain:          "gmail.com",
-    user_name:       "sinfin.testing@gmail.com",
-    password:        "rjoz hvdj bkwq hizu",
-    authentication:  "plain" }
+    address:         ENV["SMTP_ADDRESS"],
+    port:            ENV["SMTP_PORT"],
+    domain:          ENV["SMTP_DOMAIN"],
+    user_name:       ENV["SMTP_USERNAME"],
+    password:        ENV["SMTP_PASSWORD"],
+    authentication:  ENV["SMTP_AUTHENTICATION"] }
 
   config.action_mailer.default_url_options = { host: "localhost", port: ENV["PORT"].presence || 3000, protocol: "http" }
 
