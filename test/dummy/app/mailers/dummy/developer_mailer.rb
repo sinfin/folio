422130b8e890e013e9ffa836085718ec4d37b0df
David Duben <david.duben1@gmail.com>
2024-04-18 13:00:30 +0200

    set emails in developer mailer

--- test/dummy/app/mailers/dummy/developer_mailer.rb
+++ test/dummy/app/mailers/dummy/developer_mailer.rb
@@ -2,6 +2,27 @@
 
 class Dummy::DeveloperMailer < ApplicationMailer
   def debug
-    mail to: "foo@bar.baz"
+    # Litmus - Sinfin
+    # @recipient = "sinfin.9e34@litmusemail.com"
+
+    # Litmus - David
+    # @recipient = "davidduben1.e423@litmusemail.com"
+
+    # Outlook
+    # @recipient = "test.sinfin@outlook.cz"
+
+    # Gmail
+    @recipient = "david.duben@sinfin.cz"
+
+    # Seznam
+    # @recipient = "duben4@seznam.cz"
+
+    # Centrum
+    # @recipient = "best.test@centrum.cz"
+
+    # Denis
+    # @recipient = "denis@sinfin.cz"
+
+    mail to: @recipient, from: "Folio Test", subject: "Test"
   end
 end
