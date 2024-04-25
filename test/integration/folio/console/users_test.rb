41bb8df7b38fe950bf67fcf7162db5ba12066257
Petr Mlčoch <foton@centrum.cz>
2024-04-25 08:55:03 +0200

    test fix

--- test/integration/folio/console/users_test.rb
+++ test/integration/folio/console/users_test.rb
@@ -6,14 +6,11 @@ class Folio::UsersTest < Folio::Console::BaseControllerTest
   attr_reader :auth_allowed_user, :auth_forbidden_user
 
   def setup
+    Folio::Current.original_reset
     create_and_host_site
     @auth_allowed_user = create(:folio_user, password: "password")
     @auth_forbidden_user = create(:folio_user, password: "password")
     create(:folio_site_user_link, site: @site, locked_at: Time.current, user: auth_forbidden_user)
-
-   # assert auth_allowed_user.active_for_authentication?
-
-   # assert_not auth_forbidden_user.active_for_authentication?
  end
 
   test "sign_in dis/allowed" do
@@ -36,10 +33,33 @@ class Folio::UsersTest < Folio::Console::BaseControllerTest
 
     assert_redirected_to new_user_session_url
     follow_redirect!
-    assert_select ".d-ui-flash", text: "Váš účet byl zamknutý. Prosím kontaktujte správce."
+    assert_select ".d-ui-flash", text: "Váš účet byl uzamčen. Prosím kontaktujte správce."
     assert_select ".user_email", text: "E-mail#{auth_forbidden_user.email}", count: 0
   end
 
   test "correct tab in console/users" do
+    superadmin = create(:folio_user, :superadmin)
+    sign_in superadmin
+
+    get console_users_url
+
+    assert_response :success
+    skip "not yet implemented"
+
+    assert_select ".folio-console-users-tab", text: "Active"
+    assert_select ".folio-console-users__user_mail", text: @auth_allowed_user.email
+
+    get console_users_url(tab: "locked")
+
+    assert_select ".folio-console-users-tab", text: "Locked"
+    assert_select ".folio-console-users__user_mail", text: @auth_forbidden_user.email
+
+    get console_users_url(tab: "active")
+
+    assert_select ".folio-console-users-tab", text: "Active"
+    assert_select ".folio-console-users__user_mail", text: @auth_allowed_user.email
+
+
+
   end
 end
