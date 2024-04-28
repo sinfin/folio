f82719e0c71b796c54eac4508c0a690e8db01773
David Duben <david.duben1@gmail.com>
2024-04-29 00:10:57 +0200

    image_and_content component - add new options

--- test/dummy/app/components/dummy/atom/image_and_content_component.rb
+++ test/dummy/app/components/dummy/atom/image_and_content_component.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 class Dummy::Atom::ImageAndContentComponent < ApplicationComponent
-  THUMB_SIZE = "648x450#"
-
-  bem_class_name :image_right, :with_link
+  bem_class_name :image_right, :with_link, :centered_content, :background
 
   def initialize(atom:, atom_options: {})
     @atom = atom
     @atom_options = atom_options
     @image_right = @atom.image_side_with_fallback == "right"
     @with_link = @atom.url.present?
+    @thumb_size = @atom.thumb_size_with_fallback
+    @background = @atom.wrapper_with_fallback == "background"
+    @dark_mode = @atom.color_mode_with_fallback == "dark"
+    @centered_content = @atom.vertically_centered_content
   end
 
   def data
