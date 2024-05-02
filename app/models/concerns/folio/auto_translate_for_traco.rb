9322c9e8b105927d46719515101ab0a4d9d70a7f
jirkamotejl <motejl.jirka@gmail.com>
2024-05-02 15:35:27 +0200

    auto translate traco columns using deepl free api

---
+++ app/models/concerns/folio/auto_translate_for_traco.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require "deepl"
+
+module Folio::AutoTranslateForTraco
+  extend ActiveSupport::Concern
+
+  included do
+    DeepL.configure do |config|
+      config.auth_key = ENV.fetch("DEEPL_API_KEY", nil)
+      config.host = ENV.fetch("DEEPL_API_HOST", "https://api-free.deepl.com")
+    end
+  end
+
+  class_methods do
+    def auto_translates(*attributes)
+      attributes.each do |attribute|
+        define_method("#{attribute}=") do |val|
+          I18n.available_locales.each do |l|
+            return if send("#{attribute}_#{l}").present?
+
+            translated = ::DeepL.translate(val, I18n.locale, l).text
+            send("#{attribute}_#{l}=", translated)
+
+          rescue ::DeepL::Exceptions::RequestError, ::DeepL::Exceptions::NotSupported, DeepL::Exceptions::Error => e
+            Rails.logger.error("DeepL error: #{e.message}")
+            send("#{attribute}_#{l}=", val)
+          end
+
+          translate_slugs_if_new_record
+        end
+      end
+    end
+  end
+
+  def translate_slugs_if_new_record
+    return unless new_record?
+
+    current_locale = I18n.locale
+    I18n.available_locales.each do |l|
+      next if l == current_locale
+
+      I18n.with_locale(l) { set_slug }
+    end
+  end
+end
