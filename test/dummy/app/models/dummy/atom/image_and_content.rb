5e71597a2072b19622f6f9f2a0758d1a8baa40dc
David Duben <david.duben1@gmail.com>
2024-05-05 18:53:02 +0200

    change model attributes

--- test/dummy/app/models/dummy/atom/image_and_content.rb
+++ test/dummy/app/models/dummy/atom/image_and_content.rb
@@ -1,14 +1,31 @@
 # frozen_string_literal: true
 
 class Dummy::Atom::ImageAndContent < Folio::Atom::Base
+  ALLOWED_THUMB_SIZES = {
+    "origin" => "648x",
+    "1x1" => "648x648#",
+    "3x2" => "648x432#",
+    "16x9" => "648x365#",
+    "4x3" => "648x486#",
+    "9x16" => "648x1152#",
+    "3x4" => "648x864#",
+    "2x3" => "648x972#",
+  }
+
   ATTACHMENTS = %i[cover]
 
   STRUCTURE = {
     title: :string,
+    subtitle: :string,
     content: :richtext,
+    image_ratio: ALLOWED_THUMB_SIZES.keys,
+    vertically_centered_content: :boolean,
     button_label: :string,
     url: :url,
-    image_side: %w[left right]
+    open_in_new_tab: :boolean,
+    image_side: %w[left right],
+    wrapper: %w[none background outline],
+    color_mode: %w[light dark],
   }
 
   ASSOCIATIONS = {}
@@ -18,8 +35,14 @@ class Dummy::Atom::ImageAndContent < Folio::Atom::Base
 
   validate :validate_one_of_contents
 
-  def image_side_with_fallback
-    image_side.presence || "left"
+  validate :validate_color_mode
+
+  def thumb_size_with_fallback
+    ALLOWED_THUMB_SIZES[image_ratio.presence || "1x1"]
+  end
+
+  def self.default_atom_values
+    { image_ratio: "1x1" }
   end
 
   private
@@ -28,6 +51,12 @@ class Dummy::Atom::ImageAndContent < Folio::Atom::Base
         errors.add(:content, :blank)
       elsif button_label.present? && url.blank?
         errors.add(:url, :blank)
+      end
+    end
+
+    def validate_color_mode
+      if wrapper == "none" && color_mode == "dark"
+        errors.add(:color_mode, :color_mode_without_wrapper)
       end
     end
 end
