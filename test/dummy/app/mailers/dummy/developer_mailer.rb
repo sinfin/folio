1c7416d7a8b46ca03253e74777892ae5a5e59fed
David Duben <david.duben1@gmail.com>
2024-05-14 00:20:14 +0200

    delete recipients from developer mailer

--- test/dummy/app/mailers/dummy/developer_mailer.rb
+++ test/dummy/app/mailers/dummy/developer_mailer.rb
@@ -8,26 +8,8 @@ class Dummy::DeveloperMailer < ApplicationMailer
     # Litmus - Sinfin (paused account)
     # @recipients << "sinfin.9e34@litmusemail.com"
 
-    # Litmus - David (inactive account)
-    # @recipients << "davidduben1.e423@litmusemail.com"
-
-    # Email on Acid - David (please dont use this address (only one possible preview left))
-    # @recipients << "DavidTheTester+default@precheck.emailonacid.com"
-
-    # Outlook
-    # @recipients << "test.sinfin@outlook.cz"
-
-    # Outlook 2. account
-    # @recipients << "test.sinfin@outlook.com"
-
     # Gmail
     @recipients << "david.duben@sinfin.cz"
-
-    # Seznam
-    # @recipients << "duben4@seznam.cz"
-
-    # Centrum
-    # @recipients << "best.test@centrum.cz"
 
     # Denis
     # @recipients << "denis@sinfin.cz"
