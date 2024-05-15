5351a8880d67fc43d9fb9b11f093c124b8c41c72
David Duben <david.duben1@gmail.com>
2024-05-15 12:14:43 +0200

    tweak even and odd classes

--- test/dummy/app/components/dummy/mailer/cards/extra_small_container_component.rb
+++ test/dummy/app/components/dummy/mailer/cards/extra_small_container_component.rb
@@ -11,9 +11,9 @@ class Dummy::Mailer::Cards::ExtraSmallContainerComponent < ApplicationComponent
 
   def card_wrapper_class(index)
     if index.even?
-      "d-mailer-cards-extra-small-container__card-wrapper--even d-mailer-cards-extra-small-container__even-column"
+      "d-mailer-cards-extra-small-container__even-column"
     else
-      "d-mailer-cards-extra-small-container__card-wrapper--odd d-mailer-cards-extra-small-container__odd-column"
+      "d-mailer-cards-extra-small-container__odd-column"
     end
   end
 end
