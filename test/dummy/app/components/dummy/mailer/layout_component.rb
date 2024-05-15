9afb848c4b0b3b56ec0f8b44cc1c5a4971beac8c
David Duben <david.duben1@gmail.com>
2024-05-15 12:55:41 +0200

    fix preview_text_component

--- test/dummy/app/components/dummy/mailer/layout_component.rb
+++ test/dummy/app/components/dummy/mailer/layout_component.rb
@@ -4,4 +4,9 @@ class Dummy::Mailer::LayoutComponent < ApplicationComponent
   def initialize(site:)
     @site = site
   end
+
+  def preview_text
+    # Preview text must be at least 90 characters long
+    "Some hidden preview text. Should be minimal 90 characters long. Lorem ipsum dolor sit amet, lorem ipsum"
+  end
 end
--- test/dummy/app/components/dummy/mailer/preview_text_component.rb
+++ test/dummy/app/components/dummy/mailer/preview_text_component.rb
@@ -1,15 +1,11 @@
 # frozen_string_literal: true
 
 class Dummy::Mailer::PreviewTextComponent < ApplicationComponent
-  def initialize(text: nil)
+  def initialize(text:)
     @text = text
   end
 
-  def preview_text
-    if @text.present?
-      @text
-    else
-      "Some hidden preview text. Should be minimal 90 characters long. Lorem ipsum dolor sit amet, lorem ipsum"
-    end
+  def render?
+    @text.present?
   end
 end
