7ef0877a2699ae441c9123977e0d638cd0500609
Petr Marek <contact@petrmarek.eu>
2024-05-22 13:32:39 +0200

    respect per-hash skip_can_now  in console/index/actions

--- app/cells/folio/console/index/actions_cell.rb
+++ app/cells/folio/console/index/actions_cell.rb
@@ -68,7 +68,7 @@ class Folio::Console::Index::ActionsCell < Folio::ConsoleCell
       elsif sym_or_hash.is_a?(Hash)
         sym_or_hash.each do |name, obj|
           next if name == :destroy && model.class.try(:indestructible?)
-          next if !options[:skip_can_now] && !controller.can_now?(name, model)
+          next if (!options[:skip_can_now] && !obj[:skip_can_now]) && !controller.can_now?(name, model)
           base = default_actions[name].presence || {}
           if obj.is_a?(Hash)
             acts << base.merge(obj)
