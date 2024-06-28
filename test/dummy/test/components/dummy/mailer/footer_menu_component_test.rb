f4b2def46fe65ba2e2d50783490379093a84c301
Petr Marek <contact@petrmarek.eu>
2024-06-28 07:12:50 +0200

    add navigation menu STI to templates

--- test/dummy/test/components/dummy/mailer/footer_menu_component_test.rb
+++ test/dummy/test/components/dummy/mailer/footer_menu_component_test.rb
@@ -5,6 +5,7 @@ require "test_helper"
 class Dummy::Mailer::FooterMenuComponentTest < Folio::ComponentTest
   def test_render
     site = create(:folio_site)
+    create(:economia_menu_footer)
 
     render_inline(Dummy::Mailer::FooterMenuComponent.new(site:))
 
