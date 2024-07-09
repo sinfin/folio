28152e3ac133c334d780498b41b62873d71d3cf6
Tomáš Cabák <tomascabak00@gmail.com>
2024-07-09 13:23:18 +0200

    add bootstrap tooltip to folio and folio console

---
+++ app/assets/javascripts/folio/tooltip.js
@@ -0,0 +1,20 @@
+window.Folio.Stimulus.register('f-tooltip', class extends window.Stimulus.Controller {
+  static values = {
+    title: String,
+    placement: { type: String, default: 'auto' },
+    trigger: { type: String, default: 'hover' },
+  }
+
+  connect () {
+    this.tooltip = new window.bootstrap.Tooltip(this.element, {
+      title: this.titleValue,
+      placement: this.placementValue,
+      trigger: this.triggerValue,
+    })
+  }
+
+  disconnect () {
+    this.tooltip.dispose()
+    delete this.tooltip
+  }
+})
