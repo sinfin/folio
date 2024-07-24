27ed09201ae9551308a7a5c2628078c3a7ff9b61
Petr Marek <contact@petrmarek.eu>
2024-07-24 10:31:11 +0200

    rework articles/show_header to use ui/hero

--- test/dummy/app/components/dummy/blog/articles/show_header_component.rb
+++ test/dummy/app/components/dummy/blog/articles/show_header_component.rb
@@ -11,4 +11,11 @@ class Dummy::Blog::Articles::ShowHeaderComponent < ApplicationComponent
   def photo_author
     @photo_author ||= @article.cover_placement.try(:file).try(:author)
   end
+
+  def hero_component
+    render(Dummy::Ui::HeroComponent.new(title: @article.title,
+                                        perex: @article.perex,
+                                        cover: @article.cover_placement,
+                                        date: @article.published_at_with_fallback.to_date))
+  end
 end
