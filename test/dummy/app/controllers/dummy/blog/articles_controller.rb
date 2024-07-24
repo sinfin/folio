2ba293b7b5589638bd8e1233cfa429173ebca3b1
Petr Marek <contact@petrmarek.eu>
2024-07-24 07:37:00 +0200

    refactor blog controllers, add caching

--- test/dummy/app/controllers/dummy/blog/articles_controller.rb
+++ test/dummy/app/controllers/dummy/blog/articles_controller.rb
@@ -1,44 +1,40 @@
 # frozen_string_literal: true
 
 class Dummy::Blog::ArticlesController < ApplicationController
-  before_action { @klass = Dummy::Blog::Article }
-  before_action :find_article, only: [:show, :preview]
-
   def index
-    articles = @klass.published
-                     .ordered
-                     .by_locale(I18n.locale)
-                     .includes(:published_topics,
-                               cover_placement: :file)
+    folio_run_unless_cached(["blog/articles#index"] + cache_key_base) do
+      articles = Dummy::Blog::Article.published
+                                     .ordered
+                                     .by_locale(I18n.locale)
+                                     .includes(Dummy::Blog.article_includes)
 
-    @pagy, @articles = pagy(articles, items: 12)
+      @pagy, @articles = pagy(articles, items: Dummy::Blog::ARTICLE_PAGY_ITEMS)
 
-    @topics = Dummy::Blog::Topic.published
-                                .by_locale(I18n.locale)
-                                .with_published_articles
-                                .ordered
-                                .limit(20)
+      @topics = Dummy::Blog::Topic.published
+                                  .by_locale(I18n.locale)
+                                  .with_published_articles
+                                  .ordered
+                                  .limit(20)
+    end
   end
 
   def show
-    force_correct_path(url_for(@article))
-
-    articles = @klass.published
-                     .ordered
-                     .by_locale(I18n.locale)
-                     .includes(:published_topics,
-                               cover_placement: :file)
-
-    @articles = pagy(articles, items: 3)
-  end
-
-  private
-    def find_article
-      @article = @klass.published_or_preview_token(params[Folio::Publishable::PREVIEW_PARAM_NAME])
-                       .by_locale(I18n.locale)
-                       .includes(cover_placement: :file)
-                       .friendly.find(params[:id])
+    folio_run_unless_cached(["blog/articles#show", params[:id]] + cache_key_base) do
+      @article = Dummy::Blog::Article.published_or_preview_token(params[Folio::Publishable::PREVIEW_PARAM_NAME])
+                                     .by_locale(I18n.locale)
+                                     .friendly.find(params[:id])
 
       set_meta_variables(@article)
+
+      force_correct_path(url_for(@article))
+
+      articles = Dummy::Blog::Article.published
+                                     .ordered
+                                     .where.not(id: @article.id)
+                                     .by_locale(I18n.locale)
+                                     .includes(Dummy::Blog.article_includes)
+
+      @articles = pagy(articles, items: 3)
     end
+  end
 end
--- test/dummy/app/controllers/dummy/blog/topics_controller.rb
+++ test/dummy/app/controllers/dummy/blog/topics_controller.rb
@@ -1,24 +1,19 @@
 # frozen_string_literal: true
 
 class Dummy::Blog::TopicsController < ApplicationController
-  before_action { @klass = Dummy::Blog::Topic }
-  before_action :find_topic, only: [:show, :preview]
-
   def show
-    force_correct_path(url_for(@topic))
-  end
-
-  private
-    def find_topic
-      @topic = @klass.published_or_preview_token(params[Folio::Publishable::PREVIEW_PARAM_NAME])
-                     .by_locale(I18n.locale)
-                     .friendly.find(params[:id])
+    folio_run_unless_cached(["blog/topics#index", params[:page]] + cache_key_base) do
+      @topic = Dummy::Blog::Topic.published_or_preview_token(params[Folio::Publishable::PREVIEW_PARAM_NAME])
+                                 .by_locale(I18n.locale)
+                                 .friendly.find(params[:id])
 
       articles = @topic.published_articles
                        .ordered
-                       .includes(:published_topics,
-                                 cover_placement: :file)
+                       .includes(Dummy::Blog.article_includes)
 
-      @pagy, @articles = pagy(articles, items: 12)
+      @pagy, @articles = pagy(articles, items: Dummy::Blog::ARTICLE_PAGY_ITEMS)
+
+      force_correct_path(url_for(@topic))
     end
+  end
 end
