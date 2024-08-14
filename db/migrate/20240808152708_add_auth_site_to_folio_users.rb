bc2c618c9dcd66395b3f046de249803d95e3d925
Petr Mlčoch <foton@centrum.cz>
2024-08-14 12:50:14 +0200

    fix for empty db

--- db/migrate/20240808152708_add_auth_site_to_folio_users.rb
+++ db/migrate/20240808152708_add_auth_site_to_folio_users.rb
@@ -3,7 +3,7 @@
 class AddAuthSiteToFolioUsers < ActiveRecord::Migration[7.1]
   def change
     add_reference :folio_users, :auth_site, null: true, foreign_key: { to_table: :folio_sites }
-    execute "UPDATE folio_users SET auth_site_id = #{Folio.main_site.id}"
+    execute "UPDATE folio_users SET auth_site_id = #{Folio.main_site.id}" if Folio::User.any?
     change_column_null :folio_users, :auth_site_id, false
   end
 end
