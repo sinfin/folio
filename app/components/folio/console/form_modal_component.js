80690facd8671d4899be6ad19c2cbd425647a8ff
Tomáš Cabák <tomascabak00@gmail.com>
2024-08-15 11:26:35 +0200

    set hash and reload page after form modal submit

--- app/components/folio/console/form_modal_component.js
+++ app/components/folio/console/form_modal_component.js
@@ -73,10 +73,12 @@ window.Folio.Stimulus.register('f-c-form-modal', class extends window.Stimulus.C
         } else if (json && json.data && json.data.redirected) {
           this.keepLoadingClass = true
           if (typeof json.data.redirected === 'string') {
-            window.location.href = json.data.redirected
-          } else {
-            window.location.reload()
+            const hash = new URL(json.data.redirected).hash
+            if (hash) {
+              window.location.hash = hash
+            }
           }
+          window.location.reload()
         } else {
           throw new Error('Invalid response - JSON missing data/success or data/state_html')
         }
