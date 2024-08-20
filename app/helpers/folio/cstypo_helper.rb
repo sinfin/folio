8bd9bdc424f95c4ea0ea62903e4aab190cce4e09
Petr Marek <contact@petrmarek.eu>
2024-08-20 06:30:50 +0200

    add replace_newlines_with_br kwarg to cstypo
    close #288

--- app/helpers/folio/cstypo_helper.rb
+++ app/helpers/folio/cstypo_helper.rb
@@ -3,12 +3,21 @@
 module Folio::CstypoHelper
   CSTYPO_REGEXP = /(?<![<\/])\b([szkvaiou%]\b) /i
 
-  def cstypo(string)
-    if string.present? && string.is_a?(String) && I18n.locale == :cs
-      string.gsub(CSTYPO_REGEXP, '\1&nbsp;')
-            .gsub(/(\d+)\s+(\d+)/, '\1&nbsp;\2')
-            .gsub(/(\d+)\s+(Kč)/, '\1&nbsp;\2')
-            .html_safe
+  def cstypo(string, replace_newlines_with_br: false)
+    if string.present? && string.is_a?(String)
+      runner = string
+
+      if replace_newlines_with_br
+        runner = runner.gsub(/(\r\n|\n)/, "<br>")
+      end
+
+      if I18n.locale == :cs
+        runner = runner.gsub(CSTYPO_REGEXP, '\1&nbsp;')
+                       .gsub(/(\d+)\s+(\d+)/, '\1&nbsp;\2')
+                       .gsub(/(\d+)\s+(Kč)/, '\1&nbsp;\2')
+      end
+
+      runner.html_safe
     else
       string
     end
--- test/helpers/folio/cstypo_helper_test.rb
+++ test/helpers/folio/cstypo_helper_test.rb
@@ -9,6 +9,12 @@ class Folio::CstypoHelperTest < ActiveSupport::TestCase
     I18n.with_locale(:cs) do
       assert_equal('Zdá vyšších ruky <a href="#">toto</a> David listu, činná <b>ruském</b> byly zda ilustrační, šimpanzí za 3&nbsp;000&nbsp;Kč k&nbsp;zimu vy míst já budou s&nbsp;z&nbsp;špatného likvidaci.',
                    cstypo('Zdá vyšších ruky <a href="#">toto</a> David listu, činná <b>ruském</b> byly zda ilustrační, šimpanzí za 3 000 Kč k zimu vy míst já budou s z špatného likvidaci.'))
+
+      assert_equal("Za 3&nbsp;000&nbsp;Kč\nk&nbsp;zimu",
+                   cstypo("Za 3 000 Kč\nk zimu", replace_newlines_with_br: false))
+
+      assert_equal("Za 3&nbsp;000&nbsp;Kč<br>k&nbsp;zimu",
+                   cstypo("Za 3 000 Kč\nk zimu", replace_newlines_with_br: true))
     end
   end
 
@@ -16,6 +22,12 @@ class Folio::CstypoHelperTest < ActiveSupport::TestCase
     I18n.with_locale(:en) do
       assert_equal("Zdá vyšších ruky toto David listu, činná ruském byly zda ilustrační, šimpanzí za 3 000 Kč k zimu vy míst já budou s z špatného likvidaci.",
                    cstypo("Zdá vyšších ruky toto David listu, činná ruském byly zda ilustrační, šimpanzí za 3 000 Kč k zimu vy míst já budou s z špatného likvidaci."))
+
+      assert_equal("Za 3 000 Kč\nk zimu",
+                   cstypo("Za 3 000 Kč\nk zimu", replace_newlines_with_br: false))
+
+      assert_equal("Za 3 000 Kč<br>k zimu",
+                   cstypo("Za 3 000 Kč\nk zimu", replace_newlines_with_br: true))
     end
   end
 end
