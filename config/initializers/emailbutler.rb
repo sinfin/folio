88aa8d58d300f43c819f8acaa67af8ec30035a04
dedekm <dedekmm@gmail.com>
2024-09-13 15:21:41 +0200

    fix emailbutler config

--- config/initializers/emailbutler.rb
+++ config/initializers/emailbutler.rb
@@ -5,4 +5,6 @@ require "emailbutler/adapters/active_record"
 Emailbutler.configure do |config|
   config.adapter = Emailbutler::Adapters::ActiveRecord.new
   config.providers = %w[sendgrid]
+  config.ui_username = ""
+  config.ui_password = ""
 end
