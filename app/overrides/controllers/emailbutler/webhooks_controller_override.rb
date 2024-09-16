02a4b1cedae9ec0509bed4fa9a57c69cc1c8179e
dedekm <dedekmm@gmail.com>
2024-09-16 17:56:02 +0200

    fix sendgrid webhook signature verification

--- app/overrides/controllers/emailbutler/webhooks_controller_override.rb
+++ app/overrides/controllers/emailbutler/webhooks_controller_override.rb
@@ -5,12 +5,28 @@ Emailbutler::WebhooksController.class_eval do
 
   private
     def verify_signature
-      head :unauthorized unless ENV["SMTP_WEBHOOK_SIGNATURE"] == request.headers["x-twilio-email-event-webhook-signature"]
+      if signature = request.headers["X-Twilio-Email-Event-Webhook-Signature"]
+        timestamp = request.headers["X-Twilio-Email-Event-Webhook-Timestamp"]
+        public_key = OpenSSL::PKey::EC.new(Base64.decode64(ENV.fetch("SENDGRID_WEBHOOK_PUBLIC_KEY")))
+        timestamped_playload = "#{timestamp}#{request.body.read}"
+        payload_digest = Digest::SHA256.digest(timestamped_playload)
+        decoded_signature = Base64.decode64(signature)
+
+        begin
+          public_key.dsa_verify_asn1(payload_digest, decoded_signature)
+        rescue
+          head :unauthorized
+        end
+      end
     end
 
     def sendgrid_params
       p = params.permit("_json" => %w[smtp-id event timestamp sg_message_id])
-      p[:_json].map! { |h| h["smtp-id"].delete!("<>"); h.to_h }
+
+      if p[:_json].present?
+        p[:_json].map! { |h| h["smtp-id"].delete!("<>"); h.to_h }
+      end
+
       p
     end
 end
