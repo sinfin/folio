424c618c81c2d4309c7fe95689b8bf00906af3be
Petr Marek <contact@petrmarek.eu>
2024-10-14 11:30:42 +0200

    add charactersWithSpaces to word_count js, handle nbsp and other entities

--- app/assets/javascripts/folio/word_count.js
+++ app/assets/javascripts/folio/word_count.js
@@ -6,18 +6,25 @@ window.Folio.wordCount = ({ text }) => {
     cleanText: '',
     words: 0,
     characters: 0,
+    charactersWithSpaces: 0,
     formattedWords: 0,
-    formattedCharacters: 0
+    formattedCharacters: 0,
+    formattedCharactersWithSpaces: 0
   }
 
   if (text) {
-    hash.cleanText = text.replace(/<[^>]*>?/gm, ' ').trim().replace(/\s+/g, ' ')
-    hash.characters = hash.cleanText.length
-    hash.words = hash.cleanText.split(/\s+/).length
+    hash.cleanText = text.replace(/<[^>]*>?/gm, ' ').trim().replace(/(\s|&\w+;)+/g, ' ')
+
+    const textWithoutSpaces = hash.cleanText.replace(/ /g, '')
+
+    hash.charactersWithSpaces = hash.cleanText.length
+    hash.characters = textWithoutSpaces.length
+    hash.words = hash.cleanText.split(' ').length
 
     const formatter = new Intl.NumberFormat('en-US', { maximumFractionDigits: 0, useGrouping: true })
 
     hash.formattedCharacters = formatter.format(hash.characters).replace(/,/g, ' ')
+    hash.formattedCharactersWithSpaces = formatter.format(hash.charactersWithSpaces).replace(/,/g, ' ')
     hash.formattedWords = formatter.format(hash.words).replace(/,/g, ' ')
   }
 
