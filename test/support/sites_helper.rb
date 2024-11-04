f6d5f9f9e605ca3717a3d72d372c96f3e10f8bca
Petr Marek <contact@petrmarek.eu>
2024-11-04 09:24:58 +0100

    use env_aware_domain in test host_site

--- test/support/sites_helper.rb
+++ test/support/sites_helper.rb
@@ -25,12 +25,12 @@ module SitesHelper
     end
 
     def host_site(site)
-      Rails.application.routes.default_url_options[:host] = site.domain
+      Rails.application.routes.default_url_options[:host] = site.env_aware_domain
       Rails.application.routes.default_url_options[:only_path] = false
       Folio::Current.site = site
 
       if self.respond_to?(:host!)
-        host!(site.domain)
+        host!(site.env_aware_domain)
       end
       Folio.instance_variable_set(:@main_site, nil) # to clear the cached version from other tests
       @site = site
