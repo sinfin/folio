cddcffb04f6d0804e407fad48cbf1b9bfc59292f
Petr Mlčoch <foton@centrum.cz>
2024-11-07 09:07:02 +0100

    fixed flaky state cell test

--- test/cells/folio/console/state_cell_test.rb
+++ test/cells/folio/console/state_cell_test.rb
@@ -6,7 +6,7 @@ class Folio::Console::StateCellTest < Folio::Console::CellTest
   test "show displays no events without current user" do
     lead = create(:folio_lead)
 
-    html = cell("folio/console/state", lead).(:show)
+    html = I18n.with_locale(:cs) { cell("folio/console/state", lead).(:show) }
 
     state_div = html.find(".f-c-state", text: "K vyřízení")
     assert state_div.present?
@@ -19,7 +19,7 @@ class Folio::Console::StateCellTest < Folio::Console::CellTest
     assert_equal [:to_pending, :to_handled], lead.permitted_event_names
     Folio::Current.user = create(:folio_site_user_link, roles: [], site: lead.site).user
 
-    html = cell("folio/console/state", lead).(:show)
+    html = I18n.with_locale(:cs) { cell("folio/console/state", lead).(:show) }
 
     state_div = html.find(".f-c-state", text: "K vyřízení")
     assert state_div.present?
@@ -32,7 +32,7 @@ class Folio::Console::StateCellTest < Folio::Console::CellTest
     assert_equal [:to_pending, :to_handled], lead.permitted_event_names
     Folio::Current.user = create(:folio_site_user_link, roles: [:administrator], site: lead.site).user
 
-    html = cell("folio/console/state", lead).(:show)
+    html = I18n.with_locale(:cs) { cell("folio/console/state", lead).(:show) }
 
     state_div = html.find(".f-c-state", text: "K vyřízení")
     assert state_div.present?
@@ -42,7 +42,8 @@ class Folio::Console::StateCellTest < Folio::Console::CellTest
 
     lead.to_handled!
 
-    html = cell("folio/console/state", lead).(:show)
+    html = I18n.with_locale(:cs) { cell("folio/console/state", lead).(:show) }
+
     state_div = html.find(".f-c-state", text: "Vyřízeno")
     assert state_div.find(".dropdown-item", text: "Označit jako vyřizovaný")["data-url"].include?("aasm_event=to_pending")
     assert state_div.find(".dropdown-item", text: "Označit jako nevyřízený")["data-url"].include?("aasm_event=to_submitted")
