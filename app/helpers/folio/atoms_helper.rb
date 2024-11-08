85baacb6c159035d5740248e23383dcc6965d4da
Petr Marek <contact@petrmarek.eu>
2024-11-08 07:22:08 +0100

    extract render_atom and render_molecule in atoms_helper

--- app/helpers/folio/atoms_helper.rb
+++ app/helpers/folio/atoms_helper.rb
@@ -1,6 +1,42 @@
 # frozen_string_literal: true
 
 module Folio::AtomsHelper
+  def render_atom(atom, atom_options: {})
+    if atom.class.component_class
+      if self.is_a?(Folio::ApplicationCell)
+        capture { render_view_component(atom.class.component_class.new(atom:, atom_options:)) }
+      else
+        capture { render(atom.class.component_class.new(atom:, atom_options:)) }
+      end
+    elsif atom.class.cell_name
+      cell(atom.class.cell_name,
+           atom,
+           atom.cell_options.present? ? atom.cell_options.merge(atom_options) : atom_options)
+    else
+      render "folio/atoms/#{atom.partial_name}", data: atom
+    end
+  end
+
+  def render_molecule(molecule, atoms, atom_options: {})
+    if atoms.present?
+      if molecule.is_a?(String)
+        cell(molecule,
+             atoms,
+             atom_options)
+      elsif molecule < ViewComponent::Base
+        if self.is_a?(Folio::ApplicationCell)
+          capture { render_view_component(molecule.new(atoms:, atom_options:)) }
+        else
+          capture { render(molecule.new(atoms:, atom_options:)) }
+        end
+      else
+        cell(molecule.cell_name,
+             atoms,
+             molecule.cell_options.present? ? molecule.cell_options.merge(atom_options) : atom_options)
+      end
+    end
+  end
+
   def render_atoms(atoms, only: [], except: [], atom_options: {}, cover_placements: true)
     if cover_placements && !atom_options[:cover_placements]
       atom_options = atom_options.merge(cover_placements: cover_placements_for_atoms(atoms))
@@ -10,19 +46,7 @@ module Folio::AtomsHelper
       next if only.present? && !only.include?(atom.class)
       next if except.present? && except.include?(atom.class)
 
-      if atom.class.component_class
-        if self.is_a?(Folio::ApplicationCell)
-          capture { render_view_component(atom.class.component_class.new(atom:, atom_options:)) }
-        else
-          capture { render(atom.class.component_class.new(atom:, atom_options:)) }
-        end
-      elsif atom.class.cell_name
-        cell(atom.class.cell_name,
-             atom,
-             atom.cell_options.present? ? atom.cell_options.merge(atom_options) : atom_options)
-      else
-        render "folio/atoms/#{atom.partial_name}", data: atom
-      end
+      render_atom(atom, atom_options:)
     end.join("").html_safe
   end
 
@@ -45,23 +69,7 @@ module Folio::AtomsHelper
       next if atoms.blank?
 
       if molecule.present?
-        if atoms.present?
-          if molecule.is_a?(String)
-            cell(molecule,
-                 atoms,
-                 atom_options)
-          elsif molecule < ViewComponent::Base
-            if self.is_a?(Folio::ApplicationCell)
-              capture { render_view_component(molecule.new(atoms:, atom_options:)) }
-            else
-              capture { render(molecule.new(atoms:, atom_options:)) }
-            end
-          else
-            cell(molecule.cell_name,
-                 atoms,
-                 molecule.cell_options.present? ? molecule.cell_options.merge(atom_options) : atom_options)
-          end
-        end
+        render_molecule(molecule, atoms, atom_options:)
       else
         render_atoms(atoms, atom_options:)
       end
