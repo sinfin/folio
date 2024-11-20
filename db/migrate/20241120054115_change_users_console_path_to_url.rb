baf71f50cfbef70fc9e55682fff0e08bbab1a32e
Petr Marek <contact@petrmarek.eu>
2024-11-20 07:08:17 +0100

    fix users console_path migration gsub

--- db/migrate/20241120054115_change_users_console_path_to_url.rb
+++ db/migrate/20241120054115_change_users_console_path_to_url.rb
@@ -2,7 +2,7 @@
 
 class ChangeUsersConsolePathToUrl < ActiveRecord::Migration[7.1]
   def change
-    rename_column :folio_users, :console_url, :console_url
-    rename_column :folio_users, :console_url_updated_at, :console_url_updated_at
+    rename_column :folio_users, :console_path, :console_url
+    rename_column :folio_users, :console_path_updated_at, :console_url_updated_at
   end
 end
