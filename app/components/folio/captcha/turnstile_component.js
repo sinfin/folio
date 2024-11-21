870808851f1967c2b80c155363d971894ffddbb9
Tomáš Cabák <tomascabak00@gmail.com>
2024-11-21 13:03:37 +0100

    replace JS querySelector with turnstileContainerTarget

--- app/components/folio/captcha/turnstile_component.js
+++ app/components/folio/captcha/turnstile_component.js
@@ -1,5 +1,6 @@
 window.Folio.Stimulus.register('f-captcha-turnstile', class extends window.Stimulus.Controller {
   static values = { siteKey: String }
+  static targets = ["turnstileContainer"]
 
   connect() {
     if (!document.querySelector("#cloudflare-turnstile-script")) {
@@ -30,20 +31,19 @@ window.Folio.Stimulus.register('f-captcha-turnstile', class extends window.Stimu
   renderTurnstile() {
     if (!this.siteKeyValue || typeof turnstile === 'undefined') return
 
-    // TODO: handle more than one turnstile on the page (login modal is rendered everywhere)
-    const turnstileContainer = document.querySelector(".cf-turnstile")
+    const turnstileContainer = this.turnstileContainerTarget
 
     if (turnstileContainer && !turnstileContainer.innerHTML) {
       // Control the visibility of the iframe wrapper because even when the iframe is hidden,
       // the wrapper takes up unnecessary space and causes layout shifts
       const updateIframeWraperVisibility = (display) => {
-        const iframeWrapper = document.querySelector(".cf-turnstile > div")
+        const iframeWrapper = turnstileContainer.querySelector(":scope > div")
         if (iframeWrapper) {
           iframeWrapper.style.display = display
         }
       }
 
-      turnstile.render('.cf-turnstile', {
+      turnstile.render(turnstileContainer, {
         sitekey: this.siteKeyValue,
         appearance: 'interaction-only',
         'before-interactive-callback': () => { updateIframeWraperVisibility("block") },
@@ -54,8 +54,7 @@ window.Folio.Stimulus.register('f-captcha-turnstile', class extends window.Stimu
   }
 
   removeTurnstile() {
-    if (typeof turnstile === 'undefined'
-        || !document.querySelector(".cf-turnstile").innerHTML) return
+    if (typeof turnstile === 'undefined' || !this.turnstileContainerTarget.innerHTML) return
 
     turnstile.remove()
   }
--- app/components/folio/captcha/turnstile_component.rb
+++ app/components/folio/captcha/turnstile_component.rb
@@ -9,9 +9,12 @@ class Folio::Captcha::TurnstileComponent < Folio::ApplicationComponent
   end
 
   def data
-    stimulus_controller("f-captcha-turnstile",
-                        values: {
-                          site_key: ENV["CLOUDFLARE_TURNSTILE_SITE_KEY"]
-                        })
+    stimulus_merge_data(
+      stimulus_controller("f-captcha-turnstile",
+                          values: {
+                            site_key: ENV["CLOUDFLARE_TURNSTILE_SITE_KEY"]
+                          }),
+      stimulus_target("turnstileContainer")
+    )
   end
 end
