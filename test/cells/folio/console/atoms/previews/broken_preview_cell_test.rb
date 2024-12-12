efe807f193ba881b50863e15c855e3cec91286d6
Petr Marek <contact@petrmarek.eu>
2024-12-12 09:45:10 +0100

    fix(atoms): broken_preview_cell_test

--- test/cells/folio/console/atoms/previews/broken_preview_cell_test.rb
+++ test/cells/folio/console/atoms/previews/broken_preview_cell_test.rb
@@ -4,7 +4,8 @@ require "test_helper"
 
 class Folio::Console::Atoms::Previews::BrokenPreviewCellTest < Folio::Console::CellTest
   test "show" do
-    html = cell("folio/console/atoms/previews/broken_preview", nil).(:show)
+    error = StandardError.new("Some error")
+    html = cell("folio/console/atoms/previews/broken_preview", error:).(:show)
     assert html.has_css?(".f-c-atoms-previews-broken-preview")
   end
 end
