29da29d78c83f0cea4cbcd1bd1b788ddd5bbe486
Petr Marek <contact@petrmarek.eu>
2025-03-24 07:10:26 +0100

    feat(structured_data): support custom cover thumb size

--- app/components/folio/structured_data/body_component.rb
+++ app/components/folio/structured_data/body_component.rb
@@ -121,7 +121,7 @@ class Folio::StructuredData::BodyComponent < Folio::ApplicationComponent
       },
       "headline" => @record.title,
       "description" => @record.perex,
-      "image" => cover.present? ? [cover.thumb(Folio::OG_IMAGE_DIMENSIONS).url] : nil,
+      "image" => cover.present? ? [cover.thumb(record_cover_thumb_size).url] : nil,
       "datePublished" => @record.published_at_with_fallback.iso8601,
       "dateModified" => @record.try(:revised_at).present? ? @record.revised_at.iso8601 : nil,
       "keywords" => tags_ary.present? ? tags_ary.map(&:title) : nil,
@@ -129,4 +129,9 @@ class Folio::StructuredData::BodyComponent < Folio::ApplicationComponent
       "publisher" => publisher_data
     }.compact
   end
+
+  private
+    def record_cover_thumb_size
+      @record.try(:structured_data_cover_thumb_size) || Folio::OG_IMAGE_DIMENSIONS
+    end
 end
