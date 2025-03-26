8539a486513a13ad1ab5683a1ed6404b8bbc1bd0
Petr Marek <contact@petrmarek.eu>
2025-03-26 08:38:27 +0100

    feat(message_bus): only broadcast file_update to users currently in console

--- app/jobs/folio/application_job.rb
+++ app/jobs/folio/application_job.rb
@@ -3,11 +3,18 @@
 class Folio::ApplicationJob < ActiveJob::Base
   private
     def broadcast_file_update(file)
+      user_ids = Folio::User.where.not(console_url: nil)
+                            .where(console_url_updated_at: 1.hour.ago..)
+                            .pluck(:id)
+
+      return if user_ids.blank?
+
       MessageBus.publish Folio::MESSAGE_BUS_CHANNEL,
                          {
                            type: "Folio::ApplicationJob/file_update",
                            data: serialized_file(file)[:data],
-                         }.to_json
+                         }.to_json,
+                         user_ids:
     end
 
     def serializer_for(model)
