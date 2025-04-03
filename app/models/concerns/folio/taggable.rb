f9aba367300b4a692e948c1ee923699f2258b028
Petr Marek <contact@petrmarek.eu>
2025-04-03 10:39:20 +0200

    fix(acts_as_taggable): move by_site scope to override

--- app/models/concerns/folio/taggable.rb
+++ app/models/concerns/folio/taggable.rb
@@ -8,14 +8,5 @@ module Folio::Taggable
     acts_as_taggable_tenant :site_id
 
     scope :by_tag, -> (tag) { tagged_with(tag) }
-
-    ActsAsTaggableOn::Tag.class_eval do
-      scope :by_site, -> (site) {
-        joins(:taggings)
-          .where(taggings: { tenant: site.id })
-          .select("DISTINCT ON (tags.name) tags.*")
-          .reorder("tags.name, tags.taggings_count")
-      }
-    end
   end
 end
