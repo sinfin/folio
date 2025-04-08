76bc66e9561c8d6408ae253960005b1d9818ff26
Vlada <vladislav.trefil@protonmail.com>
2025-04-08 18:21:46 +0200

    fix(AtomForm): add max-width to dontGrow column to restrict stretching content

--- react/src/components/AtomForm/styled/AtomFormCardColumn.js
+++ react/src/components/AtomForm/styled/AtomFormCardColumn.js
@@ -2,5 +2,6 @@ import styled from 'styled-components'
 
 export default styled.div`
   flex: ${(props) => props.dontGrow ? '0' : '1'} 1 auto;
+  max-width: ${(props) => props.dontGrow ? '192px' : 'initial'};
   padding: 0 12px;
 `
