9705ff8f847464631b2350922ceae8484442d1a8
Petr Mlčoch <foton@centrum.cz>
2025-04-11 15:30:13 +0200

    update_newsletter_subscriptions if emailis changed or user confirmed

--- app/models/concerns/folio/has_newsletter_subscriptions.rb
+++ app/models/concerns/folio/has_newsletter_subscriptions.rb
@@ -47,6 +47,7 @@ module Folio::HasNewsletterSubscriptions
     end
 
     def update_newsletter_subscriptions
+      return unless saved_changes.key?("email") || saved_changes.key?("invitation_accepted_at")
       return unless newsletter_subscriptions_enabled?
       return unless auth_site.present?
 
