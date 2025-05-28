5412c2615f8e2226770a872265ba077bf02e9a93
Petr Marek <contact@petrmarek.eu>
2025-05-28 08:10:04 +0200

    feat(html_sanitization): init concern and sanitizer

---
+++ app/lib/folio/html_sanitization/sanitizer.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+module Folio
+  module HtmlSanitization
+    class Sanitizer
+      DEFAULT_ATTRIBUTE_CONFIG = :string
+
+      def initialize(record:)
+        @record = record
+      end
+
+      def self.sanitize(record:)
+        new(record:).sanitize
+      end
+
+      def sanitize
+        return unless @record.folio_html_sanitization_config[:enabled]
+
+        if @record.is_a?(Folio::Atom::Base)
+          sanitize_atom
+        else
+          sanitize_record
+        end
+
+        @record
+      end
+
+      private
+        def attributes_config
+          @attributes_config ||= @record.folio_html_sanitization_config[:attributes]
+        end
+
+        def sanitize_attribute(attribute:)
+          value = @record.send(attribute)
+          return unless value.is_a?(String)
+
+          attribute_config = attributes_config[attribute] || DEFAULT_ATTRIBUTE_CONFIG
+
+          if attribute_config.is_a?(Proc)
+            sanitize_attribute_via_proc(attribute:, value:, proc: attribute_config)
+          else
+            case attribute_config
+            when :unsafe_html
+              # No sanitization for unsafe_html
+            when :richtext
+              sanitize_attribute_as_richtext(attribute:, value:)
+            when :string
+              sanitize_attribute_as_string(attribute:, value:)
+            else
+              raise ArgumentError, "Unknown attribute config: #{attribute_config.inspect}"
+            end
+          end
+        end
+
+        def sanitize_attribute_as_richtext(attribute:, value:)
+          sanitized_value = ActionController::Base.helpers.sanitize(value)
+
+          if value != sanitized_value
+            @record.send("#{attribute}=", sanitized_value)
+            log(attribute:, message: "Sanitized as richtext from #{value.inspect} to #{sanitized_value.inspect}")
+          end
+        end
+
+        def sanitize_attribute_as_string(attribute:, value:)
+          sanitized_value = ActionView::Base.full_sanitizer.sanitize(value)
+
+          if value != sanitized_value
+            @record.send("#{attribute}=", sanitized_value)
+            log(attribute:, message: "Sanitized as string from #{value.inspect} to #{sanitized_value.inspect}")
+          end
+        end
+
+        def sanitize_attribute_via_proc(attribute:, value:, proc:)
+          sanitized_value = proc.call(value)
+
+          if value != sanitized_value
+            @record.send("#{attribute}=", sanitized_value)
+            log(attribute:, message: "Sanitized via proc from #{value.inspect} to #{sanitized_value.inspect}")
+          end
+        end
+
+        def sanitize_atom
+          if @record.changed_attributes.key?("data")
+            @record.class::STRUCTURE.each do |attribute, type|
+              sanitize_attribute(attribute: attribute.to_sym)
+            end
+          end
+        end
+
+        def sanitize_record
+          @record.changed_attributes.each do |attribute, _change|
+            sanitize_attribute(attribute: attribute.to_sym)
+          end
+        end
+
+        def log(attribute:, message:)
+          Rails.logger.debug("[Folio::HtmlSanitization::Sanitizer][#{@record.class}][#{@record.id || "new-record"}][#{attribute}] - #{message}")
+        end
+    end
+  end
+end
---
+++ test/models/concerns/folio/html_sanitization/model_test.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module Folio
+  module HtmlSanitization
+    class ModelTest < ActiveSupport::TestCase
+      class PageWithSanitizationConfig < Folio::Page
+        def folio_html_sanitization_config
+          {
+            enabled: true,
+            attributes: {
+              title: :string,
+              meta_description: :unsafe_html,
+              meta_title: -> (value) { value.upcase },
+              perex: :richtext,
+            }
+          }
+        end
+      end
+
+      test "sanitizes input" do
+        unsafe_input = "<p>script-<script>alert('xss')</script> absolute-a-<a href=\"https://www.google.com/\">a</a> relative-a-<a href=\"/foo\">a</a> hash-a-<a href=\"#foo\">a</a> xss-a-<a href=\"javascript:alert('xss')\">a</a> img-<img onerror=\"alert('xss')\"> input-<input onfocus=\"alert('xss')\"> baz</p>"
+
+        page = PageWithSanitizationConfig.new(title: unsafe_input,
+                                              meta_title: unsafe_input,
+                                              meta_description: unsafe_input,
+                                              perex: unsafe_input,
+                                              site: get_any_site)
+        assert page.valid?
+
+        assert_equal("script- absolute-a-a relative-a-a hash-a-a xss-a-a img- input- baz", page.title)
+        assert_equal("<P>SCRIPT-<SCRIPT>ALERT('XSS')</SCRIPT> ABSOLUTE-A-<A HREF=\"HTTPS://WWW.GOOGLE.COM/\">A</A> RELATIVE-A-<A HREF=\"/FOO\">A</A> HASH-A-<A HREF=\"#FOO\">A</A> XSS-A-<A HREF=\"JAVASCRIPT:ALERT('XSS')\">A</A> IMG-<IMG ONERROR=\"ALERT('XSS')\"> INPUT-<INPUT ONFOCUS=\"ALERT('XSS')\"> BAZ</P>", page.meta_title)
+        assert_equal(unsafe_input, page.meta_description)
+        assert_equal("<p>script-alert('xss') absolute-a-<a href=\"https://www.google.com/\">a</a> relative-a-<a href=\"/foo\">a</a> hash-a-<a href=\"#foo\">a</a> xss-a-<a>a</a> img-<img> input- baz</p>", page.perex)
+      end
+    end
+  end
+end
