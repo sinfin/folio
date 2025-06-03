bf71edab1f07cd49c36a2472986461c82c130aa9
Tomáš Cabák <tomascabak00@gmail.com>
2025-06-03 15:43:53 +0200

    fix(private_attachment_serializer): add missing expiring URL attribute that is used by PrivateAttachmentsFieldsComponent

--- app/serializers/folio/console/private_attachment_serializer.rb
+++ app/serializers/folio/console/private_attachment_serializer.rb
@@ -8,4 +8,8 @@ class Folio::Console::PrivateAttachmentSerializer
              :file_name,
              :type,
              :title
+
+  attribute :expiring_url do |object|
+    Folio::S3.url_rewrite(object.file.remote_url(expires: 1.hour.from_now))
+  end
 end
