7f7fecf396f1c26606a4b9870aee261dec5629dc
Petr Marek <contact@petrmarek.eu>
2025-06-10 13:01:50 +0200

    feat(tiptap): init simple-editor

--- tiptap/src/main.tsx
+++ tiptap/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './styles/_variables.scss'
+import './styles/_keyframe-animations.scss'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
