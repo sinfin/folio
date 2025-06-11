9d60c9bf830e3f04321f5e8bf86178959567571a
Petr Marek <contact@petrmarek.eu>
2025-06-11 06:41:04 +0200

    feat(tiptap): add 3rd party global-drag-handle extension

--- tiptap/src/components/tiptap-templates/simple/simple-editor.tsx
+++ tiptap/src/components/tiptap-templates/simple/simple-editor.tsx
@@ -75,6 +75,10 @@ import "@/components/tiptap-templates/simple/simple-editor.scss"
 
 import content from "@/components/tiptap-templates/simple/data/content.json"
 
+// --- 3rd party extensions ---
+import GlobalDragHandle from 'tiptap-extension-global-drag-handle'
+import AutoJoiner from 'tiptap-extension-auto-joiner'
+
 const MainToolbarContent = ({
   onHighlighterClick,
   onLinkClick,
@@ -220,6 +224,18 @@ export function SimpleEditor() {
       }),
       TrailingNode,
       Link.configure({ openOnClick: false }),
+      GlobalDragHandle.configure({
+          dragHandleWidth: 20,    // default
+
+          // The scrollTreshold specifies how close the user must drag an element to the edge of the lower/upper screen for automatic
+          // scrolling to take place. For example, scrollTreshold = 100 means that scrolling starts automatically when the user drags an
+          // element to a position that is max. 99px away from the edge of the screen
+          // You can set this to 0 to prevent auto scrolling caused by this extension
+          scrollTreshold: 100     // default
+      }),
+      AutoJoiner.configure({
+          elementsToJoin: ["bulletList", "orderedList"] // default
+      }),
     ],
     content: content,
   })
