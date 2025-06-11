01239cc1bcd376252532525d5d2fba042dc3fb59
Tomáš Cabák <tomascabak00@gmail.com>
2025-06-11 10:49:21 +0200

    autohide server-rendered alerts (#393)
    * feat(flash): add autohide functionality to server-rendered alerts
    
    * refactor(alert): apply copilot review changes
    
    * feat(alert_cell): move stimulus data to ruby class
    
    * refactor(alert): let stimulus controller handle the autohide
    
    * chore(changelog): update changelog
    
    * Update alert_cell.rb
    
    ---------
    
    Co-authored-by: Petr Marek <contact@petrmarek.eu>

--- app/cells/folio/console/flash_cell.rb
+++ app/cells/folio/console/flash_cell.rb
@@ -12,11 +12,14 @@ class Folio::Console::FlashCell < Folio::ConsoleCell
 
   def flashes
     if model.present?
-      # filter out devise timedout
-      # https://github.com/heartcombo/devise/issues/1777
-      model.filter { |key, _value| key != "timedout" }
+      # filter out devise timedout (https://github.com/heartcombo/devise/issues/1777) and autohide
+      model.filter { |key, _value| key != "timedout" && key != "autohide" }
     else
       []
     end
+  end
+
+  def autohide?
+    model.present? && (model["autohide"] || model[:autohide])
   end
 end
