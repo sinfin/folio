f9953ddf73117728c3145da76c11d6e37492f1f5
Petr Marek <contact@petrmarek.eu>
2025-06-24 11:05:35 +0200

    feat(tiptap): add loader, cache HTML from API

--- tiptap/src/components/tiptap-node/folio-tiptap-node/folio-tiptap-node.tsx
+++ tiptap/src/components/tiptap-node/folio-tiptap-node/folio-tiptap-node.tsx
@@ -1,10 +1,18 @@
-import * as React from "react"
-import type { NodeViewProps } from "@tiptap/react"
-import { NodeViewWrapper } from "@tiptap/react"
+import * as React from "react";
+import type { NodeViewProps } from "@tiptap/react";
+import { NodeViewWrapper } from "@tiptap/react";
 // import "@/components/tiptap-node/image-upload-node/image-upload-node.scss"
 
-let uniqueIdForNode = 0
-const getUniqueIdForNode = () => uniqueIdForNode++
+import "./folio-tiptap-node.scss";
+
+let uniqueIdForNode = 0;
+const getUniqueIdForNode = () => uniqueIdForNode++;
+
+let htmlCache = [];
+
+const storeHtmlToCache = ({ html, serializedAttrs }) => {
+  htmlCache = [{ html, serializedAttrs }, ...htmlCache.slice(0, 9)];
+};
 
 export const FolioTiptapNode: React.FC<NodeViewProps> = (props) => {
   const handleClick = React.useCallback(
@@ -22,50 +30,64 @@ export const FolioTiptapNode: React.FC<NodeViewProps> = (props) => {
     [],
   );
 
-  const [uniqueId, setUniqueId] = React.useState<number>(0)
-  const [loaded, setLoaded] = React.useState<boolean>(false)
-  const [htmlFromApi, setHtmlFromApi] = React.useState<string>("")
+  const [uniqueId, setUniqueId] = React.useState<number>(0);
+  const [loaded, setLoaded] = React.useState<boolean>(false);
+  const [htmlFromApi, setHtmlFromApi] = React.useState<string>("");
 
   React.useEffect(() => {
     if (uniqueId === 0) {
-      setUniqueId(getUniqueIdForNode())
+      setUniqueId(getUniqueIdForNode());
     }
-  }, [uniqueId])
+  }, [uniqueId]);
 
   // Effect to fetch HTML content from API
   React.useEffect(() => {
     if (!loaded && uniqueId !== 0) {
-      window.top!.postMessage(
-        {
-          type: "f-tiptap-node:render",
-          uniqueId,
-          attrs: props.node.attrs,
-        },
-        "*",
+      const serializedAttrs = JSON.stringify(props.node.attrs);
+      // Check if we have cached HTML for these attributes
+      const cachedEntry = htmlCache.find(
+        (entry) => entry.serializedAttrs === serializedAttrs,
       );
+
+      if (cachedEntry) {
+        setHtmlFromApi(cachedEntry.html);
+        setLoaded(true);
+        return;
+      } else {
+        window.top!.postMessage(
+          {
+            type: "f-tiptap-node:render",
+            uniqueId,
+            attrs: props.node.attrs,
+          },
+          "*",
+        );
+      }
     }
-  }, [loaded, uniqueId])
+  }, [loaded, uniqueId]);
 
   // Effect to handle messages from the parent window
   React.useEffect(() => {
     const handleMessage = (event: MessageEvent) => {
-      console.log('message', event, uniqueId)
       if (event.data.type === "f-input-tiptap:render-nodes") {
         event.data.nodes.forEach((node: any) => {
           if (node.unique_id === uniqueId) {
-            setHtmlFromApi(node.html)
-            setLoaded(true)
-          }
-        })
-      }
-    }
+            const serializedAttrs = JSON.stringify(props.node.attrs);
+            storeHtmlToCache({ html: node.html, serializedAttrs });
 
-    window.addEventListener("message", handleMessage)
+            setHtmlFromApi(node.html);
+            setLoaded(true);
+          }
+        });
+      }
+    };
+
+    window.addEventListener("message", handleMessage);
 
     return () => {
-      window.removeEventListener("message", handleMessage)
-    }
-  }, [uniqueId])
+      window.removeEventListener("message", handleMessage);
+    };
+  }, [uniqueId]);
 
   return (
     <NodeViewWrapper
@@ -73,8 +95,16 @@ export const FolioTiptapNode: React.FC<NodeViewProps> = (props) => {
       tabIndex={0}
       onClick={handleClick}
     >
-      {htmlFromApi ?
-        <div dangerouslySetInnerHTML={{ __html: htmlFromApi }} /> : 'loading'}
+      {htmlFromApi ? (
+        <div
+          className="tiptap-folio-tiptap-node__html"
+          dangerouslySetInnerHTML={{ __html: htmlFromApi }}
+        />
+      ) : (
+        <div className="tiptap-folio-tiptap-node__loader-wrap rounded">
+          <span className="folio-loader" />
+        </div>
+      )}
     </NodeViewWrapper>
-  )
-}
+  );
+};
