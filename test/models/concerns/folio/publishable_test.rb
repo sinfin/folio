3242cdb23403fbac2b0021333a22973254a5816c
Petr Mlčoch <foton@centrum.cz>
2025-07-07 11:51:29 +0200

    (chore) Enhanced concern Folio::Pubichable with event methods `publish(!)` and  `unpublish(!)`

--- test/models/concerns/folio/publishable_test.rb
+++ test/models/concerns/folio/publishable_test.rb
@@ -9,6 +9,46 @@ class Folio::PublishableTest < ActiveSupport::TestCase
     end
   end
 
+  class PublishableTemplateObject
+    def self.before_validation(_action)
+    end
+
+    def self.scope(*args, &block)
+    end
+
+    def initialize
+      @published = false
+      @published_at = nil
+      @published_from = nil
+      @published_until = nil
+    end
+
+    def generate_preview_token
+      "preview_token"
+    end
+
+    def save!(*args) # for publish! and unpublish!
+    end
+  end
+
+  class PublishableBasicObject < PublishableTemplateObject
+    attr_accessor :published
+
+    include Folio::Publishable::Basic
+  end
+
+  class PublishableWithDateObject < PublishableTemplateObject
+    attr_accessor :published, :published_at
+
+    include Folio::Publishable::WithDate
+  end
+
+  class PublishableWithinObject < PublishableTemplateObject
+    attr_accessor :published, :published_from, :published_until
+
+    include Folio::Publishable::Within
+  end
+
   test "with_date" do
     assert_equal(0, Folio::Page.published.count)
     assert_equal(0, Folio::Page.unpublished.count)
@@ -86,5 +126,145 @@ class Folio::PublishableTest < ActiveSupport::TestCase
     assert_equal true, page_with_date.published?
     assert page_with_date.published_at
     assert PagePublishableWithDate.published.exists?(id: page_with_date.id)
+  end
+
+
+  test "publishable_basic" do
+    object = PublishableBasicObject.new
+    assert_equal false, object.published
+    assert_equal false, object.published?
+
+    object.publish
+
+    assert_equal true, object.published
+    assert_equal true, object.published?
+
+    object.unpublish
+
+    assert_equal false, object.published
+    assert_equal false, object.published?
+  end
+
+  test "publishable_with_date" do
+    object = PublishableWithDateObject.new
+    assert_equal false, object.published
+    assert_nil object.published_at
+    assert_equal false, object.published?
+
+    object.published_at = 1.hour.ago
+    assert_equal false, object.published
+    assert_equal false, object.published?
+
+    object.published = true
+    assert_equal true, object.published?
+
+    object.published_at = Time.current + 1.hour
+    assert_equal true, object.published
+    assert_equal false, object.published?
+
+    object.unpublish
+
+    assert_equal false, object.published
+    assert_not_nil object.published_at # we keep the date
+    assert_equal false, object.published?
+
+    object.published_at = nil
+
+    t = Time.current
+    Time.stub(:current, t) do
+      object.publish
+    end
+
+    assert_equal true, object.published
+    assert_equal true, object.published?
+    assert_equal t, object.published_at
+
+    object.unpublish
+
+    assert_equal false, object.published?
+
+    t = Time.current + 10.minutes
+    object.publish(published_at: t)
+
+    assert_equal true, object.published
+    assert_equal t, object.published_at
+    assert_equal false, object.published?
+  end
+
+  test "publishable_within" do
+    object = PublishableWithinObject.new
+    assert_equal false, object.published
+    assert_nil object.published_from
+    assert_nil object.published_until
+    assert_equal false, object.published?
+
+    object.published = true
+
+    assert_nil object.published_from
+    assert_nil object.published_until
+    assert_equal true, object.published?
+
+    object.published_from = 1.hour.ago
+    assert_equal true, object.published
+    assert_nil object.published_until
+    assert_equal true, object.published?
+
+    object.published_from = 1.minute.from_now
+    assert_equal false, object.published?
+
+    object.published_until = 2.minutes.from_now
+    assert_equal false, object.published?
+
+    object.published_from = 10.minutes.ago
+    assert_equal true, object.published?
+
+    object.published_until = 2.minutes.ago
+    assert_equal false, object.published?
+
+    object.published_until = 1.minute.from_now
+    object.published_from = nil
+    assert_equal true, object.published?
+
+    object.published = false
+    assert_equal false, object.published?
+
+    object.published_from = nil
+    object.published_until = nil
+    assert_equal false, object.published
+
+    object.publish!
+
+    assert_equal true, object.published?
+    assert_equal true, object.published
+    assert_nil object.published_from
+    assert_nil object.published_until
+
+    object.unpublish!
+
+    assert_equal false, object.published?
+    assert_equal false, object.published
+
+
+    t = 1.minute.from_now
+    object.publish!(published_from: t)
+
+    assert_equal false, object.published? # future published_from
+    assert_equal true, object.published
+    assert_equal t, object.published_from
+    assert_nil object.published_until
+
+    object.unpublish!
+
+    assert_equal false, object.published
+    assert_equal t, object.published_from
+
+    t2 = 2.minutes.ago
+
+    object.publish!(published_from: t2, published_until: t)
+
+    assert_equal true, object.published?
+    assert_equal true, object.published
+    assert_equal t2, object.published_from
+    assert_equal t, object.published_until
   end
 end
