a0c2de04f6c7fb9435519e14548cb9f5a8fd2eef
Petr Marek <contact@petrmarek.eu>
2025-07-22 14:14:38 +0200

    fix(tiptap): minor ui fixes

--- app/components/folio/tiptap/content_component.rb
+++ app/components/folio/tiptap/content_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class Folio::Tiptap::ContentComponent < ApplicationComponent
-  def initialize(record:, attribute: :tiptap_content)
+  def initialize(record:, attribute: :tiptap_content, class_name: nil)
     @record = record
     @attribute = attribute
+    @class_name = class_name
   end
 
   def render?
