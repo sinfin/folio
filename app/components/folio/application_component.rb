5d803df5785c3fe7ce5cac58e72ac5d96cb1fae8
Vlada <vladislav.trefil@protonmail.com>
2025-08-19 19:53:33 +0200

    fix(application_component): remove default initialize method

--- app/components/folio/application_component.rb
+++ app/components/folio/application_component.rb
@@ -6,12 +6,6 @@ class Folio::ApplicationComponent < ViewComponent::Base
   include Folio::IconHelper
   include Folio::StimulusHelper
 
-  def initialize(**kwargs)
-    kwargs.each do |key, value|
-      instance_variable_set("@#{key}", value)
-    end
-  end
-
   def original_bem_class_name
     base = self.class.name.delete_suffix("Component")
 
