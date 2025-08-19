e7ab4ef68c6e780c88333a6a01779145020f8222
Petr Marek <contact@petrmarek.eu>
2025-08-19 10:40:08 +0200

    chore(tiptap): rm leftover console.log

--- tiptap/src/components/tiptap-editors/folio-editor/folio-editor-responsive-preview.tsx
+++ tiptap/src/components/tiptap-editors/folio-editor/folio-editor-responsive-preview.tsx
@@ -31,7 +31,6 @@ export function FolioEditorResponsivePreview({
     };
 
     const onMouseUp = () => {
-      console.log('onMouseUp')
       document.removeEventListener("mousemove", onMouseMove);
       document.removeEventListener("mouseup", onMouseUp);
     };
