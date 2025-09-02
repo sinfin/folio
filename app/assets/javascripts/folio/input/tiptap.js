01637673ee7f3987c963e6c6ff3745793096b386
Tomáš Cabák <tomascabak00@gmail.com>
2025-09-02 15:51:42 +0200

    fix(tiptap): reduce auto-save delay from 2000ms to 1000ms

--- app/assets/javascripts/folio/input/tiptap.js
+++ app/assets/javascripts/folio/input/tiptap.js
@@ -1,7 +1,7 @@
 window.Folio.Stimulus.register('f-input-tiptap', class extends window.Stimulus.Controller {
   static targets = ['input', 'iframe', 'loader']
 
-  static AUTO_SAVE_DELAY = 2000
+  static AUTO_SAVE_DELAY = 1000
 
   static values = {
     loaded: { type: Boolean, default: false },
