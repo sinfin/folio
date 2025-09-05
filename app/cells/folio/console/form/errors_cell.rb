741f916bd64c43f11f1820d8bfc29a1a226f798c
zanetagebka <zanetagebka@gmail.com>
2025-09-05 14:07:36 +0200

    Add possibility to hide button for specific attribute errors

--- app/cells/folio/console/form/errors_cell.rb
+++ app/cells/folio/console/form/errors_cell.rb
@@ -24,4 +24,11 @@ class Folio::Console::Form::ErrorsCell < Folio::ConsoleCell
       "action" => "f-c-form-errors#onButtonClick"
     }
   end
+
+  def show_fix_button?(error)
+    buttons_enabled = options[:buttons] != false
+    hide_for = Array(options[:hide_btn_for]).map(&:to_s)
+
+    buttons_enabled && hide_for.exclude?(error.attribute.to_s)
+  end
 end
