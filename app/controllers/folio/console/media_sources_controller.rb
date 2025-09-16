10694daa03a2bee8c563a09195f367ff75d21c9c
Tomáš Cabák <tomascabak00@gmail.com>
2025-09-16 10:32:49 +0200

    feat(media_sources): conditionally permit site links based on shared_files_between_sites config

--- app/controllers/folio/console/media_sources_controller.rb
+++ app/controllers/folio/console/media_sources_controller.rb
@@ -5,11 +5,19 @@ class Folio::Console::MediaSourcesController < Folio::Console::BaseController
 
   private
     def media_source_params
-      params.require(:media_source)
-            .permit(:title,
-                    :licence,
-                    :copyright_text,
-                    :max_usage_count,
-                    media_source_site_links_attributes: %w[id _destroy site_id])
+      if Rails.application.config.folio_shared_files_between_sites
+        params.require(:media_source)
+              .permit(:title,
+                      :licence,
+                      :copyright_text,
+                      :max_usage_count,
+                      media_source_site_links_attributes: %w[id _destroy site_id])
+      else
+        params.require(:media_source)
+              .permit(:title,
+                      :licence,
+                      :copyright_text,
+                      :max_usage_count)
+      end
     end
 end
