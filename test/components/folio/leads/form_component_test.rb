3b2ae0b2adec8a8b2f2a9c8277358529e0d896f7
Filip Ornstein <filip@sinfin.cz>
2025-09-18 09:25:27 +0100

    refactor(tests): remove session requirement tests from form component tests

--- test/components/folio/leads/form_component_test.rb
+++ test/components/folio/leads/form_component_test.rb
@@ -9,20 +9,6 @@ class Folio::Leads::FormComponentTest < Folio::ComponentTest
     assert_selector(".f-leads-form")
   end
 
-  def test_requires_session_for_component
-    component = Folio::Leads::FormComponent.new
-
-    # Test polymorphic API
-    assert component.requires_session?
-    assert_equal "lead_form_csrf_and_flash", component.session_requirement_reason
-
-    # Test session requirement hash structure
-    requirement = component.session_requirement
-    assert_equal "lead_form_csrf_and_flash", requirement[:reason]
-    assert requirement[:component].include?("FormComponent")
-    assert_kind_of Time, requirement[:timestamp]
-  end
-
   def test_renders_with_lead_instance
     lead = Folio::Lead.new(email: "test@example.com")
     render_inline(Folio::Leads::FormComponent.new(lead: lead))
