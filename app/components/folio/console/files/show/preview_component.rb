0f7c54d347c0156ea314b9adb73ac7d1c7a8f9a8
Petr Marek <contact@petrmarek.eu>
2025-09-18 12:30:24 +0200

    fix(files): use s3 url_rewrite in files/show/preview

--- app/components/folio/console/files/show/preview_component.rb
+++ app/components/folio/console/files/show/preview_component.rb
@@ -6,11 +6,16 @@ class Folio::Console::Files::Show::PreviewComponent < Folio::Console::Applicatio
     @human_type = file.class.human_type
   end
 
-  def render?
-    self.class.should_render?(@human_type)
-  end
-
   def self.should_render?(human_type)
     human_type.in?(%w[image video audio])
   end
+
+  private
+    def render?
+      self.class.should_render?(@human_type)
+    end
+
+    def file_url
+      @file_url ||= Folio::S3.url_rewrite(@file.file.remote_url)
+    end
 end
