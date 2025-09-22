16fb8ec01fa13a92299fb6fc86af68e79fc25c23
Petr Marek <contact@petrmarek.eu>
2025-09-22 12:48:53 +0200

    fix(db): make file_placements_count migration reversible

--- db/migrate/20250922094005_rename_file_placements_size_to_file_placements_count.rb
+++ db/migrate/20250922094005_rename_file_placements_size_to_file_placements_count.rb
@@ -4,7 +4,7 @@ class RenameFilePlacementsSizeToFilePlacementsCount < ActiveRecord::Migration[8.
   def change
     # Rename column and set proper defaults for counter cache
     rename_column :folio_files, :file_placements_size, :file_placements_count
-    change_column_default :folio_files, :file_placements_count, 0
+    change_column_default :folio_files, :file_placements_count, from: nil, to: 0
     change_column_null :folio_files, :file_placements_count, false, 0
   end
 end
