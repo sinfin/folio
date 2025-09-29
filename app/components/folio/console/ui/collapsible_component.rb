7ee197baeabaef3eb1fabb90bfa9ebf7085a71fc
Petr Marek <contact@petrmarek.eu>
2025-09-29 07:37:03 +0200

    feat(files): add tag_list to show, use tagsinput in batch form

--- app/components/folio/console/ui/collapsible_component.rb
+++ app/components/folio/console/ui/collapsible_component.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Folio::Console::Ui::CollapsibleComponent < Folio::Console::ApplicationComponent
+  FOCUS_INPUT_CLASS_NAME = "f-c-ui-collapsible-focus-input"
+
   def initialize(title:, collapsed: true)
     @title = title
     @collapsed = collapsed
