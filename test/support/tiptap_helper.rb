93292c79fe929bf8dea4cf2ff487fb1e452b5bdc
Petr Marek <contact@petrmarek.eu>
2025-09-30 12:24:20 +0200

    feat(test): make tiptap_text_content a class method

--- test/support/tiptap_helper.rb
+++ test/support/tiptap_helper.rb
@@ -1,18 +1,22 @@
 # frozen_string_literal: true
 
 module TiptapHelper
+  def self.tiptap_text_content(text)
+    {
+      Folio::Tiptap::TIPTAP_CONTENT_JSON_STRUCTURE[:content] => {
+        "type" => "doc",
+        "content" => [
+          { "type" => "paragraph", "content" => [{ "type" => "text", "text" => text }] },
+        ]
+      },
+      Folio::Tiptap::TIPTAP_CONTENT_JSON_STRUCTURE[:text] => text,
+      Folio::Tiptap::TIPTAP_CONTENT_JSON_STRUCTURE[:word_count] => text.split.size,
+      Folio::Tiptap::TIPTAP_CONTENT_JSON_STRUCTURE[:character_count] => text.length,
+    }
+  end
+
   private
     def tiptap_text_content(text)
-      {
-        Folio::Tiptap::TIPTAP_CONTENT_JSON_STRUCTURE[:content] => {
-          "type" => "doc",
-          "content" => [
-            { "type" => "paragraph", "content" => [{ "type" => "text", "text" => text }] },
-          ]
-        },
-        Folio::Tiptap::TIPTAP_CONTENT_JSON_STRUCTURE[:text] => text,
-        Folio::Tiptap::TIPTAP_CONTENT_JSON_STRUCTURE[:word_count] => text.split.size,
-        Folio::Tiptap::TIPTAP_CONTENT_JSON_STRUCTURE[:character_count] => text.length,
-      }
+      TiptapHelper.tiptap_text_content(text)
     end
 end
