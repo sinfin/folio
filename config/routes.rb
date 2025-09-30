3558c48d491063cc86f36547b461146e753fa4bd
Petr Marek <contact@petrmarek.eu>
2025-09-30 06:52:56 +0200

    feat(files): add CANCAN_MAPPINGS to files to be used in abilities

--- config/routes.rb
+++ config/routes.rb
@@ -206,7 +206,6 @@ Folio::Engine.routes.draw do
 
             resources key, only: %i[index show update destroy] do
               collection do
-                post :tag
                 get :pagination
 
                 get :batch_bar
