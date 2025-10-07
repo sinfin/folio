6d31aef44a51eca176f12d7d61d44998f4802c74
Petr Marek <contact@petrmarek.eu>
2025-10-07 11:45:33 +0200

    fix(embed): improve blank input value handling

--- app/inputs/embed_input.rb
+++ app/inputs/embed_input.rb
@@ -12,8 +12,6 @@ class EmbedInput < SimpleForm::Inputs::StringInput
 
     merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
 
-    folio_embed_data = {}
-
     source_hash = if input_html_options[:value].present?
       if input_html_options[:value].is_a?(Hash)
         input_html_options[:value].stringify_keys
@@ -27,13 +25,17 @@ class EmbedInput < SimpleForm::Inputs::StringInput
         {}
       end
     else
-      @builder.object.send(attribute_name).presence || {}
+      if @builder.object.respond_to?(attribute_name)
+        @builder.object.send(attribute_name).presence || {}
+      else
+        {}
+      end
     end
 
-    if Folio::Embed.invalid_reason_for(source_hash).nil?
-      folio_embed_data = Folio::Embed.normalize_value(source_hash)
+    folio_embed_data = if Folio::Embed.invalid_reason_for(source_hash).nil?
+      Folio::Embed.normalize_value(source_hash)
     else
-      folio_embed_data = Folio::Embed.normalize_value({ "active" => false })
+      Folio::Embed.normalize_value({ "active" => false })
     end
 
     options[:custom_html] = @builder.template.capture do
@@ -41,7 +43,11 @@ class EmbedInput < SimpleForm::Inputs::StringInput
                                                                        compact: options[:compact]))
     end
 
-    merged_input_options[:value] = folio_embed_data.to_json
+    merged_input_options[:value] = if folio_embed_data.present?
+      folio_embed_data.to_json
+    else
+      nil
+    end
 
     @builder.hidden_field(attribute_name, merged_input_options)
   end
