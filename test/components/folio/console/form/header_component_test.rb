4a3bb053237e6e2be9a216204af952d7622716c7
petr-panoska <panoska.petr@gmail.com>
2025-10-08 13:52:45 +0200

    guard

--- test/components/folio/console/form/header_component_test.rb
+++ test/components/folio/console/form/header_component_test.rb
@@ -5,7 +5,7 @@ require "test_helper"
 class Folio::Console::Form::HeaderComponentTest < Folio::Console::ComponentTest
   def test_render
     title = "Hello world!"
-    page = create(:folio_page, title: )
+    page = create(:folio_page, title:)
 
     render_inline(Folio::Console::Form::HeaderComponent.new(model: page, title: page.title))
 
