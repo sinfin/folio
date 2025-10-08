abd61ad6cb7679bc1f9d4e97e47299b8adf328d1
Petr Marek <contact@petrmarek.eu>
2025-10-08 18:05:56 +0200

    feat(thumbnails): use the best image in ratio preview

--- app/components/folio/console/files/show/thumbnails/ratio_component.rb
+++ app/components/folio/console/files/show/thumbnails/ratio_component.rb
@@ -11,6 +11,32 @@ class Folio::Console::Files::Show::Thumbnails::RatioComponent < Folio::Console::
     def button
       render(Folio::Console::Ui::ButtonComponent.new(variant: :warning,
                                                      size: :sm,
-                                                     label: t("folio.console.actions.edit")))
+                                                     icon: :crop,
+                                                     label: t(".edit")))
+    end
+
+    def image_url
+      return nil if @thumbnail_size_keys.empty?
+
+      valid_keys = @thumbnail_size_keys.reject do |key|
+        url = @file.thumb(key).url
+        url&.start_with?("https://doader")
+      end
+      return nil if valid_keys.empty?
+
+      highest_area_key = valid_keys.max_by do |key|
+        dimensions = key.gsub(/[#>^]$/, "")
+        width_str, height_str = dimensions.split("x", 2)
+
+        if width_str.nil? || width_str.empty?
+          height_str.to_i
+        elsif height_str.nil? || height_str.empty?
+          width_str.to_i
+        else
+          width_str.to_i * height_str.to_i
+        end
+      end
+
+      @file.thumb(highest_area_key).url
     end
 end
