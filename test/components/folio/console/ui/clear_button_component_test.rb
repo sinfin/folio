930e6521df53ccedcd8f807b702e15a19250c827
petr-panoska <panoska.petr@gmail.com>
2025-10-16 11:43:30 +0200

    fix(clear_button): tests

--- test/components/folio/console/ui/clear_button_component_test.rb
+++ test/components/folio/console/ui/clear_button_component_test.rb
@@ -4,9 +4,7 @@ require "test_helper"
 
 class Folio::Console::Ui::ClearButtonComponentTest < Folio::Console::ComponentTest
   def test_render
-    model = "hello"
-
-    render_inline(Folio::Console::Ui::ClearButtonComponent.new(model:))
+    render_inline(Folio::Console::Ui::ClearButtonComponent.new)
 
     assert_selector(".f-c-ui-clear-button")
   end
