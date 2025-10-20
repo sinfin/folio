24caf221b06e4a5ce3c71f0839b4e4957ee9d9e6
Petr Marek <contact@petrmarek.eu>
2025-10-20 09:08:00 +0200

    feat(aws-file-handler): change uppy s3 before URL

--- app/assets/javascripts/folio/parameterize.js
+++ app/assets/javascripts/folio/parameterize.js
@@ -1,7 +1,12 @@
 window.Folio = window.Folio || {}
 
 window.Folio.parameterize = (string) => {
-  if (!string) return string
+  if (typeof string !== 'string') return string
 
-  return string.normalize('NFD').replace(/\p{Diacritic}/gu, '').replace(/[^\w\d]/g, '-')
+  return string.normalize('NFD')
+    .replace(/\p{Diacritic}/gu, '')
+    .replace(/[^\w\d]/g, '-')
+    .toLowerCase()
+    .replace(/[\s_-]+/g, '-')
+    .replace(/^-+|-+$/g, '')
 }
