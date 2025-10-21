dada154e7cd38fdde1e9e60a6613f4b43ae67ea6
Petr Marek <contact@petrmarek.eu>
2025-10-21 08:57:33 +0200

    feat(embed): send 304 based on etag in the middleware

--- app/lib/rack/folio/embed_middleware.rb
+++ app/lib/rack/folio/embed_middleware.rb
@@ -11,34 +11,62 @@ module Rack
 
       def call(env)
         if env["PATH_INFO"] == "/folio/embed"
-          render_embed_html
+          render_embed_html(env["HTTP_IF_NONE_MATCH"])
         else
           @app.call(env)
         end
       end
 
-      def render_embed_html
+      def render_embed_html(if_none_match)
         content, etag = self.class.embed_html_with_etag
 
-        [200, {
-          "Content-Type" => "text/html",
-          "ETag" => etag,
-          "Cache-Control" => "max-age=15, public, must-revalidate, stale-while-revalidate=15, stale-if-error=300"
-        }, [content]]
+        # Check if client has current version via If-None-Match header
+        if if_none_match == etag
+          [304, {
+            "ETag" => etag,
+            "Cache-Control" => "max-age=15, public, must-revalidate, stale-while-revalidate=15, stale-if-error=300"
+          }, []]
+        else
+          [200, {
+            "Content-Type" => "text/html",
+            "ETag" => etag,
+            "Cache-Control" => "max-age=15, public, must-revalidate, stale-while-revalidate=15, stale-if-error=300"
+          }, [content]]
+        end
       end
 
       def self.embed_html_with_etag
-        if Rails.env.development? && ENV["FOLIO_EMBED_DEV"]
-          content = ::File.read(::Folio::Engine.root.join("data/embed/dist/folio-embed-dist.html"))
-          etag = Digest::MD5.hexdigest(content)
-          [content, etag]
+        if should_cache_embed_html?
+          @embed_html_with_etag ||= read_embed_html_with_etag
         else
-          @embed_html_with_etag ||= begin
-            content = ::File.read(::Folio::Engine.root.join("data/embed/dist/folio-embed-dist.html"))
-            etag = Digest::MD5.hexdigest(content)
-            [content, etag]
-          end
+          read_embed_html_with_etag
         end
+      end
+
+      def self.should_cache_embed_html?
+        return @should_cache_embed_html if defined?(@should_cache_embed_html)
+
+        @should_cache_embed_html = if Rails.env.development? && ENV["FOLIO_EMBED_DEV"]
+          false
+        else
+          true
+        end
+      end
+
+      def self.read_embed_html_with_etag
+        content = ::File.read(::Folio::Engine.root.join("data/embed/dist/folio-embed-dist.html"))
+
+        etag_content = if ENV["CURRENT_RELEASE_COMMIT_HASH"]
+          "#{content} #{ENV["CURRENT_RELEASE_COMMIT_HASH"]}"
+        elsif Rails.env.development?
+          "#{content} #{Time.now.to_i}"
+        else
+          content
+        end
+
+        etag = Digest::MD5.hexdigest(etag_content)
+
+        [content, etag]
       end
     end
   end
