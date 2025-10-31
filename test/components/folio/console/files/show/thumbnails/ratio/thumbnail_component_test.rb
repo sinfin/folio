123cff00c941621155244b98afde87e64b14ef8b
Petr Marek <contact@petrmarek.eu>
2025-10-31 15:47:30 +0100

    fix(thumbnails): ratio/thumbnail component test

--- test/components/folio/console/files/show/thumbnails/ratio/thumbnail_component_test.rb
+++ test/components/folio/console/files/show/thumbnails/ratio/thumbnail_component_test.rb
@@ -7,7 +7,8 @@ class Folio::Console::Files::Show::Thumbnails::Ratio::ThumbnailComponentTest < F
     thumbnail = { uid: "foo/bar/baz.jpg", signature: "123456", url: "https://a/b/c/d.jpg", width: 250, height: 167, quality: 82, x: nil, y: nil, private: false, gravity: nil, webp_uid: "https://a/b/c/d.webp", webp_url: "https://a/b/c/d.webp", webp_signature: "654321" }
 
     render_inline(Folio::Console::Files::Show::Thumbnails::Ratio::ThumbnailComponent.new(thumbnail:,
-                                                                                         thumbnail_size_key: "250x167#"))
+                                                                                         thumbnail_size_key: "250x167#",
+                                                                                         file: create(:folio_file_image)))
 
     assert_selector(".f-c-files-show-thumbnails-ratio-thumbnail")
   end
