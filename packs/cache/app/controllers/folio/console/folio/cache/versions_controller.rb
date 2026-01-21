1f52da1740d101f9a6f6ad98332372262153c6fb
Petr Marek <contact@petrmarek.eu>
2026-01-21 10:55:55 +0100

    feat(cache): add invalidate_all and clear_rails_cache actions
    - Add invalidate_all collection action to invalidate all cache versions for current site
    - Add clear_rails_cache action to clear Rails.cache
    - Add buttons to index header for both actions with confirmation dialogs
    - Update index_header helper to support block syntax for component customization
    - Add pg_search_scope to Cache::Version model for search functionality
    - Add tests for both new actions
    - Update translations with proper Czech phrasing

--- packs/cache/app/controllers/folio/console/folio/cache/versions_controller.rb
+++ packs/cache/app/controllers/folio/console/folio/cache/versions_controller.rb
@@ -9,4 +9,24 @@ class Folio::Console::Folio::Cache::VersionsController < Folio::Console::BaseCon
     flash[:notice] = t(".flash")
     redirect_to url_for([:console, Folio::Cache::Version])
   end
+
+  def invalidate_all
+    site = Folio::Current.site
+    keys = Folio::Cache::Version.where(site_id: site.id).pluck(:key)
+
+    if keys.any?
+      Folio::Cache::Invalidator.invalidate!(site_id: site.id, keys:)
+      flash[:notice] = t(".invalidate_all_flash")
+    else
+      flash[:notice] = t(".invalidate_all_flash_empty")
+    end
+
+    redirect_to url_for([:console, Folio::Cache::Version])
+  end
+
+  def clear_rails_cache
+    Rails.cache.clear
+    flash[:notice] = t(".clear_rails_cache_flash")
+    redirect_to url_for([:console, Folio::Cache::Version])
+  end
 end
--- packs/cache/test/controllers/folio/console/folio/cache/versions_controller_test.rb
+++ packs/cache/test/controllers/folio/console/folio/cache/versions_controller_test.rb
@@ -38,4 +38,44 @@ class Folio::Console::Folio::Cache::VersionsControllerTest < Folio::Console::Bas
       assert model.updated_at > original_updated_at
     end
   end
+
+  test "invalidate_all" do
+    version1 = create(:folio_cache_version, site: @site)
+    version2 = create(:folio_cache_version, site: @site)
+    other_site = create_site(force: true)
+    other_site_version = create(:folio_cache_version, site: other_site)
+
+    original_updated_at1 = version1.updated_at
+    original_updated_at2 = version2.updated_at
+    original_updated_at_other = other_site_version.updated_at
+
+    travel 1.minute do
+      post url_for([:invalidate_all, :console, Folio::Cache::Version])
+
+      assert_redirected_to url_for([:console, Folio::Cache::Version])
+      assert_not_nil flash[:notice]
+
+      version1.reload
+      version2.reload
+      other_site_version.reload
+
+      assert version1.updated_at > original_updated_at1
+      assert version2.updated_at > original_updated_at2
+      assert_equal original_updated_at_other.to_i, other_site_version.updated_at.to_i
+    end
+  end
+
+  test "clear_rails_cache" do
+    test_key = "cache_versions_test_#{SecureRandom.hex}"
+    Rails.cache.write(test_key, "test_value")
+    assert_equal "test_value", Rails.cache.read(test_key)
+
+    post url_for([:clear_rails_cache, :console, Folio::Cache::Version])
+
+    assert_redirected_to url_for([:console, Folio::Cache::Version])
+    assert_not_nil flash[:notice]
+
+    # Verify cache was cleared by checking the key is gone
+    assert_nil Rails.cache.read(test_key), "Cache should be cleared after clear_rails_cache action"
+  end
 end
