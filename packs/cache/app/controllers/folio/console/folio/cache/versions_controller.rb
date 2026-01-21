5124cd30a336e5ac6a20cdb337dfb55020eef426
Petr Marek <contact@petrmarek.eu>
2026-01-21 10:31:15 +0100

    feat(cache): add invalidate action to cache versions
    - Add invalidate controller action that bumps updated_at via Invalidator
    - Add invalidate action button to versions index catalogue with reload icon
    - Add flash message translations in cache locale files
    - Add to_label method to Cache::Version model
    - Add test for invalidate action

--- packs/cache/app/controllers/folio/console/folio/cache/versions_controller.rb
+++ packs/cache/app/controllers/folio/console/folio/cache/versions_controller.rb
@@ -2,4 +2,11 @@
 
 class Folio::Console::Folio::Cache::VersionsController < Folio::Console::BaseController
   folio_console_controller_for "Folio::Cache::Version"
+
+  def invalidate
+    @version = Folio::Cache::Version.find(params[:id])
+    Folio::Cache::Invalidator.invalidate!(site_id: @version.site_id, keys: [@version.key])
+    flash[:notice] = t(".flash")
+    redirect_to url_for([:console, Folio::Cache::Version])
+  end
 end
