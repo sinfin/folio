543acb93981e4847c4c069f4593ffe97f45addb1
Petr Marek <contact@petrmarek.eu>
2026-01-26 12:33:01 +0100

    fix(cache): railtie and helper loading

--- lib/folio.rb
+++ lib/folio.rb
@@ -4,12 +4,53 @@ module Folio
   mattr_accessor :enabled_packs
   self.enabled_packs = [:cache]
 
+  LANGUAGES = {
+    cs: "CZ",
+    sk: "SK",
+    de: "DE",
+    pl: "PL",
+    es: "ES",
+    en: "GB",
+    en_US: "US"
+  }.freeze
+
+  EMAIL_REGEXP = URI::MailTo::EMAIL_REGEXP # Devise.email_regexp
+  OG_IMAGE_DIMENSIONS = "1200x630#"
+
+  # respect app/assets/javascripts/folio/message_bus.js
+  MESSAGE_BUS_CHANNEL = "folio_messagebus_channel"
+
+  LIGHTBOX_IMAGE_SIZE = "2560x2048>"
+
   def self.pack_enabled?(name)
     enabled_packs.include?(name.to_sym)
+  end
+
+  def self.table_name_prefix
+    "folio_"
+  end
+
+  def self.atoms_previews_stylesheet_path(site:, class_name:)
+    site.layout_assets_stylesheets_path
+  end
+
+  def self.expires_in
+    1.hour
+  end
+
+  def self.configure
+    yield self
   end
 end
 
 require "folio/engine"
+
+# Load pack Railties early so their initializers are registered before Rails processes them
+# This must happen at gem load time, not in an initializer
+Folio.enabled_packs.each do |pack_name|
+  pack_railtie_path = File.expand_path("../packs/#{pack_name}/lib/folio/#{pack_name}", __dir__)
+  require pack_railtie_path if File.exist?("#{pack_railtie_path}.rb")
+end
 
 require "dotenv-rails"
 require "friendly_id"
@@ -57,39 +98,3 @@ require "turbo-rails"
 require "dragonfly"
 require "dragonfly/s3_data_store"
 require "dragonfly_libvips"
-
-module Folio
-  LANGUAGES = {
-    cs: "CZ",
-    sk: "SK",
-    de: "DE",
-    pl: "PL",
-    es: "ES",
-    en: "GB",
-    en_US: "US"
-  }
-
-  EMAIL_REGEXP = URI::MailTo::EMAIL_REGEXP # Devise.email_regexp
-  OG_IMAGE_DIMENSIONS = "1200x630#"
-
-  # respect app/assets/javascripts/folio/message_bus.js
-  MESSAGE_BUS_CHANNEL = "folio_messagebus_channel"
-
-  LIGHTBOX_IMAGE_SIZE = "2560x2048>"
-
-  def self.table_name_prefix
-    "folio_"
-  end
-
-  def self.atoms_previews_stylesheet_path(site:, class_name:)
-    site.layout_assets_stylesheets_path
-  end
-
-  def self.expires_in
-    1.hour
-  end
-
-  def self.configure
-    yield self
-  end
-end
