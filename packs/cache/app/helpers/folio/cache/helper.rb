69d6a1d4aa0553bcba7745052f5a8663a332d305
Petr Marek <contact@petrmarek.eu>
2026-01-26 16:07:15 +0100

    feat(cache): add if/unless opts and Folio::Cache.exist?

--- packs/cache/app/helpers/folio/cache/helper.rb
+++ packs/cache/app/helpers/folio/cache/helper.rb
@@ -11,10 +11,27 @@ module Folio
       def folio_cache(name = {}, keys: [], **options, &block)
         return yield unless block_given?
 
+        # Handle if: and unless: options with early return
+        # Evaluate the condition (supports boolean, proc, symbol)
+        if options.key?(:if)
+          condition = options[:if]
+          condition_result = condition.is_a?(Proc) ? instance_exec(&condition) : (condition.is_a?(Symbol) ? send(condition) : condition)
+          return yield unless condition_result
+        end
+
+        if options.key?(:unless)
+          condition = options[:unless]
+          condition_result = condition.is_a?(Proc) ? instance_exec(&condition) : (condition.is_a?(Symbol) ? send(condition) : condition)
+          return yield if condition_result
+        end
+
         full_key = Folio::Cache.full_key(name:, keys:)
 
         # Set default expires_in
         options[:expires_in] ||= Folio::Cache::DEFAULT_EXPIRES_IN
+
+        # Remove if: and unless: from options since we've already handled them
+        options = options.except(:if, :unless)
 
         # Delegate to Rails cache helper (use helpers.cache for ViewComponent compatibility)
         if respond_to?(:helpers)
