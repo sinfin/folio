2fd2cc3dbf1eab5602006fcde3bc48af5c93ffe2
Vlada <vladislav.trefil@protonmail.com>
2026-01-28 12:50:45 +0100

    fix(meta): public_page_title remove tags

--- app/controllers/concerns/folio/set_meta_variables.rb
+++ app/controllers/concerns/folio/set_meta_variables.rb
@@ -30,6 +30,11 @@ module Folio::SetMetaVariables
     og_description = instance.try(m[:meta_description]).presence
     @public_page_description = og_description || description
 
+    if @public_page_title.present?
+      @public_page_title.gsub!("<br>", " ")
+      @public_page_title = ActionView::Base.full_sanitizer.sanitize(@public_page_title, tags: [])
+    end
+
     if @public_page_description.present?
       @public_page_description = ActionView::Base.full_sanitizer.sanitize(@public_page_description, tags: [])
     end
