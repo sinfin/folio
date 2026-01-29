4e4f9689786e4909b3d74fa22b5e0a06180f6912
Petr Marek <contact@petrmarek.eu>
2026-01-29 12:32:14 +0100

    Revert "improved displaying  conflicting revisions for TipTap"
    This reverts commit 3fe24056ba05437738b8300133c286ec3ebc79cc.

--- app/inputs/tiptap_input.rb
+++ app/inputs/tiptap_input.rb
@@ -75,7 +75,7 @@ class TiptapInput < SimpleForm::Inputs::StringInput
     end
 
     def current_user_latest_revision
-      @current_user_latest_revision ||= @builder.object.try(:latest_tiptap_revision, user: Folio::Current.user)
+      @current_user_latest_revision ||= @builder.object.try(:latest_tiptap_revision)
     end
 
     def has_unsaved_changes?
