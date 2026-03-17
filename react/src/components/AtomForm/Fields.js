ca108a166b8cb91c1776b24d88376271112a56ee
zanetagebka <zanetagebka@gmail.com>
2026-03-17 20:14:59 +0100

    fix: fix reorder

--- react/src/components/AtomForm/Fields.js
+++ react/src/components/AtomForm/Fields.js
@@ -3,13 +3,15 @@ import React from 'react'
 import Field from './Field'
 
 export default function Fields ({ atom, index, onChange, onValueChange, startSplittingAtom }) {
+  const atomKey = atom.record.id || atom.record.lodashId
+  
   return (
     <React.Fragment>
       {Object.keys(atom.record.meta.structure).map((field) => (
         <Field
           atom={atom}
           field={field}
-          key={field}
+          key={`${atomKey}-${field}`}
           index={index}
           onChange={onChange}
           onValueChange={onValueChange}
