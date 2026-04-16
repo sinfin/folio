7055bf43e9bdb51a170550860a730bd09d44478f
Petr Marek <contact@petrmarek.eu>
2026-04-16 11:53:02 +0200

    feat(special_characters): tweak trigger

--- app/components/folio/special_characters/trigger_component.js
+++ app/components/folio/special_characters/trigger_component.js
@@ -1,6 +1,15 @@
 window.Folio.Stimulus.register('f-special-characters-trigger', class extends window.Stimulus.Controller {
   toggle () {
-    this.element.dispatchEvent(new CustomEvent('f-special-characters-trigger:toggle', { bubbles: true }))
+    const formFooter = this.element.closest('.f-c-form-footer')
+    let target = this.element
+
+    if (formFooter) {
+      formFooter.dispatchEvent(new CustomEvent('f-c-form-footer:collapse'))
+      // use document as target on mobile so that the popover is centered on the screen
+      target = document
+    }
+
+    target.dispatchEvent(new CustomEvent('f-special-characters-trigger:toggle', { bubbles: true }))
   }
 
   preventDefault (e) {
