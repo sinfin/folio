31583311416c8dc879ce4cff7ed8ec1d94a14c6d
Petr Marek <contact@petrmarek.eu>
2026-04-30 09:38:37 +0200

    feat(tiptap): add color node attributes
    Normalize supported CSS color formats to persisted hex values and render color inputs in the Tiptap overlay.

--- app/lib/folio/tiptap/node_builder.rb
+++ app/lib/folio/tiptap/node_builder.rb
@@ -39,6 +39,8 @@ class Folio::Tiptap::NodeBuilder
           setup_structure_for_integer(key:)
         when :embed
           setup_structure_for_embed(key:)
+        when :color
+          setup_structure_for_color(key:)
         else
           fail ArgumentError, "Unsupported type #{attr_config[:type]} in tiptap_node definition"
         end
@@ -77,6 +79,23 @@ class Folio::Tiptap::NodeBuilder
 
     def setup_structure_default(key:)
       @klass.attribute key, type: :text
+    end
+
+    def setup_structure_for_color(key:)
+      @klass.attribute key, type: :text
+      @klass.validates key, format: { with: Folio::Tiptap::Color::HEX_FORMAT }, allow_blank: true
+
+      @klass.define_method "#{key}=" do |value|
+        transformed_value = if value.nil?
+          nil
+        elsif value.is_a?(String)
+          Folio::Tiptap::Color.normalize(value) || value
+        else
+          fail ArgumentError, "Expected a String for #{key}, got #{value.class.name}"
+        end
+
+        super(transformed_value)
+      end
     end
 
     def setup_structure_for_collection(key:, attr_config:)
--- test/components/folio/console/tiptap/overlay/form_component_test.rb
+++ test/components/folio/console/tiptap/overlay/form_component_test.rb
@@ -3,6 +3,12 @@
 require "test_helper"
 
 class Folio::Console::Tiptap::Overlay::FormComponentTest < Folio::Console::ComponentTest
+  class ColorNode < Folio::Tiptap::Node
+    tiptap_node structure: {
+      accent_color: :color,
+    }
+  end
+
   def test_render
     with_controller_class(Folio::Console::PagesController) do
       with_request_url "/console/pages/new" do
@@ -10,6 +16,17 @@ class Folio::Console::Tiptap::Overlay::FormComponentTest < Folio::Console::Compo
         render_inline(Folio::Console::Tiptap::Overlay::FormComponent.new(node:))
 
         assert_selector(".f-c-tiptap-overlay-form")
+      end
+    end
+  end
+
+  def test_render_color_input
+    with_controller_class(Folio::Console::PagesController) do
+      with_request_url "/console/pages/new" do
+        node = ColorNode.new(accent_color: "#ff00aa")
+        render_inline(Folio::Console::Tiptap::Overlay::FormComponent.new(node:))
+
+        assert_selector('input[type="color"][name="tiptap_node_attrs[data][accent_color]"][value="#ff00aa"]')
       end
     end
   end
