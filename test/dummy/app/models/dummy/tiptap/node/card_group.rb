135c42fcd6079ac3ee3d2d9fd4b85413ed28a174
Petr Marek <contact@petrmarek.eu>
2026-05-25 11:18:36 +0200

    test(tiptap): add dummy nested node example

---
+++ test/dummy/app/models/dummy/tiptap/node/card_group.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class Dummy::Tiptap::Node::CardGroup < Folio::Tiptap::Node
+  class Card < Folio::Tiptap::Node
+    BACKGROUNDS = %w[white blue].freeze
+
+    tiptap_node nested: true,
+                 structure: {
+                   title: :string,
+                   text: :text,
+                   content: :rich_text,
+                   button_url_json: :url_json,
+                   background: BACKGROUNDS,
+                   cover: :image,
+                 }
+
+    validates :title,
+              presence: true
+  end
+
+  tiptap_node structure: {
+    title: :string,
+    intro: :text,
+    cards: {
+      type: :nested_nodes,
+      node_class: Card,
+    },
+  }
+
+  validates :title,
+            presence: true
+end
